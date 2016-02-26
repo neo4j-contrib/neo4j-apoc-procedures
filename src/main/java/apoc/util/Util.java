605c3e7164a137aa68a48289024672bdd48b32cb
Michael Hunger <github@jexp.de>
2016-02-26 12:26:41 +0100

    added sort nodes by property, from/toJson

--- src/main/java/apoc/util/Util.java
+++ src/main/java/apoc/util/Util.java
@@ -34,12 +34,25 @@ public class Util {
         Collections.sort((List<? extends Comparable>)sorted);
         return Stream.of(new ListResult(sorted));
     }
-/*
+
     @Procedure
     public Stream<ListResult> sortNodes(@Name("coll") List coll, @Name("prop") String prop) {
         List sorted = new ArrayList(coll);
-        Collections.sort((List<? extends PropertyContainer>)sorted, (x,y) -> x.getProperty(prop, null) y.getProperty(prop, null) );
+        Collections.sort((List<? extends PropertyContainer>)sorted, 
+                         (x,y) -> compare(x.getProperty(prop, null), y.getProperty(prop, null)));
         return Stream.of(new ListResult(sorted));
     }
-*/
+    public static int compare(Object o1, Object o2) {
+	    if (o1 == null) return -1;
+	    if (o1 == null) return o2 == null ? 0 : -1;
+	    if (o2 == null) return 1;
+	    if (o1.equals(o2)) return 0;
+        if (o1 instanceof Number && o2 instanceof Number) {
+	        if (o1 instanceof Double || o2 instanceof Double  || o1 instanceof Float|| o2 instanceof Float) 
+				return Double.compare(((Number)o1).doubleValue(),((Number)o2).doubleValue());
+			return Long.compare(((Number)o1).longValue(),((Number)o2).longValue());
+		}
+        if (o1 instanceof Boolean && o2 instanceof Boolean) return ((Boolean)o1) ? 1 : -1;
+        return o1.toString().compareTo(o2.toString());
+    }
 }
--- src/test/java/apoc/util/UtilTest.java
+++ src/test/java/apoc/util/UtilTest.java
@@ -12,10 +12,16 @@ import org.neo4j.kernel.GraphDatabaseAPI;
 import org.neo4j.kernel.impl.proc.Procedures;
 
 public class UtilTest {
-    @Test public void testIN() throws Exception {
-	    GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-	    ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(Util.class);
 
+    private GraphDatabaseService db;
+	@Before public void setUp() throws Exception {
+	    db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+	    ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(Util.class);
+	}
+    @After public void tearDown() {
+	    db.shutdown();
+    }
+    @Test public void testIN() throws Exception {
         Result res = db.execute("CALL apoc.util.IN(1,[1,2,3])");
 
         assertTrue(res.hasNext());
@@ -24,14 +30,23 @@ public class UtilTest {
         assertFalse(res.hasNext());
     }
     @Test public void testSort() throws Exception {
-	    GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-	    ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(Util.class);
-
         Result res = db.execute("CALL apoc.util.sort([3,2,1])");
 
         assertTrue(res.hasNext());
         Map<String,Object> row = res.next();
         assertEquals(Arrays.asList(1L,2L,3L), row.get("value"));
         assertFalse(res.hasNext());
+    }
+    @Test public void testSortNodes() throws Exception {
+        try (Transaction tx = db.beginTx()) {
+	        Result res = db.execute("CREATE (n {name:'foo'}),(m {name:'bar'}) WITH n,m CALL apoc.util.sortNodes([n,m], 'name') YIELD value RETURN value");
+	
+	        assertTrue(res.hasNext());
+	        Map<String,Object> row = res.next();
+			List<Node> nodes = (List<Node>)row.get("value");
+	        assertEquals("bar",nodes.get(0).getProperty("name"));
+	        assertEquals("foo",nodes.get(1).getProperty("name"));
+	        assertFalse(res.hasNext());
+        }
     }
 }
