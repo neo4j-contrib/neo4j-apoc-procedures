857d659ecd466eea6dcb777715fd9745757019f8
Michael Hunger <github@jexp.de>
2016-02-28 11:51:20 +0100

    added Json Functions and loadJson

---
+++ src/main/java/apoc/util/Json.java
@@ -0,0 +1,80 @@
+package apoc.util;
+
+import java.net.URL;
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+import apoc.util.result.ListResult;
+import apoc.util.result.MapResult;
+import apoc.util.result.ObjectResult;
+import apoc.util.result.StringResult;
+import org.neo4j.graphdb.*;
+import org.neo4j.procedure.*;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Json {
+
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    @Context public org.neo4j.graphdb.GraphDatabaseService db;
+
+    @Procedure
+    public Stream<StringResult> toJson(@Name("value") Object value) {
+        try {
+            return Stream.of(new StringResult(OBJECT_MAPPER.writeValueAsString(value)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't convert " + value + " to json", e);
+        }
+    }
+
+    @Procedure
+    @PerformsWrites
+    public void setJsonProperty(@Name("node") Node node, @Name("key") String key, @Name("value") Object value) {
+        try {
+            node.setProperty(key,OBJECT_MAPPER.writeValueAsString(value));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't convert " + value + " to json", e);
+        }
+    }
+
+    @Procedure
+    public Stream<ObjectResult> getJsonProperty(@Name("node") Node node, @Name("key") String key) {
+        String value = (String)node.getProperty(key, null);
+        try {
+            return Stream.of(new ObjectResult(OBJECT_MAPPER.readValue(value, Object.class)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't convert " + value + " to json", e);
+        }
+    }
+
+    @Procedure
+    public Stream<MapResult> fromJsonMap(@Name("map") String value) {
+        try {
+            return Stream.of(new MapResult(OBJECT_MAPPER.readValue(value, Map.class)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't deserialize to Map:\n"+value,e);
+        }
+    }
+
+    @Procedure
+    public Stream<ListResult> fromJsonList(@Name("list") String value) {
+        try {
+            return Stream.of(new ListResult(OBJECT_MAPPER.readValue(value, List.class)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't deserialize to List:\n"+value,e);
+        }
+    }
+
+    @Procedure
+    public Stream<ObjectResult> loadJson(@Name("url") String url) {
+        try {
+            Object value = OBJECT_MAPPER.readValue(new URL(url), Object.class);
+                if (value instanceof Iterable) {
+                return StreamSupport.stream(((Iterable<Object>)value).spliterator(),false).map(ObjectResult::new);
+            }
+            return Stream.of(new ObjectResult(value));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read url " + url + " as json", e);
+        }
+    }
+}
---
+++ src/test/java/apoc/util/JsonTest.java
@@ -0,0 +1,68 @@
+package apoc.util;
+
+import java.net.URL;
+import java.util.*;
+
+import org.neo4j.graphdb.*;
+import org.junit.*;
+
+import static apoc.util.TestUtil.testCall;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.*;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.kernel.impl.proc.Procedures;
+
+public class JsonTest {
+
+    private GraphDatabaseService db;
+	@Before public void setUp() throws Exception {
+	    db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+	    ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(Json.class);
+	}
+    @After public void tearDown() {
+	    db.shutdown();
+    }
+
+    @Test public void testToJsonList() throws Exception {
+	    testCall(db, "CALL apoc.util.toJson([1,2,3])",
+	             (row) -> assertEquals("[1,2,3]", row.get("value")) );
+    }
+    @Test public void testToJsonMap() throws Exception {
+	    testCall(db, "CALL apoc.util.toJson({a:42,b:\"foo\",c:[1,2,3]})",
+	             (row) -> assertEquals("{\"a\":42,\"b\":\"foo\",\"c\":[1,2,3]}", row.get("value")) );
+    }
+    @Test public void testFromJsonList() throws Exception {
+	    testCall(db, "CALL apoc.util.fromJsonList('[1,2,3]')",
+	             (row) -> assertEquals(asList(1,2,3), row.get("value")) );
+    }
+    @Test public void testFromJsonMap() throws Exception {
+	    testCall(db, "CALL apoc.util.fromJsonMap('{\"a\":42,\"b\":\"foo\",\"c\":[1,2,3]}')",
+	             (row) -> {
+		           Map value = (Map)row.get("value");
+		           assertEquals(42, value.get("a"));
+		           assertEquals("foo", value.get("b"));
+		           assertEquals(asList(1,2,3), value.get("c"));
+		         });
+    }
+    @Test public void testSetJsonProperty() throws Exception {
+        testCall(db, "CREATE (n) WITH n CALL apoc.util.setJsonProperty(n, 'json', [1,2,3]) RETURN n",
+                (row) -> assertEquals("[1,2,3]", ((Node)row.get("n")).getProperty("json")));
+    }
+
+    @Test public void testGetJsonProperty() throws Exception {
+        testCall(db, "CREATE (n {json:'[1,2,3]'}) WITH n CALL apoc.util.getJsonProperty(n, 'json') YIELD value RETURN value",
+                (row) -> assertEquals(asList(1,2,3), row.get("value")) );
+    }
+    @Test public void testLoadJson() throws Exception {
+//		URL url = getClass().getResource("map.json");
+		testCall(db, "CALL apoc.util.loadJson('file:map.json') YIELD value RETURN value",
+                (row) -> {
+                    Object value = row.get("value");
+                    System.err.println(value.getClass());
+                    assertEquals(singletonMap("foo",asList(1,2,3)), value);
+                });
+    }
+}
