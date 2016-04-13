5717a67208e3561a4f0c59acb9917e6b86f3479a
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-13 22:28:53 +0200

    monitor.ids

---
+++ src/main/java/apoc/monitor/Id.java
@@ -0,0 +1,47 @@
+package apoc.monitor;
+
+import apoc.Description;
+import apoc.result.IdsResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.jmx.JmxUtils;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Procedure;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+public class Id {
+
+    private static final String JMX_OBJECT_NAME = "Primitive count";
+    private static final String NODE_IDS_KEY = "NumberOfNodeIdsInUse";
+    private static final String REL_IDS_KEY = "NumberOfRelationshipIdsInUse";
+    private static final String PROP_IDS_KEY = "NumberOfPropertyIdsInUse";
+    private static final String REL_TYPE_IDS_KEY = "NumberOfRelationshipTypeIdsInUse";
+
+    @Context
+    public GraphDatabaseService database;
+
+    @Procedure
+    @Description("apoc.monitor.ids() returns the object ids in use for this neo4j instance")
+    public Stream<IdsResult> ids() throws Exception {
+        return Stream.of(getIdsInUse());
+    }
+
+    private IdsResult getIdsInUse() {
+        ObjectName objectName = JmxUtils.getObjectName(database, JMX_OBJECT_NAME);
+
+        return new IdsResult(
+                getAttribute(objectName, NODE_IDS_KEY),
+                getAttribute(objectName, REL_IDS_KEY),
+                getAttribute(objectName, PROP_IDS_KEY),
+                getAttribute(objectName, REL_TYPE_IDS_KEY)
+        );
+    }
+
+    private long getAttribute(ObjectName objectName, String attribute) {
+        return JmxUtils.getAttribute(objectName, attribute);
+    }
+
+}
---
+++ src/test/java/apoc/monitor/IdProcedureTest.java
@@ -0,0 +1,51 @@
+package apoc.monitor;
+
+import apoc.util.TestUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import static org.junit.Assert.*;
+
+import static apoc.util.TestUtil.testCall;
+
+public class IdProcedureTest {
+
+    private static GraphDatabaseService db;
+
+    @Before
+    public void setUp() throws Exception {
+        db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        TestUtil.registerProcedure(db, Id.class);
+    }
+
+    @Test
+    public void testGetNodeIdsInUse() {
+        createData();
+        testCall(db, "CALL apoc.monitor.ids()", (row) -> {
+            long nodeIds = (long) row.get("nodeIds");
+            long relIds = (long) row.get("relIds");
+            long propIds = (long) row.get("propIds");
+            long relTypeIds = (long) row.get("relTypeIds");
+            assertEquals(6L, nodeIds);
+            assertEquals(2L, relIds);
+            assertEquals(1L, propIds);
+            assertEquals(2L, relTypeIds);
+        });
+    }
+
+    private void createData() {
+        testCall(db, "CREATE (n)", (row) -> {});
+        testCall(db, "CREATE (n)-[:REL_TYPE1]->(n2)", (row) -> {});
+        testCall(db, "CREATE (n)-[:REL_TYPE2]->(n2)", (row) -> {});
+        testCall(db, "CREATE (n) SET n.key = 123", (row) -> {});
+    }
+
+    @After
+    public void tearDown() {
+        db.shutdown();
+    }
+
+}
