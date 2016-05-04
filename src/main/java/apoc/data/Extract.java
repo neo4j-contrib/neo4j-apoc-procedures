7f141b693503a75c492bd6b5adcb58b69df27e23
Michael Hunger <github@jexp.de>
2016-05-04 15:35:14 +0200

    Add apoc.data.domain to extract domains from email and url

---
+++ src/main/java/apoc/data/Extract.java
@@ -0,0 +1,28 @@
+package apoc.data;
+
+import apoc.Description;
+import apoc.result.StringResult;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Extract {
+
+    public static final Pattern DOMAIN = Pattern.compile("([\\w-]+\\.[\\w-]+)+(\\w+)");
+
+    // null if nothing found
+    @Procedure
+    @Description("apoc.data.domain('url_or_email_address') YIELD domain - extract the domain name from a url or an email address. If nothing was found, yield null.")
+    public Stream<StringResult> domain(final @Name("url_or_email_address") String value) {
+        if (value != null) {
+            for (String part : value.split("[@/<>]")) {
+                if (DOMAIN.matcher(part).matches()) return Stream.of(new StringResult(part));
+            }
+        }
+        return Stream.of(new StringResult(null));
+    }
+}
---
+++ src/test/java/apoc/data/ExtractTest.java
@@ -0,0 +1,73 @@
+package apoc.data;
+
+import apoc.util.TestUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import static apoc.util.TestUtil.map;
+import static apoc.util.TestUtil.testCall;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+public class ExtractTest {
+
+    private GraphDatabaseService db;
+
+    @Before
+    public void setUp() throws Exception {
+        db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        TestUtil.registerProcedure(db, Extract.class);
+    }
+
+    @After
+    public void tearDown() {
+        db.shutdown();
+    }
+
+    @Test
+    public void testQuotedEmail() {
+        testCall(db, "CALL apoc.data.domain('<foo@bar.baz>')",
+                row -> Assert.assertThat(row.get("value"), equalTo("bar.baz")));
+    }
+
+    @Test
+    public void testEmail() {
+        testCall(db, "CALL apoc.data.domain('foo@bar.baz')",
+                row -> Assert.assertThat(row.get("value"), equalTo("bar.baz")));
+    }
+
+    @Test
+    public void testNull() {
+        testCall(db, "CALL apoc.data.domain(null) YIELD value",
+                row -> assertEquals(null, row.get("value")));
+    }
+
+    @Test
+    public void testBadString() {
+        testCall(db, "CALL apoc.data.domain('asdsgawe4ge') YIELD value",
+                row -> assertEquals(null, row.get("value")));
+    }
+
+    @Test
+    public void testEmptyString() {
+        testCall(db, "CALL apoc.data.domain('') YIELD value",
+                row -> assertEquals(null, row.get("value")));
+    }
+
+    @Test
+    public void testUrl() {
+        testCall(db, "CALL apoc.data.domain('http://www.example.com/lots-of-stuff') YIELD value",
+                row -> assertEquals("www.example.com", row.get("value")));
+    }
+
+    @Test
+    public void testQueryParameter() {
+        testCall(db, "CALL apoc.data.domain({param}) YIELD value",
+                map("param", "www.foo.bar/baz"),
+                row -> assertEquals("www.foo.bar", row.get("value")));
+    }
+}
