d8de8fc7d02e0432c10937a94e4d351e2d8f3290
Michael Hunger <github@jexp.de>
2016-05-04 23:37:06 +0200

    fixed geocode

--- src/main/java/apoc/spatial/Geocode.java
+++ src/main/java/apoc/spatial/Geocode.java
@@ -1,11 +1,7 @@
 package apoc.spatial;
 
 import apoc.Description;
-import apoc.load.LoadJson;
-import apoc.result.MapResult;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -23,16 +19,36 @@ public class Geocode {
     public GraphDatabaseService db;
 
     @Procedure
-    @Description("apoc.spatial.geocode('address') YIELD object - look up geographic location of address from geocoding service")
-    public Stream<MapResult> geocode(@Name("address") String address) {
+    @Description("apoc.spatial.geocode('address') YIELD location, latitude, longitude, description, osmData - look up geographic location of location from openstreetmap geocoding service")
+    public Stream<GeoCodeResult> geocode(@Name("location") String address) {
         String url = "http://nominatim.openstreetmap.org/search.php?q=" + address.replace(" ", "+") + "&format=json";
         Object value = JsonUtil.loadJson(url);
         if (value instanceof List) {
-            return ((List<Map>) value).stream().map(data ->
-                    map("latitude", data.get("lat"), "longitude", data.get("lon"),
-                        "description", data.get("display_name"), "osm", data)
-            ).map(MapResult::new);
+            return ((List<Map<String, Object>>) value).stream().map(data ->
+                    new GeoCodeResult(toDouble(data.get("lat")), toDouble(data.get("lon")), String.valueOf(data.get("display_name")), data));
         }
-        throw new RuntimeException("Can't parse geocoding results "+value);
+        throw new RuntimeException("Can't parse geocoding results " + value);
+    }
+
+    private static Double toDouble(Object value) {
+        if (value == null) return null;
+        if (value instanceof Number) return ((Number) value).doubleValue();
+        return Double.parseDouble(value.toString());
+    }
+
+    public static class GeoCodeResult {
+        public final Map<String, Object> location;
+        public final Map<String, Object> osmData;
+        public final Double latitude;
+        public final Double longitude;
+        public final String description;
+
+        public GeoCodeResult(Double latitude, Double longitude, String description, Map<String, Object> osmData) {
+            this.osmData = osmData;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.description = description;
+            this.location = map("latitude", latitude, "longitude", longitude, "description", description);
+        }
     }
 }
--- src/test/java/apoc/spatial/GeocodeTest.java
+++ src/test/java/apoc/spatial/GeocodeTest.java
@@ -1,95 +1,77 @@
 package apoc.spatial;
 
 import apoc.util.JsonUtil;
+import apoc.util.TestUtil;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import apoc.util.TestUtil;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.test.TestGraphDatabaseFactory;
-
-import static apoc.util.TestUtil.testCallCount;
-import static org.junit.Assert.assertEquals;
 import static apoc.util.MapUtil.map;
-import static apoc.util.TestUtil.testCall;
-import static apoc.util.TestUtil.testCallEmpty;
+import static apoc.util.TestUtil.*;
+import static org.junit.Assert.assertEquals;
 
-public class GeocodeTest
-{
+public class GeocodeTest {
 
     private GraphDatabaseService db;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-        TestUtil.registerProcedure( db, Geocode.class );
+        TestUtil.registerProcedure(db, Geocode.class);
     }
 
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         db.shutdown();
     }
 
     @Test
-    public void testGeocode() throws Exception
-    {
-        URL url = ClassLoader.getSystemResource( "spatial.json" );
-        Map tests = (Map) JsonUtil.OBJECT_MAPPER.readValue( url.openConnection().getInputStream(), Object.class );
-        for ( Object address : (List) tests.get( "addresses" ) )
-        {
-            testGeocodeAddress( (Map) address );
+    public void testGeocode() throws Exception {
+        URL url = ClassLoader.getSystemResource("spatial.json");
+        Map tests = (Map) JsonUtil.OBJECT_MAPPER.readValue(url.openConnection().getInputStream(), Object.class);
+        for (Object address : (List) tests.get("addresses")) {
+            testGeocodeAddress((Map) address);
         }
     }
 
-    private void testGeocodeAddress( Map map )
-    {
-        if ( map.containsKey( "noresults" ) )
-        {
-            for ( String field : new String[]{"address", "noresults"} )
-            {
-                Assert.assertTrue( "Expected " + field + " field", map.containsKey( field ) );
+    private void testGeocodeAddress(Map map) {
+        if (map.containsKey("noresults")) {
+            for (String field : new String[]{"address", "noresults"}) {
+                Assert.assertTrue("Expected " + field + " field", map.containsKey(field));
             }
-            testCallEmpty( db, "CALL apoc.spatial.geocode({url})", map( "url", map.get( "address" ).toString() ) );
-        }
-        else if ( map.containsKey( "count" ) )
-        {
-            for ( String field : new String[]{"address", "count"} )
-            {
-                Assert.assertTrue( "Expected " + field + " field", map.containsKey( field ) );
+            testCallEmpty(db, "CALL apoc.spatial.geocode({url})", map("url", map.get("address").toString()));
+        } else if (map.containsKey("count")) {
+            for (String field : new String[]{"address", "count"}) {
+                Assert.assertTrue("Expected " + field + " field", map.containsKey(field));
             }
-            testCallCount( db, "CALL apoc.spatial.geocode({url})",
-                    map( "url", map.get( "address" ).toString() ),
-                    (int) map.get( "count" ) );
-        }
-        else
-        {
-            for ( String field : new String[]{"address", "latitude", "longitude"} )
-            {
-                Assert.assertTrue( "Expected " + field + " field", map.containsKey( field ) );
+            testCallCount(db, "CALL apoc.spatial.geocode({url})",
+                    map("url", map.get("address").toString()),
+                    (int) map.get("count"));
+        } else {
+            for (String field : new String[]{"address", "latitude", "longitude"}) {
+                Assert.assertTrue("Expected " + field + " field", map.containsKey(field));
             }
-            testGeocodeAddress( map.get( "address" ).toString(),
-                    (double) map.get( "latitude" ),
-                    (double) map.get( "longitude" ) );
+            testGeocodeAddress(map.get("address").toString(),
+                    (double) map.get("latitude"),
+                    (double) map.get("longitude"));
         }
     }
 
-    private void testGeocodeAddress( String address, double lat, double lon )
-    {
-        testCall( db, "CALL apoc.spatial.geocode({url})", map( "url", address ),
-                ( row ) -> {
-                    Map value = (Map) row.get( "value" );
-                    assertEquals( "Incorrect latitude found", lat, Double.parseDouble( value.get( "latitude" ).toString() ),
-                            0.0001 );
-                    assertEquals( "Incorrect longitude found", lon, Double.parseDouble( value.get( "longitude" ).toString() ),
-                            0.0001 );
-                } );
+    private void testGeocodeAddress(String address, double lat, double lon) {
+        testCall(db, "CALL apoc.spatial.geocode({url})", map("url", address),
+                (row) -> {
+                    Map value = (Map) row.get("location");
+                    assertEquals("Incorrect latitude found", lat, Double.parseDouble(value.get("latitude").toString()),
+                            0.0001);
+                    assertEquals("Incorrect longitude found", lon, Double.parseDouble(value.get("longitude").toString()),
+                            0.0001);
+                });
     }
 }
