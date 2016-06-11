8c4af649da41bc2d1e74999e636079d2858349dd
Michael Hunger <github@jexp.de>
2016-06-12 00:19:42 +0200

    Addditional tests for eagerness problems with procedures

--- src/test/java/apoc/refactor/GraphRefactoringTest.java
+++ src/test/java/apoc/refactor/GraphRefactoringTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.neo4j.graphdb.*;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -34,11 +35,79 @@ public class GraphRefactoringTest {
         TestUtil.registerProcedure(db, GraphRefactoring.class);
     }
 
+    /*
+        MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) call apoc.refactor.mergeNodes([o,n]) yield node return node
+     */
     @After
     public void tearDown() {
         db.shutdown();
     }
 
+    @Test
+    public void testDeleteOneNode() throws Exception {
+        long id = db.execute("CREATE (p1:Person {ID:1}), (p2:Person {ID:2}) RETURN id(p1) as id ").<Long>columnAs("id").next();
+        ExecutionPlanDescription plan = db.execute("EXPLAIN MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) DELETE o RETURN o as node").getExecutionPlanDescription();
+        System.out.println(plan);
+        System.out.flush();
+        testCall(db, "EXPLAIN MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) DELETE o RETURN o as node",
+                      map("oldID", 1L, "newID",2L),
+                (r) -> {
+                    Node node = (Node) r.get("node");
+                    assertEquals(id, node.getId());
+                    assertEquals(true, node.hasLabel(Label.label("Person")));
+                    assertEquals(2L, node.getProperty("ID"));
+                });
+    }
+
+    @Test(expected = QueryExecutionException.class)
+    public void testEagernessMergeNodesFails() throws Exception {
+        db.execute("CREATE INDEX ON :Person(ID)").close();
+        long id = db.execute("CREATE (p1:Person {ID:1}), (p2:Person {ID:2}) RETURN id(p1) as id ").<Long>columnAs("id").next();
+        ExecutionPlanDescription plan = db.execute("EXPLAIN MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) CALL apoc.refactor.mergeNodes([o,n]) yield node return node").getExecutionPlanDescription();
+        System.out.println(plan);
+        System.out.flush();
+        testCall(db, "MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) CALL apoc.refactor.mergeNodes([o,n]) yield node return node",
+                      map("oldID", 1L, "newID",2L),
+                (r) -> {
+                    Node node = (Node) r.get("node");
+                    assertEquals(id, node.getId());
+                    assertEquals(true, node.hasLabel(Label.label("Person")));
+                    assertEquals(2L, node.getProperty("ID"));
+                });
+    }
+
+    @Test
+    public void testMergeNodesEagerAggregation() throws Exception {
+        long id = db.execute("CREATE (p1:Person {ID:1}), (p2:Person {ID:2}) RETURN id(p1) as id ").<Long>columnAs("id").next();
+        ExecutionPlanDescription plan = db.execute("EXPLAIN MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) WITH head(collect([o,n])) as nodes CALL apoc.refactor.mergeNodes(nodes) yield node return node").getExecutionPlanDescription();
+        System.out.println(plan);
+        System.out.flush();
+        testCall(db, "MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) WITH head(collect([o,n])) as nodes CALL apoc.refactor.mergeNodes(nodes) yield node return node",
+                      map("oldID", 1L, "newID",2L),
+                (r) -> {
+                    Node node = (Node) r.get("node");
+                    assertEquals(id, node.getId());
+                    assertEquals(true, node.hasLabel(Label.label("Person")));
+                    assertEquals(2L, node.getProperty("ID"));
+                });
+    }
+
+    @Test
+    public void testMergeNodesEagerIndex() throws Exception {
+        db.execute("CREATE INDEX ON :Person(ID)").close();
+        long id = db.execute("CREATE (p1:Person {ID:1}), (p2:Person {ID:2}) RETURN id(p1) as id ").<Long>columnAs("id").next();
+        ExecutionPlanDescription plan = db.execute("EXPLAIN MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) USING INDEX o:Person(ID) USING INDEX n:Person(ID) CALL apoc.refactor.mergeNodes([o,n]) yield node return node").getExecutionPlanDescription();
+        System.out.println(plan);
+        System.out.flush();
+        testCall(db, "MATCH (o:Person {ID:{oldID}}), (n:Person {ID:{newID}}) USING INDEX o:Person(ID) USING INDEX n:Person(ID) CALL apoc.refactor.mergeNodes([o,n]) yield node return node",
+                      map("oldID", 1L, "newID",2L),
+                (r) -> {
+                    Node node = (Node) r.get("node");
+                    assertEquals(id, node.getId());
+                    assertEquals(true, node.hasLabel(Label.label("Person")));
+                    assertEquals(2L, node.getProperty("ID"));
+                });
+    }
     @Test
     public void testExtractNode() throws Exception {
         Long id = db.execute("CREATE (f:Foo)-[rel:FOOBAR {a:1}]->(b:Bar) RETURN id(rel) as id").<Long>columnAs("id").next();
