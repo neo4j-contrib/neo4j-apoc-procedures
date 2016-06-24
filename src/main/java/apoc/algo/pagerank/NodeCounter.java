5b9333b89f8ec19bcd9def67371a2b28510587aa
johnymontana <lyonwj@gmail.com>
2016-06-24 16:07:26 -0700

    Remove runtime=compiled in NodeCounter Cypher statement
    runtime=compiled option was removed in Neo4j 3.0.3 and using
    this results in a Cypher syntax error

--- src/main/java/apoc/algo/pagerank/NodeCounter.java
+++ src/main/java/apoc/algo/pagerank/NodeCounter.java
@@ -7,7 +7,7 @@ public class NodeCounter
 {
     public int getNodeCount( GraphDatabaseService db )
     {
-        Result result = db.execute( "CYPHER runtime=compiled MATCH (n) RETURN max(id(n)) AS maxId" );
+        Result result = db.execute( "MATCH (n) RETURN max(id(n)) AS maxId" );
         return ((Number) result.next().get( "maxId" )).intValue() + 1;
 //        NeoStores neoStore = ((GraphDatabaseAPI) db).getDependencyResolver().resolveDependency(NeoStores.class);
 //        return (int) neoStore.getNodeStore().getHighId();
@@ -15,7 +15,7 @@ public class NodeCounter
 
     public int getRelationshipCount( GraphDatabaseService db )
     {
-        Result result = db.execute( "CYPHER runtime=compiled MATCH ()-[r]->() RETURN max(id(r)) AS maxId" );
+        Result result = db.execute( "MATCH ()-[r]->() RETURN max(id(r)) AS maxId" );
         return ((Number) result.next().get( "maxId" )).intValue() + 1;
 //        NeoStores neoStore = ((GraphDatabaseAPI) db).getDependencyResolver().resolveDependency(NeoStores.class);
 //        return (int) neoStore.getRelationshipStore().getHighId();
