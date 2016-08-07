a61e0090cf8460467cac9257922a8abb306b8568
Tom Michiels <tom.michiels@me.com>
2016-08-07 13:03:47 +0200

    Basic WCC Implementation (#50)
    * added WCC, ER  custom style
    * added Weakly connected components
    * used neo4j primitive collections
    * added wcc with nodes and label counter

---
+++ src/main/java/apoc/algo/wcc/CCVar.java
@@ -0,0 +1,22 @@
+package apoc.algo.wcc;
+
+public class CCVar {
+	
+	private String id;
+	private String type;
+	
+	public CCVar(String id,String type){
+		this.id=id;
+		this.type=type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+
+}
