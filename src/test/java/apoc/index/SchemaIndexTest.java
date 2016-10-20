916574199a2e6ed3e3d69c4d85102470394fc5ba
Michael Hunger <github@jexp.de>
2016-10-20 11:17:06 +0200

    Clean up schema index procedures.

--- src/test/java/apoc/index/SchemaIndexTest.java
+++ src/test/java/apoc/index/SchemaIndexTest.java
@@ -6,11 +6,19 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.StreamSupport;
 
+import static apoc.util.MapUtil.map;
+import static apoc.util.TestUtil.testCall;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -27,7 +35,7 @@ public class SchemaIndexTest {
                 .newImpermanentDatabaseBuilder()
                 .newGraphDatabase();
         TestUtil.registerProcedure(db, SchemaIndex.class);
-        db.execute("UNWIND range(1,200) as id CREATE (:Person {name:'name'+id, id:id, age:id % 100})").close();
+        db.execute("CREATE (city:City {name:'London'}) WITH city UNWIND range(1,200) as id CREATE (:Person {name:'name'+id, id:id, age:id % 100})-[:LIVES_IN]->(city)").close();
         db.execute("CREATE INDEX ON :Person(name)").close();
         db.execute("CREATE INDEX ON :Person(age)").close();
         db.execute("CREATE CONSTRAINT ON (p:Person) ASSERT p.id IS UNIQUE").close();
@@ -42,6 +50,14 @@ public class SchemaIndexTest {
         db.shutdown();
     }
 
+    @Test
+    public void testRelated() throws Exception {
+        TestUtil.testResult(db, "MATCH (city:City) WITH city CALL apoc.index.related([city],'Person','age','LIVES_IN>',5) YIELD node RETURN *", r -> {
+            Function<Result,Object> age = (res) -> ((Node)res.next().get("node")).getProperty("age");
+            LongStream.of(1,2,3,4,5).forEach( (a) -> assertEquals(a,age.apply(r)));
+            assertEquals(false, r.hasNext());
+        });
+    }
     @Test
     public void testOrderedRangeNumbers() throws Exception {
         TestUtil.testResult(db, "CALL apoc.index.orderedRange('Person','age',10,30,false,10)", r -> {
@@ -105,4 +121,22 @@ public class SchemaIndexTest {
             assertEquals(false, r.hasNext());
         });
     }
+
+    @Test
+    public void testDistinctProperties() throws Exception {
+        db.execute("CREATE INDEX ON :Foo(bar)").close();
+        db.execute("CREATE (f:Foo {bar:'one'}), (f2a:Foo {bar:'two'}), (f2b:Foo {bar:'two'})").close();
+        String label = "Foo";
+        String key = "bar";
+        try (Transaction tx = db.beginTx()) {
+            db.schema().awaitIndexesOnline(2, TimeUnit.SECONDS);
+            tx.success();
+        }
+
+        testCall(db,"CALL apoc.schema.properties.distinct({label}, {key})",
+                map("label",label,"key",key),
+                (row) -> assertEquals(asList("one","two"), row.get("value"))
+        );
+    }
+
 }
