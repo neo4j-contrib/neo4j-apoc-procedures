ec48d500d97a3dd9d736d239eb850084a06c45fa
Michael Hunger <github@jexp.de>
2016-10-28 18:11:20 +0200

    fixes bug in periodic execution that made subtasks execute in main-thread, polluting the tx-state of the main thread

--- src/main/java/apoc/Pools.java
+++ src/main/java/apoc/Pools.java
@@ -22,7 +22,27 @@ public class Pools {
         int threads = Runtime.getRuntime().availableProcessors()*2;
         int queueSize = threads * 25;
         return new ThreadPoolExecutor(threads / 2, threads, 30L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(queueSize),
-                new ThreadPoolExecutor.CallerRunsPolicy());
+                new CallerBlocksPolicy());
+//                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+    static class CallerBlocksPolicy implements RejectedExecutionHandler {
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            if (!executor.isShutdown()) {
+                try {
+                    // block caller for 100ms
+                    Thread.sleep(100);
+                } catch (InterruptedException ie) {
+                    // ignore
+                }
+                try {
+                    // submit again
+                    executor.submit(r).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static int getNoThreadsInDefaultPool() {
