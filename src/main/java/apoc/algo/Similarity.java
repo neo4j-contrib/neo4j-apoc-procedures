971c2e269b323f9b92ae9c95bc27dd462e4ad758
InverseFalcon <inverse.falcon@gmail.com>
2017-02-24 22:06:55 -0800

    Changed to use double values ands only perform a single pass through the relationship list.

--- src/main/java/apoc/algo/Similarity.java
+++ src/main/java/apoc/algo/Similarity.java
@@ -8,7 +8,6 @@ import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,35 +16,31 @@ public class Similarity {
     public GraphDatabaseService db;
 
     @UserFunction
-    @Description("apoc.algo.cosineSimilarity([[rel1A, rel1B], [rel2A, rel2B], …], weightPropertyName, defaultWeight: 0) " +
+    @Description("apoc.algo.cosineSimilarity([[rel1A, rel1B], [rel2A, rel2B], …], weightPropertyName, defaultWeight: 0.0) " +
             "given a collection of collected pairs of relationships, the weight property of the relationships, " +
             "and a default weight (if relationship or property is null), calculate cosine similarity")
-    public Double cosineSimilarity(
+    public double cosineSimilarity(
             @Name("relPairs") List<List<Relationship>> relPairs,
             @Name("weightPropertyName") String weightPropertyName,
-            @Name(value = "defaultWeight", defaultValue = "0.0") Double defaultWeight) {
+            @Name(value = "defaultWeight", defaultValue = "0.0d") double defaultWeight) {
 
-        List<Double> weight1 = new ArrayList<>();
-        List<Double> weight2 = new ArrayList<>();
+        double dotProduct = 0d;
+        double xLength = 0d;
+        double yLength = 0d;
+        for (int i = 0; i < relPairs.size(); i++) {
+            Relationship rel1 = relPairs.get(i).get(0);
+            double weight1 = (rel1 == null ? defaultWeight : Util.doubleValue(rel1, weightPropertyName, defaultWeight));
+            Relationship rel2 = relPairs.get(i).get(1);
+            double weight2 = (rel2 == null ? defaultWeight : Util.doubleValue(rel2, weightPropertyName, defaultWeight));
 
-        relPairs.stream().forEach(relationships -> {
-            Relationship rel = relationships.get(0);
-            weight1.add(rel == null ? defaultWeight : Util.doubleValue(rel, weightPropertyName, defaultWeight));
-
-            rel = relationships.get(1);
-            weight2.add(rel == null ? defaultWeight : Util.doubleValue(rel, weightPropertyName, defaultWeight));
-        });
-
-        Double dotProduct = 0d;
-        for (int i = 0, sum = 0; i < weight1.size(); i++) {
-            dotProduct += weight1.get(i) * weight2.get(i);
+            dotProduct += weight1 * weight2;
+            xLength += weight1 * weight1;
+            yLength += weight2 * weight2;
         }
 
-        Double xLength = Math.sqrt(weight1.stream().reduce(0.0d, (runningTotal, nextWeight) -> runningTotal + Math.pow(nextWeight, 2)));
-        Double yLength = Math.sqrt(weight2.stream().reduce(0.0d, (runningTotal, nextWeight) -> runningTotal + Math.pow(nextWeight, 2)));
+        xLength = Math.sqrt(xLength);
+        yLength = Math.sqrt(yLength);
 
-        Double similarity = dotProduct / (xLength * yLength);
-
-        return similarity;
+        return dotProduct / (xLength * yLength);
     }
 }
