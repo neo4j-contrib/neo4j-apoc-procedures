b072fb7900e1b0ba7daca966e27f551f62a2b548
InverseFalcon <inverse.falcon@gmail.com>
2017-03-13 04:52:24 -0700

    Changed cosineSimilarity() to only operate on two collection parameters rather than relationships.
    Also added euclideanDistance() and euclideanSimilarity().

--- src/main/java/apoc/algo/Similarity.java
+++ src/main/java/apoc/algo/Similarity.java
@@ -1,8 +1,6 @@
 package apoc.algo;
 
-import apoc.util.Util;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Relationship;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
@@ -16,22 +14,19 @@ public class Similarity {
     public GraphDatabaseService db;
 
     @UserFunction
-    @Description("apoc.algo.cosineSimilarity([[rel1A, rel1B], [rel2A, rel2B], …], weightPropertyName, defaultWeight: 0.0) " +
-            "given a collection of collected pairs of relationships, the weight property of the relationships, " +
-            "and a default weight (if relationship or property is null), calculate cosine similarity")
-    public double cosineSimilarity(
-            @Name("relPairs") List<List<Relationship>> relPairs,
-            @Name("weightPropertyName") String weightPropertyName,
-            @Name(value = "defaultWeight", defaultValue = "0.0d") double defaultWeight) {
+    @Description("apoc.algo.cosineSimilarity([vector1], [vector2]) " +
+            "given two collection vectors, calculate cosine similarity")
+    public double cosineSimilarity(@Name("vector1") List<Number> vector1, @Name("vector2") List<Number> vector2) {
+        if (vector1.size() != vector2.size() || vector1.size() == 0) {
+            throw new RuntimeException("Vectors must be non-empty and of the same size");
+        }
 
         double dotProduct = 0d;
         double xLength = 0d;
         double yLength = 0d;
-        for (int i = 0; i < relPairs.size(); i++) {
-            Relationship rel1 = relPairs.get(i).get(0);
-            double weight1 = (rel1 == null ? defaultWeight : Util.doubleValue(rel1, weightPropertyName, defaultWeight));
-            Relationship rel2 = relPairs.get(i).get(1);
-            double weight2 = (rel2 == null ? defaultWeight : Util.doubleValue(rel2, weightPropertyName, defaultWeight));
+        for (int i = 0; i < vector1.size(); i++) {
+            double weight1 = vector1.get(i).doubleValue();
+            double weight2 = vector2.get(i).doubleValue();
 
             dotProduct += weight1 * weight2;
             xLength += weight1 * weight1;
@@ -42,5 +37,31 @@ public class Similarity {
         yLength = Math.sqrt(yLength);
 
         return dotProduct / (xLength * yLength);
+    }
+
+    @UserFunction
+    @Description("apoc.algo.euclideanDistance([vector1], [vector2]) " +
+            "given two collection vectors, calculate the euclidean distance (square root of the sum of the squared differences)")
+    public double euclideanDistance(@Name("vector1") List<Number> vector1, @Name("vector2") List<Number> vector2) {
+        if (vector1.size() != vector2.size() || vector1.size() == 0) {
+            throw new RuntimeException("Vectors must be non-empty and of the same size");
+        }
+
+        double distance = 0.0;
+        for (int i = 0; i < vector1.size(); i++) {
+            double sqOfDiff = vector1.get(i).doubleValue() - vector2.get(i).doubleValue();
+            sqOfDiff *= sqOfDiff;
+            distance += sqOfDiff;
+        }
+        distance = Math.sqrt(distance);
+
+        return distance;
+    }
+
+    @UserFunction
+    @Description("apoc.algo.euclideanSimilarity([vector1], [vector2]) " +
+            "given two collection vectors, calculate similarity based on euclidean distance")
+    public double euclideanSimilarity(@Name("vector1") List<Number> vector1, @Name("vector2") List<Number> vector2) {
+        return 1.0d / (1 + euclideanDistance(vector1, vector2));
     }
 }
--- src/test/java/apoc/algo/SimilarityTest.java
+++ src/test/java/apoc/algo/SimilarityTest.java
@@ -6,17 +6,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import static apoc.util.TestUtil.testResult;
 import static org.junit.Assert.assertEquals;
 
-
 public class SimilarityTest {
-    private static final String SETUP = "create (java:Personal_Skill{name:'Java'})\n" +
-            "create (neo4j:Personal_Skill{name:'Neo4j'})\n" +
-            "create (nodejs:Personal_Skill{name:'NodeJS'})\n" +
-            "create (scala:Personal_Skill{name:'Scala'})\n" +
+    private static final String SETUP = "create (java:Skill{name:'Java'})\n" +
+            "create (neo4j:Skill{name:'Neo4j'})\n" +
+            "create (nodejs:Skill{name:'NodeJS'})\n" +
+            "create (scala:Skill{name:'Scala'})\n" +
             "create (jim:Employee{name:'Jim'})\n" +
             "create (bob:Employee{name:'Bob'})\n" +
             "create (role:Role {name:'Role 1-Analytics Manager'})\n" +
@@ -31,10 +32,13 @@ public class SimilarityTest {
             "create (jim)-[:HAS_SKILL{proficiency:8.25}]->(java)\n" +
             "create (jim)-[:HAS_SKILL{proficiency:7.1}]->(scala)";
 
-    private static final String BOB_SIMILARITY_AGAINST_MATCHED_SKILLS = "0.9287";
-    private static final String JIM_SIMILARITY_AGAINST_MATCHED_SKILLS = "0.9703";
-    private static final String BOB_SIMILARITY_AGAINST_REQUIRED_SKILLS = "0.9287";
-    private static final String JIM_SIMILARITY_AGAINST_REQUIRED_SKILLS = "0.6794";
+    // "against matched skills" means we only consider skills present via a relationship from both sides
+    // "against required skills" matches first on required skills, and then does an optional match on employee skills
+    // so this takes into account skills the employee doesn't have at all on the graph
+
+    // cosine similarity taken from here: https://neo4j.com/graphgist/a7c915c8-a3d6-43b9-8127-1836fecc6e2f
+    // euclid distance taken from here: https://neo4j.com/blog/real-time-recommendation-engine-data-science/
+    // euclid similarity taken from here: http://stats.stackexchange.com/a/158285
 
     private GraphDatabaseService db;
 
@@ -52,32 +56,130 @@ public class SimilarityTest {
     }
 
     @Test
-    public void testSimilarityWithSomeWeightPropertiesNull() throws Exception {
+    public void testCosineSimilarityWithSomeWeightPropertiesNull() throws Exception {
+        String controlQuery =
+                "MATCH (p1:Employee)-[x:HAS_SKILL]->(sk:Skill)<-[y:REQUIRES_SKILL] -(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                "WITH SUM(x.proficiency * y.proficiency) AS xyDotProduct,\n" +
+                "SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.proficiency) | xDot + a^2)) AS xLength,\n" +
+                "SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.proficiency) | yDot + b^2)) AS yLength,\n" +
+                "p1, p2\n" +
+                "WITH  p1.name as name, xyDotProduct / (xLength * yLength) as cosineSim\n" +
+                "ORDER BY name ASC\n" +
+                "RETURN name, apoc.number.format(cosineSim, '0.####') as cosineSim";
+        String bobSimilarity;
+        String jimSimilarity;
+        try (Transaction tx = db.beginTx()) {
+            Result result = db.execute(controlQuery);
+            bobSimilarity = (String) result.next().get("cosineSim");
+            jimSimilarity = (String) result.next().get("cosineSim");
+        }
+
         testResult(db,
-                "MATCH (p1:Employee)-[x:HAS_SKILL]->(sk:Personal_Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
-                     "WITH p1, COLLECT([x, y]) as skills\n" +
-                     "WITH p1.name as name, apoc.algo.cosineSimilarity(skills, 'proficiency', 0.0) as cosineSim ORDER BY name ASC\n" +
+                "MATCH (p1:Employee)-[x:HAS_SKILL]->(sk:Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                     "WITH p1, COLLECT(coalesce(x.proficiency, 0.0d)) as v1, COLLECT(coalesce(y.proficiency, 0.0d)) as v2\n" +
+                     "WITH p1.name as name, apoc.algo.cosineSimilarity(v1, v2) as cosineSim ORDER BY name ASC\n" +
                      "RETURN name, apoc.number.format(cosineSim, '0.####') as cosineSim",
                 result -> {
-                    assertEquals(BOB_SIMILARITY_AGAINST_MATCHED_SKILLS, result.next().get("cosineSim"));
-                    assertEquals(JIM_SIMILARITY_AGAINST_MATCHED_SKILLS, result.next().get("cosineSim"));
+                    assertEquals(bobSimilarity, result.next().get("cosineSim"));
+                    assertEquals(jimSimilarity, result.next().get("cosineSim"));
                 }
         );
-
     }
 
     @Test
-    public void testSimilarityWithSomeRelationshipsNull() throws Exception {
+    public void testCosineSimilarityWithSomeRelationshipsNull() throws Exception {
+        String controlQuery =
+                "MATCH (p1:Employee)\n" +
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL] -(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
+                "WITH SUM(x.proficiency * y.proficiency) AS xyDotProduct,\n" +
+                "SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.proficiency) | xDot + a^2)) AS xLength,\n" +
+                "SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.proficiency) | yDot + b^2)) AS yLength,\n" +
+                "p1, p2\n" +
+                "WITH  p1.name as name, xyDotProduct / (xLength * yLength) as cosineSim\n" +
+                "ORDER BY name ASC\n" +
+                "RETURN name, apoc.number.format(cosineSim, '0.####') as cosineSim";
+        String bobSimilarity;
+        String jimSimilarity;
+        try (Transaction tx = db.beginTx()) {
+            Result result = db.execute(controlQuery);
+            bobSimilarity = (String) result.next().get("cosineSim");
+            jimSimilarity = (String) result.next().get("cosineSim");
+        }
+
         testResult(db,
-                "MATCH (sk:Personal_Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
                      "MATCH (p1:Employee)\n" +
                      "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
-                     "WITH p1, COLLECT([x, y]) as skillLevel\n" +
-                     "WITH p1.name as name, apoc.algo.cosineSimilarity(skillLevel, 'proficiency', 0.0) as cosineSim ORDER BY name ASC\n" +
+                     "WITH p1, COLLECT(coalesce(x.proficiency, 0.0d)) as v1, COLLECT(coalesce(y.proficiency, 0.0d)) as v2\n" +
+                     "WITH p1.name as name, apoc.algo.cosineSimilarity(v1, v2) as cosineSim ORDER BY name ASC\n" +
                      "RETURN name, apoc.number.format(cosineSim, '0.####') as cosineSim",
                 result -> {
-                    assertEquals(BOB_SIMILARITY_AGAINST_REQUIRED_SKILLS, result.next().get("cosineSim"));
-                    assertEquals(JIM_SIMILARITY_AGAINST_REQUIRED_SKILLS, result.next().get("cosineSim"));
+                    assertEquals(bobSimilarity, result.next().get("cosineSim"));
+                    assertEquals(jimSimilarity, result.next().get("cosineSim"));
+                }
+        );
+    }
+
+    @Test
+    public void testEuclideanDistance() throws Exception {
+        String controlQuery =
+                "MATCH (p1:Employee)\n" +
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL] -(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
+                "WITH SQRT(SUM((coalesce(x.proficiency,0) - coalesce(y.proficiency, 0))^2)) AS euclidDist, p1, p2\n" +
+                "ORDER BY p1.name ASC\n" +
+                "RETURN p1.name, apoc.number.format(euclidDist, '0.####') as euclidDist";
+        String bobDist;
+        String jimDist;
+        try (Transaction tx = db.beginTx()) {
+            Result result = db.execute(controlQuery);
+            bobDist = (String) result.next().get("euclidDist");
+            jimDist = (String) result.next().get("euclidDist");
+        }
+
+        testResult(db,
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                        "MATCH (p1:Employee)\n" +
+                        "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
+                        "WITH p1, COLLECT(coalesce(x.proficiency, 0.0d)) as v1, COLLECT(coalesce(y.proficiency, 0.0d)) as v2\n" +
+                        "WITH p1.name as name, apoc.algo.euclideanDistance(v1, v2) as euclidDist ORDER BY name ASC\n" +
+                        "RETURN name, apoc.number.format(euclidDist, '0.####') as euclidDist",
+                result -> {
+                    assertEquals(bobDist, result.next().get("euclidDist"));
+                    assertEquals(jimDist, result.next().get("euclidDist"));
+                }
+        );
+    }
+
+    @Test
+    public void testEuclideanSimilarity() throws Exception {
+        String controlQuery =
+                "MATCH (p1:Employee)\n" +
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL] -(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
+                "WITH SQRT(SUM((coalesce(x.proficiency,0) - coalesce(y.proficiency, 0))^2)) AS euclidDist, p1\n" +
+                "WITH p1.name as name, 1 / (1 + euclidDist) as euclidSim\n" +
+                "ORDER BY name ASC\n" +
+                "RETURN name, apoc.number.format(euclidSim, '0.####') as euclidSim";
+        String bobSim;
+        String jimSim;
+        try (Transaction tx = db.beginTx()) {
+            Result result = db.execute(controlQuery);
+            bobSim = (String) result.next().get("euclidSim");
+            jimSim = (String) result.next().get("euclidSim");
+        }
+
+        testResult(db,
+                "MATCH (sk:Skill)<-[y:REQUIRES_SKILL]-(p2:Role {name:'Role 1-Analytics Manager'})\n" +
+                        "MATCH (p1:Employee)\n" +
+                        "OPTIONAL MATCH (p1)-[x:HAS_SKILL]->(sk)\n" +
+                        "WITH p1, COLLECT(coalesce(x.proficiency, 0.0d)) as v1, COLLECT(coalesce(y.proficiency, 0.0d)) as v2\n" +
+                        "WITH p1.name as name, apoc.algo.euclideanSimilarity(v1, v2) as euclidSim ORDER BY name ASC\n" +
+                        "RETURN name, apoc.number.format(euclidSim, '0.####') as euclidSim",
+                result -> {
+                    assertEquals(bobSim, result.next().get("euclidSim"));
+                    assertEquals(jimSim, result.next().get("euclidSim"));
                 }
         );
     }
