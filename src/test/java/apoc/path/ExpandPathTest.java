604d78f774dbe3c214df3bbc97013c660484c063
InverseFalcon <inverse.falcon@gmail.com>
2017-03-15 01:52:13 -0700

    Added startNodeExempt config parameter to expandConfig() (and all procedures using the config map). Default is false for versions < 3.2.x.x.

--- src/test/java/apoc/path/ExpandPathTest.java
+++ src/test/java/apoc/path/ExpandPathTest.java
@@ -73,4 +73,10 @@ public class ExpandPathTest {
 					assertEquals("Clint Eastwood", path.endNode().getProperty("name"));
 				});
 	}
+
+	@Test
+	public void testExplorePathWithStartNodeExemptIgnoresLabelFilter() throws Throwable {
+		String query = "MATCH (m:Movie {title: 'The Matrix'}) CALL apoc.path.expandConfig(m,{labelFilter:'+Person', maxLevel:2, startNodeExempt:true}) yield path return count(*) as c";
+		TestUtil.testCall(db, query, (row) -> assertEquals(9L,row.get("c")));
+	}
 }
