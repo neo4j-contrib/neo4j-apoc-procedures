e5ec84878f99fdac02eaaf284cc9f2fd91cb18a7
AngeloBusato <angelo040595@gmail.com>
2017-03-21 15:50:46 +0100

    199 - Numeric constants for minimum and maximum value

--- src/main/java/apoc/math/Maths.java
+++ src/main/java/apoc/math/Maths.java
@@ -20,4 +20,54 @@ public class Maths {
         if (value == null) return null;
         return BigDecimal.valueOf(value).setScale((int)precision, RoundingMode.valueOf(mode)).doubleValue();
     }
+
+    @UserFunction
+    @Description("apoc.math.maxLong() | return the maximum value a long can have")
+    public Long maxLong(){
+        return Long.MAX_VALUE;
+    }
+
+    @UserFunction
+    @Description("apoc.math.minLong() | return the minimum value a long can have")
+    public Long minLong(){
+        return Long.MIN_VALUE;
+    }
+
+    @UserFunction
+    @Description("apoc.math.maxDouble() | return the largest positive finite value of type double")
+    public Double maxDouble(){
+        return Double.MAX_VALUE;
+    }
+
+    @UserFunction
+    @Description("apoc.math.minDouble() | return the smallest positive nonzero value of type double")
+    public Double minDouble(){
+        return Double.MIN_VALUE;
+    }
+
+    @UserFunction
+    @Description("apoc.math.maxInt() | return the maximum value an int can have")
+    public Long maxInt(){
+        return Long.valueOf(Integer.MAX_VALUE);
+    }
+
+    @UserFunction
+    @Description("apoc.math.minInt() | return the minimum value an int can have")
+    public Long minInt(){
+        return Long.valueOf(Integer.MIN_VALUE);
+    }
+
+    @UserFunction
+    @Description("apoc.math.maxByte() | return the maximum value an byte can have")
+    public Long maxByte(){
+        return Long.valueOf(Byte.MAX_VALUE);
+    }
+
+    @UserFunction
+    @Description("apoc.math.minByte() | return the minimum value an byte can have")
+    public Long minByte(){
+        return Long.valueOf(Byte.MIN_VALUE);
+    }
+
+
 }
