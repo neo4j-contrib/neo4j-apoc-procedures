bd341e13da3bfea3faed943fbe60d55d20a0b03d
InverseFalcon <inverse.falcon@gmail.com>
2017-03-27 15:44:48 -0700

    Fixed apoc.data.domain() when there are .'s before the @ symbol (#344)
    * Fixed apoc.data.domain() when there are .'s before the @ symbol.
    
    * Optimized imports

--- src/main/java/apoc/data/Extract.java
+++ src/main/java/apoc/data/Extract.java
@@ -1,15 +1,10 @@
 package apoc.data;
 
 import org.neo4j.procedure.Description;
-import apoc.result.StringResult;
 import org.neo4j.procedure.Name;
-import org.neo4j.procedure.Procedure;
 import org.neo4j.procedure.UserFunction;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Extract {
 
@@ -19,8 +14,16 @@ public class Extract {
     @Description("apoc.data.domain('url_or_email_address') YIELD domain - extract the domain name from a url or an email address. If nothing was found, yield null.")
     public String domain(final @Name("url_or_email_address") String value) {
         if (value != null) {
-            for (String part : value.split("[@/<>]")) {
-                if (DOMAIN.matcher(part).matches()) return part;
+            if (value.contains("@")) {
+                String[] tokens = value.split("[@/<>]");
+                for (int i = tokens.length - 1; i >= 0; i--) {
+                    String token = tokens[i];
+                    if (DOMAIN.matcher(token).matches()) return token;
+                }
+            } else {
+                for (String part : value.split("[@/<>]")) {
+                    if (DOMAIN.matcher(part).matches()) return part;
+                }
             }
         }
         return null;
