215c835aa157929db3933d0fed446db6490a29ae
AngeloBusato <angelo040595@gmail.com>
2017-03-28 18:22:49 +0200

    fixes #70 - CALL apoc.load.json path ignores config file setting

--- src/main/java/apoc/export/util/FileUtils.java
+++ src/main/java/apoc/export/util/FileUtils.java
@@ -4,8 +4,10 @@ import apoc.ApocConfiguration;
 import apoc.util.Util;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
+
 /**
  * @author mh
  * @since 22.05.16
@@ -15,12 +17,40 @@ public class FileUtils {
     public static CountingReader readerFor(String fileName) throws IOException {
         checkReadAllowed(fileName);
         if (fileName==null) return null;
+        fileName= changeFileUrlIfImportDirectoryConstrained(fileName);
         if (fileName.matches("^\\w+:/.+")) {
             return Util.openInputStream(fileName,null,null).asReader();
         }
         File file = new File(fileName);
         if (!file.exists() || !file.isFile() || !file.canRead()) throw new IOException("Cannot open file "+fileName+" for reading.");
         return new CountingReader(file);
+    }
+
+    public static String changeFileUrlIfImportDirectoryConstrained(String url) throws IOException {
+        if (isFile(url) && ApocConfiguration.isEnabled("import.file.use_neo4j_config")) {
+            if (!ApocConfiguration.isEnabled("import.file.allow_read_from_filesystem"))
+                throw new RuntimeException("Import file "+url+" not enabled, please set dbms.security.allow_csv_import_from_file_urls=true in your neo4j.conf");
+            String importDir = ApocConfiguration.get("import.file.directory", null);
+            if (importDir != null && !importDir.isEmpty()) {
+                try {
+                    File dir = new File(importDir);
+                    if (!dir.exists() || !dir.isDirectory() || !dir.canRead()) throw new Exception();
+                    String fileAsUrl = null;
+                    try { fileAsUrl = new URI(url).getPath(); } catch (Exception e) { /*no protocol*/ }
+                    if ("".equals(fileAsUrl)){
+                        String newUrl = url.replace("file://", "file:///");
+                        fileAsUrl = new URI(newUrl).getPath();
+                        if ("".equals(fileAsUrl)) fileAsUrl=newUrl.replace("file://","");
+                    }
+                    File file = url.startsWith(importDir) ? new File(fileAsUrl) : new File(dir.getAbsolutePath(), fileAsUrl);
+                    if (!file.exists() || !file.isFile() || !file.canRead()) throw new Exception();
+                    return new URL("file","",file.getAbsolutePath()).toString();
+                } catch (Exception e){
+                    throw new IOException("Cannot open file "+url+" from directory "+importDir+" for reading.");
+                }
+            }
+        }
+        return url;
     }
 
     public static boolean isFile(String fileName) {
--- src/test/java/apoc/config/ConfigTest.java
+++ src/test/java/apoc/config/ConfigTest.java
@@ -1,11 +1,17 @@
 package apoc.config;
 
+import apoc.ApocConfiguration;
+import apoc.export.util.FileUtils;
 import apoc.util.TestUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.test.TestGraphDatabaseFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.Assert.*;
 
@@ -16,13 +22,18 @@ import static org.junit.Assert.*;
 public class ConfigTest {
 
     private GraphDatabaseService db;
+    private File testFile;
 
     @Before
     public void setUp() throws Exception {
         db = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder()
+                .setConfig("apoc.import.file.use_neo4j_config", "true")
+                .setConfig("dbms.directories.import", "build/resources/test")
+                .setConfig("dbms.security.allow_csv_import_from_file_urls","true")
                 .setConfig("foo", "bar").setConfig("foouri", "baruri")
                 .setConfig("foopass", "foopass").setConfig("foo.credentials", "foo.credentials").newGraphDatabase();
         TestUtil.registerProcedure(db, Config.class);
+        testFile = new File(ClassLoader.getSystemResource("test.csv").getPath());
     }
 
     @After
@@ -33,6 +44,42 @@ public class ConfigTest {
     @Test
     public void list() throws Exception {
         TestUtil.testCall(db, "CALL apoc.config.list() yield key with * where key STARTS WITH 'foo' RETURN *",(row) -> assertEquals("foo",row.get("key")));
+    }
+
+    @Test
+    public void getConfigurationValue(){
+        assertEquals(ApocConfiguration.list().get("foo"),"bar");
+    }
+
+    @Test
+    public void changeNoSlashesUrlWithDirectoryImportContrained() throws Exception {
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("test.csv"));
+    }
+
+    @Test
+    public void changeSlashUrlWithDirectoryImportContrained() throws Exception {
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("/test.csv"));
+    }
+
+    @Test
+    public void changeFileSlashUrlWithDirectoryImportContrained() throws Exception {
+
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("file:/test.csv"));
+    }
+
+    @Test
+    public void changeFileDoubleSlashesUrlWithdirectoryImportConstrained() throws Exception {
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("file://test.csv"));
+    }
+
+    @Test
+    public void changeFileTripleSlashesUrlWithdirectoryImportConstrained() throws Exception {
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("file:///test.csv"));
+    }
+
+    @Test
+    public void notChangeFileUrlWithdirectoryImportConstrained() throws Exception {
+        assertEquals(new URL("file","",testFile.getAbsolutePath()).toString(), FileUtils.changeFileUrlIfImportDirectoryConstrained("build/resources/test/test.csv"));
     }
 
 }
