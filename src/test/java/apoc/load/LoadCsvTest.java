ba37536c3e3e385d100d9b26cef43488062f4926
Daniele Berton <daniele.berton@larus-ba.it>
2017-03-28 13:47:30 +0200

    issue #337 follow redirect for URLs (#342)

--- src/test/java/apoc/load/LoadCsvTest.java
+++ src/test/java/apoc/load/LoadCsvTest.java
@@ -21,13 +21,13 @@ import static org.junit.Assert.assertEquals;
 public class LoadCsvTest {
 
     private GraphDatabaseService db;
-	@Before public void setUp() throws Exception {
-	    db = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder().setConfig("apoc.import.file.enabled","true").newGraphDatabase();
+    @Before public void setUp() throws Exception {
+        db = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder().setConfig("apoc.import.file.enabled","true").newGraphDatabase();
         TestUtil.registerProcedure(db, LoadCsv.class);
     }
 
     @After public void tearDown() {
-	    db.shutdown();
+        db.shutdown();
     }
 
     @Test public void testLoadCsv() throws Exception {
@@ -52,7 +52,7 @@ public class LoadCsvTest {
         URL url = ClassLoader.getSystemResource("test.csv");
         testResult(db, "CALL apoc.load.csv({url},{skip:1,limit:1})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    assertRow(r,"Rana","11",1L);
+                    assertRow(r, "Rana", "11", 1L);
                     assertEquals(false, r.hasNext());
                 });
     }
@@ -60,7 +60,7 @@ public class LoadCsvTest {
         URL url = ClassLoader.getSystemResource("test-tab.csv");
         testResult(db, "CALL apoc.load.csv({url},{sep:'TAB'})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    assertRow(r,"Rana","11",0L);
+                    assertRow(r, "Rana", "11", 0L);
                     assertEquals(false, r.hasNext());
                 });
     }
@@ -102,9 +102,34 @@ public class LoadCsvTest {
         testResult(db, "CALL apoc.load.csv({url},{mapping:{name:{type:'string'},age:{type:'int'},kids:{array:true,arraySep:':',type:'int'},pass:{ignore:true}}})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
                     Map<String, Object> row = r.next();
-                    assertEquals(map("name", "Michael","age", 41L,"kids",asList(8L,11L,18L)), row.get("map"));
-                    assertEquals(asList("Michael", 41L,asList(8L,11L,18L)), row.get("list"));
+                    assertEquals(map("name", "Michael", "age", 41L, "kids", asList(8L, 11L, 18L)), row.get("map"));
+                    assertEquals(asList("Michael", 41L, asList(8L, 11L, 18L)), row.get("list"));
                     assertEquals(0L, row.get("lineNo"));
+                    assertEquals(false, r.hasNext());
+                });
+    }
+
+    @Test
+    public void testLoadCsvByUrl() throws Exception {
+        URL url = new URL("https://raw.githubusercontent.com/neo4j-contrib/neo4j-apoc-procedures/3.1/src/test/resources/test.csv");
+        testResult(db, "CALL apoc.load.csv({url},null)", map("url", url.toString()),
+                (r) -> {
+                    assertRow(r, "Selma", "8", 0L);
+                    assertRow(r, "Rana", "11", 1L);
+                    assertRow(r, "Selina", "18", 2L);
+                    assertEquals(false, r.hasNext());
+                });
+
+    }
+
+    @Test
+    public void testLoadCsvByUrlRedirect() throws Exception {
+        URL url = new URL("http://bit.ly/2nXgHA2");
+        testResult(db, "CALL apoc.load.csv({url},null)", map("url", url.toString()),
+                (r) -> {
+                    assertRow(r, "Selma", "8", 0L);
+                    assertRow(r, "Rana", "11", 1L);
+                    assertRow(r, "Selina", "18", 2L);
                     assertEquals(false, r.hasNext());
                 });
     }
