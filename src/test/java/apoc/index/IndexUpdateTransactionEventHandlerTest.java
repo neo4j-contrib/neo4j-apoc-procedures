d2d5f6f8a77f4fab49dab56fa914805c908db2e9
Stefan Armbruster <stefan@armbruster-it.de>
2017-04-01 10:35:38 +0200

    fixes #348 index update tx handler uses correct lucene field names

--- src/test/java/apoc/index/IndexUpdateTransactionEventHandlerTest.java
+++ src/test/java/apoc/index/IndexUpdateTransactionEventHandlerTest.java
@@ -5,7 +5,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import static apoc.util.TestUtil.*;
@@ -17,8 +16,9 @@ public class IndexUpdateTransactionEventHandlerTest {
 
     @Before
     public void setUp() throws Exception {
-        db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-        db.registerTransactionEventHandler(new IndexUpdateTransactionEventHandler((GraphDatabaseAPI) db, false));
+        db = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder()
+            .setConfig("apoc.autoIndex.enabled", "true")
+            .newGraphDatabase();
         TestUtil.registerProcedure(db, FreeTextSearch.class);
     }
 
@@ -34,13 +34,32 @@ public class IndexUpdateTransactionEventHandlerTest {
         testCallEmpty(db, "create (c:City{name:\"Made Up City\",url:\"/places/nowhere/made-up-city\"})", null);
 
         // check if we find the node
-        testCallCount(db, "start n=node:search_index('name:\"Made Up\"') return n", null, 1);
+        testCallCount(db, "start n=node:search_index('City.name:\"Made Up\"') return n", null, 1);
 
         // when
         TestUtil.testCall(db, "match (c:City{name:'Made Up City'}) delete c return count(c) as count", map -> assertEquals(1L, map.get("count")));
 
         // nothing found in the index after deletion
-        testCallCount(db, "start n=node:search_index('name:\"Made Up\"') return n", null, 0);
+        testCallCount(db, "start n=node:search_index('City.name:\"Made Up\"') return n", null, 0);
 
     }
+
+    @Test
+    public void shouldIndexFieldsBeUsedConsistently() throws InterruptedException {
+        // setup: add a node, index it and add another node
+        testCallEmpty(db, "create (c:City{name:\"Made Up City\",url:\"/places/nowhere/made-up-city\"})", null);
+
+        testCallCount(db, "call apoc.index.addAllNodesExtended('search_index',{City:['name']},{autoUpdate:true})", null, 1);
+
+        testCallCount(db, "create (c:City{name:\"Made Up City 2\",url:\"/places/nowhere/made-up-city\"}) return c",
+                null, 1);
+
+        // when & then
+        testCallCount(db, "call apoc.index.search('search_index', 'City.name:Made') yield node, weight return node, weight", null, 2);
+        testCallCount(db, "start n=node:search_index('name:\"Made Up\"') return n", null, 0);
+        testCallCount(db, "start n=node:search_index('City.name:\"Made Up\"') return n", null, 2);
+
+    }
+
+
 }
