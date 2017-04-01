9b3d65aeb68b064c6e44c5156de2d487d0086e7c
tomasonjo <bratanic.tomaz@gmail.com>
2017-04-01 17:37:04 +0200

    GephiWeightedRelationships (#335)
    Added an optional `weight` property parameter, so you can pick which property do you want to use as a weight or set it at null for standard 1.0 weight

--- src/test/java/apoc/gephi/GephiTest.java
+++ src/test/java/apoc/gephi/GephiTest.java
@@ -24,17 +24,36 @@ public class GephiTest {
     public static void setUp() throws Exception {
         db = new TestGraphDatabaseFactory().newImpermanentDatabase();
         TestUtil.registerProcedure(db,Gephi.class);
-        db.execute("CREATE (f:Foo {name:'Foo'})-[:KNOWS]->(b:Bar {name:'Bar'})").close();
+        db.execute("CREATE (:Foo {name:'Foo'})-[:KNOWS{weight:7.2}]->(:Bar {name:'Bar'})").close();
     }
     @AfterClass
     public static void tearDown() {
         db.shutdown();
     }
-
     @Test
     public void testAdd() throws Exception {
         TestUtil.ignoreException(() -> {
             TestUtil.testCall(db, "MATCH p = (:Foo)-->() WITH p CALL apoc.gephi.add(null,'workspace1',p) yield nodes, relationships, format return *", r -> {
+                assertEquals(2L, r.get("nodes"));
+                assertEquals(1L, r.get("relationships"));
+                assertEquals("gephi", r.get("format"));
+            });
+        }, java.io.FileNotFoundException.class, java.net.ConnectException.class, IOException.class);
+    }
+    @Test
+    public void testWeightParameter() throws Exception {
+        TestUtil.ignoreException(() -> {
+            TestUtil.testCall(db, "MATCH p = (:Foo)-->() WITH p CALL apoc.gephi.add(null,'workspace1',p,'weight') yield nodes, relationships, format return *", r -> {
+                assertEquals(2L, r.get("nodes"));
+                assertEquals(1L, r.get("relationships"));
+                assertEquals("gephi", r.get("format"));
+            });
+        }, java.io.FileNotFoundException.class, java.net.ConnectException.class, IOException.class);
+    }
+    @Test
+    public void testWrongWeightParameter() throws Exception {
+        TestUtil.ignoreException(() -> {
+            TestUtil.testCall(db, "MATCH p = (:Foo)-->() WITH p CALL apoc.gephi.add(null,'workspace1',p,'test') yield nodes, relationships, format return *", r -> {
                 assertEquals(2L, r.get("nodes"));
                 assertEquals(1L, r.get("relationships"));
                 assertEquals("gephi", r.get("format"));
