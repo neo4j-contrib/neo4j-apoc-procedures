8de7695572ee6ec83b927b15161663654f2734b3
Michael Hunger <github@jexp.de>
2017-05-14 10:16:35 +0200

    property record loading for warmup
    	src/main/java/apoc/warmup/Warmup.java

--- src/main/java/apoc/warmup/Warmup.java
+++ src/main/java/apoc/warmup/Warmup.java
@@ -1,21 +1,23 @@
 package apoc.warmup;
 
 import apoc.util.Util;
-import org.neo4j.cursor.Cursor;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.kernel.api.ReadOperations;
-import org.neo4j.kernel.impl.core.ThreadToStatementContextBridge;
 import org.neo4j.kernel.impl.storageengine.impl.recordstorage.RecordStorageEngine;
 import org.neo4j.kernel.impl.store.NeoStores;
+import org.neo4j.kernel.impl.store.RecordStore;
 import org.neo4j.kernel.impl.store.StoreAccess;
 import org.neo4j.kernel.impl.store.format.standard.NodeRecordFormat;
+import org.neo4j.kernel.impl.store.format.standard.PropertyRecordFormat;
 import org.neo4j.kernel.impl.store.format.standard.RelationshipRecordFormat;
+import org.neo4j.kernel.impl.store.record.AbstractBaseRecord;
+import org.neo4j.kernel.impl.store.record.NodeRecord;
+import org.neo4j.kernel.impl.store.record.PropertyRecord;
+import org.neo4j.kernel.impl.store.record.RelationshipRecord;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
-import org.neo4j.storageengine.api.NodeItem;
-import org.neo4j.storageengine.api.RelationshipItem;
 
 import java.util.stream.Stream;
 
@@ -30,64 +32,40 @@ public class Warmup {
     @Context
     public GraphDatabaseAPI db;
 
-    public Warmup(GraphDatabaseAPI db) {
-        this.db = db;
-    }
-
-    public Warmup() {
-    }
-
-    private static void loadNode(long id, ReadOperations rOps) {
-        try (Cursor<NodeItem> c = rOps.nodeCursor(id)) {
-            c.next();
-        }
-    }
-
-    private static void loadRelationship(long id, ReadOperations rOps) {
-        try (Cursor<RelationshipItem> c = rOps.relationshipCursor(id)) {
-            c.next();
-        }
-    }
-
     @Procedure
     @Description("apoc.warmup.run() - quickly loads all nodes and rels into memory by skipping one page at a time")
-    public Stream<WarmupResult> run() {
+    public Stream<WarmupResult> run(@Name(value = "loadProperties",defaultValue = "false") boolean loadProperties) {
 
         int pageSize = 1 << 13; // default page size, todo read from config
         int nodesPerPage = pageSize / NodeRecordFormat.RECORD_SIZE;
         int relsPerPage = pageSize / RelationshipRecordFormat.RECORD_SIZE;
+        int propsPerPage = pageSize / PropertyRecordFormat.RECORD_SIZE;
         long nodesTotal = Util.nodeCount(db);
         long relsTotal = Util.relCount(db);
-        long nodePages = 0, relPages = 0;
-        long start, timeNodes, timeRels;
+        long nodePages = 0, relPages = 0, propPages = 0;
+        long start, timeNodes, timeRels, timeProps;
 
         StoreAccess storeAccess = new StoreAccess(db.getDependencyResolver()
                 .resolveDependency(RecordStorageEngine.class).testAccessNeoStores());
         NeoStores neoStore = storeAccess.getRawNeoStores();
-        long highestNodeKey = neoStore.getNodeStore().getHighestPossibleIdInUse();
-        long highestRelationshipKey = neoStore.getRelationshipStore().getHighestPossibleIdInUse();
+        long highestNodeId = neoStore.getNodeStore().getHighestPossibleIdInUse();
+        long highestRelationshipId = neoStore.getRelationshipStore().getHighestPossibleIdInUse();
+        long highestPropertyId = neoStore.getPropertyStore().getHighestPossibleIdInUse();
+        long propRecordsTotal = highestPropertyId+1;
 
         try (Transaction tx = db.beginTx()) {
-            ThreadToStatementContextBridge ctx = db.getDependencyResolver().resolveDependency(ThreadToStatementContextBridge.class);
-            // If the transaction is terminated just return an empty stream
-            if (Util.transactionIsTerminated(db)) {
-                return Stream.empty();
-            }
-
-            ReadOperations readOperations = ctx.get().readOperations();
-
-            // Load specific nodes and rels
             start = System.nanoTime();
-            for (int i = 0; i <= highestNodeKey; i += nodesPerPage) {
-                nodePages++;
-                loadNode(i, readOperations);
-            }
+            nodePages = loadRecords(nodesPerPage, highestNodeId, neoStore.getNodeStore(), new NodeRecord(-1));
             timeNodes = System.nanoTime();
-            for (int i = 0; i <= highestRelationshipKey; i += relsPerPage) {
-                relPages++;
-                loadRelationship(i, readOperations);
-            }
+
+            relPages = loadRecords(relsPerPage, highestRelationshipId, neoStore.getRelationshipStore(), new RelationshipRecord(-1));
             timeRels = System.nanoTime();
+
+            timeProps = timeRels;
+            if (loadProperties) {
+                propPages = loadRecords(propsPerPage, highestPropertyId, neoStore.getPropertyStore(), new PropertyRecord(-1));
+                timeProps = System.nanoTime();
+            }
             tx.success();
         }
 
@@ -95,8 +73,24 @@ public class Warmup {
                 pageSize,
                 nodesPerPage, nodesTotal, nodePages, NANOSECONDS.toSeconds(timeNodes - start),
                 relsPerPage, relsTotal, relPages, NANOSECONDS.toSeconds(timeRels - timeNodes),
-                NANOSECONDS.toSeconds(timeRels - start));
+                loadProperties, propsPerPage, propRecordsTotal, propPages, NANOSECONDS.toSeconds(timeProps - timeRels),
+                NANOSECONDS.toSeconds(timeProps - start),
+                Util.transactionIsTerminated(db));
         return Stream.of(result);
+    }
+
+    public <R extends AbstractBaseRecord> long loadRecords(int recordsPerPage, long highestRecordId, RecordStore<R> recordStore, R record) {
+        long pages = 0;
+        for (int id = 0; id <= highestRecordId; id += recordsPerPage) {
+            if (pages % 100_000 == 0) {
+                if (Util.transactionIsTerminated(db)) return pages;
+            }
+            pages++;
+            record.setId(id);
+            record.clear();
+            recordStore.ensureHeavy(record);
+        }
+        return pages;
     }
 
     public static class WarmupResult {
@@ -110,9 +104,20 @@ public class Warmup {
         public final long relsTotal;
         public final long relPages;
         public final long relsTime;
+        public final boolean propertiesLoaded;
+        public final long propsPerPage;
+        public final long propRecordsTotal;
+        public final long propPages;
+        public final long propsTime;
         public final long totalTime;
+        public final boolean transactionWasTerminated;
 
-        public WarmupResult(long pageSize, long nodesPerPage, long nodesTotal, long nodePages, long nodesTime, long relsPerPage, long relsTotal, long relPages, long relsTime, long totalTime) {
+        public WarmupResult(long pageSize,
+                            long nodesPerPage, long nodesTotal, long nodePages, long nodesTime,
+                            long relsPerPage, long relsTotal, long relPages, long relsTime,
+                            boolean propertiesLoaded,
+                            long propsPerPage, long propRecordsTotal, long propPages, long propsTime,
+                            long totalTime, boolean transactionWasTerminated) {
             this.pageSize = pageSize;
             this.nodesPerPage = nodesPerPage;
             this.nodesTotal = nodesTotal;
@@ -122,7 +127,13 @@ public class Warmup {
             this.relsTotal = relsTotal;
             this.relPages = relPages;
             this.relsTime = relsTime;
+            this.propertiesLoaded = propertiesLoaded;
+            this.propsPerPage = propsPerPage;
+            this.propRecordsTotal = propRecordsTotal;
+            this.propPages = propPages;
+            this.propsTime = propsTime;
             this.totalTime = totalTime;
+            this.transactionWasTerminated = transactionWasTerminated;
         }
     }
 
--- src/test/java/apoc/warmup/WarmupTest.java
+++ src/test/java/apoc/warmup/WarmupTest.java
@@ -30,7 +30,7 @@ public class WarmupTest {
     @Test
     public void testWarmup() throws Exception {
         // Create enough nodes and relationships to span 2 pages
-        db.execute("UNWIND range(1, 300) AS i CREATE (n)-[:KNOWS]->(m)").close();
+        db.execute("UNWIND range(1, 300) AS i CREATE (n {foo:1})-[:KNOWS {bar:2}]->(m {foobar:3})").close();
         // Delete all relationships and their nodes, but ones with the minimum and maximum relationship ids, so
         // they still span 2 pages
         db.execute("MATCH ()-[r:KNOWS]->() " +
@@ -43,8 +43,17 @@ public class WarmupTest {
         {
             assertEquals(4L, r.get("nodesTotal"));
             assertEquals(2L, r.get("nodePages"));
+            assertEquals(546L, r.get("nodesPerPage"));
             assertEquals(2L, r.get("relsTotal"));
             assertEquals(2L, r.get("relPages"));
+            assertEquals(240L, r.get("relsPerPage"));
+        });
+        TestUtil.testCall(db,"CALL apoc.warmup.run(true)", r ->
+        {
+            assertEquals(true, r.get("propertiesLoaded"));
+            assertEquals(900L, r.get("propRecordsTotal"));
+            assertEquals(199L, r.get("propsPerPage"));
+            assertEquals(5L, r.get("propPages"));
         });
     }
 }
