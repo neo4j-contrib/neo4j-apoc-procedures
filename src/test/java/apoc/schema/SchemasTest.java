40f265b0c146d147941c6a0d3aff813e93a2e6af
InverseFalcon <inverse.falcon@gmail.com>
2017-06-07 17:07:21 -0700

    Refactored apoc.schema.assert() into a proper procedure Also added optional argument 'dropExisting', defaulting to true (so we don't break existing usage), giving users the option to retain their current indexes and unique constraints.

--- src/test/java/apoc/schema/SchemasTest.java
+++ src/test/java/apoc/schema/SchemasTest.java
@@ -14,8 +14,10 @@ import org.neo4j.helpers.collection.Iterables;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.util.List;
+import java.util.Map;
 
 import static apoc.util.TestUtil.testCall;
+import static apoc.util.TestUtil.testResult;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
 import static org.junit.Assert.*;
@@ -30,7 +32,7 @@ public class SchemasTest {
     @Before
     public void setUp() throws Exception {
         db = new TestGraphDatabaseFactory().newImpermanentDatabase();
-//        TestUtil.registerProcedure(db,AssertSchemaProcedure.class);
+        TestUtil.registerProcedure(db, Schemas.class);
     }
 
     @After
@@ -73,7 +75,7 @@ public class SchemasTest {
     }
 
     @Test
-    public void testDropIndex() throws Exception {
+    public void testDropIndexWhenUsingDropExisting() throws Exception {
         db.execute("CREATE INDEX ON :Foo(bar)").close();
         testCall(db,"CALL apoc.schema.assert(null,null)", (r) -> {
             assertEquals("Foo",r.get("label"));
@@ -88,7 +90,57 @@ public class SchemasTest {
     }
 
     @Test
-    public void testDropSchema() throws Exception {
+    public void testDropIndexAndCreateIndexWhenUsingDropExisting() throws Exception {
+        db.execute("CREATE INDEX ON :Foo(bar)").close();
+        testResult(db,"CALL apoc.schema.assert({Bar:['foo']},null)", (result) -> {
+            Map<String, Object> r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("DROPPED",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+        });
+        try (Transaction tx = db.beginTx()) {
+            List<IndexDefinition> indexes = Iterables.asList(db.schema().getIndexes());
+            assertEquals(1, indexes.size());
+        }
+    }
+
+    @Test
+    public void testRetainIndexWhenNotUsingDropExisting() throws Exception {
+        db.execute("CREATE INDEX ON :Foo(bar)").close();
+        testResult(db,"CALL apoc.schema.assert({Bar:['foo', 'bar']}, null, false)", (result) -> {
+            Map<String, Object> r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("KEPT",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+        });
+        try (Transaction tx = db.beginTx()) {
+            List<IndexDefinition> indexes = Iterables.asList(db.schema().getIndexes());
+            assertEquals(3, indexes.size());
+        }
+    }
+
+    @Test
+    public void testDropSchemaWhenUsingDropExisting() throws Exception {
         db.execute("CREATE CONSTRAINT ON (f:Foo) assert f.bar is unique").close();
         testCall(db,"CALL apoc.schema.assert(null,null)", (r) -> {
             assertEquals("Foo",r.get("label"));
@@ -101,33 +153,98 @@ public class SchemasTest {
             assertEquals(0, constraints.size());
         }
     }
+
+    @Test
+    public void testDropSchemaAndCreateSchemaWhenUsingDropExisting() throws Exception {
+        db.execute("CREATE CONSTRAINT ON (f:Foo) assert f.bar is unique").close();
+        testResult(db,"CALL apoc.schema.assert(null, {Bar:['foo']})", (result) -> {
+            Map<String, Object> r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("DROPPED",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+        });
+        try (Transaction tx = db.beginTx()) {
+            List<ConstraintDefinition> constraints = Iterables.asList(db.schema().getConstraints());
+            assertEquals(1, constraints.size());
+        }
+    }
+
+    @Test
+    public void testRetainSchemaWhenNotUsingDropExisting() throws Exception {
+        db.execute("CREATE CONSTRAINT ON (f:Foo) assert f.bar is unique").close();
+        testResult(db,"CALL apoc.schema.assert(null, {Bar:['foo', 'bar']}, false)", (result) -> {
+            Map<String, Object> r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("KEPT",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+
+            r = result.next();
+            assertEquals("Bar",r.get("label"));
+            assertEquals("bar",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
+        });
+        try (Transaction tx = db.beginTx()) {
+            List<ConstraintDefinition> constraints = Iterables.asList(db.schema().getConstraints());
+            assertEquals(3, constraints.size());
+        }
+    }
+
     @Test
     public void testKeepIndex() throws Exception {
         db.execute("CREATE INDEX ON :Foo(bar)").close();
-        testCall(db,"CALL apoc.schema.assert({Foo:['bar']},null)", (r) -> {
+        testResult(db,"CALL apoc.schema.assert({Foo:['bar', 'foo']},null)", (result) -> {
+            Map<String, Object> r = result.next();
             assertEquals("Foo",r.get("label"));
             assertEquals("bar",r.get("key"));
             assertEquals(false,r.get("unique"));
             assertEquals("KEPT",r.get("action"));
+
+            r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(false,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
         });
         try (Transaction tx = db.beginTx()) {
             List<IndexDefinition> indexes = Iterables.asList(db.schema().getIndexes());
-            assertEquals(1, indexes.size());
+            assertEquals(2, indexes.size());
         }
     }
 
     @Test
     public void testKeepSchema() throws Exception {
         db.execute("CREATE CONSTRAINT ON (f:Foo) assert f.bar is unique").close();
-        testCall(db,"CALL apoc.schema.assert(null,{Foo:['bar']})", (r) -> {
+        testResult(db,"CALL apoc.schema.assert(null,{Foo:['bar', 'foo']})", (result) -> {
+            Map<String, Object> r = result.next();
             assertEquals("Foo",r.get("label"));
             assertEquals("bar",r.get("key"));
             assertEquals(true,r.get("unique"));
             assertEquals("KEPT",r.get("action"));
+
+            r = result.next();
+            assertEquals("Foo",r.get("label"));
+            assertEquals("foo",r.get("key"));
+            assertEquals(true,r.get("unique"));
+            assertEquals("CREATED",r.get("action"));
         });
         try (Transaction tx = db.beginTx()) {
             List<ConstraintDefinition> constraints = Iterables.asList(db.schema().getConstraints());
-            assertEquals(1, constraints.size());
+            assertEquals(2, constraints.size());
         }
     }
 }
