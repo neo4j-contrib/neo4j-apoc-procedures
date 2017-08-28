88cd3a1324b3d78a3e57d3b66ce5357d7b1107af
Michael Hunger <github@jexp.de>
2017-08-28 02:38:01 +0200

    Parallelize Warmup

--- src/main/java/apoc/warmup/Warmup.java
+++ src/main/java/apoc/warmup/Warmup.java
@@ -1,7 +1,7 @@
 package apoc.warmup;
 
+import apoc.Pools;
 import apoc.util.Util;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.impl.storageengine.impl.recordstorage.RecordStorageEngine;
 import org.neo4j.kernel.impl.store.NeoStores;
 import org.neo4j.kernel.impl.store.RecordStore;
@@ -11,11 +11,18 @@ import org.neo4j.kernel.impl.store.format.standard.PropertyRecordFormat;
 import org.neo4j.kernel.impl.store.format.standard.RelationshipRecordFormat;
 import org.neo4j.kernel.impl.store.record.*;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -26,12 +33,15 @@ import static java.util.concurrent.TimeUnit.NANOSECONDS;
  */
 public class Warmup {
 
+    private static final int BATCH_SIZE = 100_000;
     @Context
     public GraphDatabaseAPI db;
+    @Context
+    public Log log;
 
     @Procedure
     @Description("apoc.warmup.run() - quickly loads all nodes and rels into memory by skipping one page at a time")
-    public Stream<WarmupResult> run(@Name(value = "loadProperties",defaultValue = "false") boolean loadProperties) {
+    public Stream<WarmupResult> run(@Name(value = "loadProperties", defaultValue = "false") boolean loadProperties) {
 
         int pageSize = 1 << 13; // default page size, todo read from config
         int nodesPerPage = pageSize / NodeRecordFormat.RECORD_SIZE;
@@ -39,8 +49,8 @@ public class Warmup {
         int propsPerPage = pageSize / PropertyRecordFormat.RECORD_SIZE;
         long nodesTotal = Util.nodeCount(db);
         long relsTotal = Util.relCount(db);
-        long nodePages = 0, relPages = 0, propPages = 0;
-        long start, timeNodes, timeRels, timeProps;
+        long nodePages, relPages, propPages = 0;
+        long start, timeNodes, timeRels, timeProps = 0;
 
         StoreAccess storeAccess = new StoreAccess(db.getDependencyResolver()
                 .resolveDependency(RecordStorageEngine.class).testAccessNeoStores());
@@ -48,47 +58,82 @@ public class Warmup {
         long highestNodeId = neoStore.getNodeStore().getHighestPossibleIdInUse();
         long highestRelationshipId = neoStore.getRelationshipStore().getHighestPossibleIdInUse();
         long highestPropertyId = neoStore.getPropertyStore().getHighestPossibleIdInUse();
-        long propRecordsTotal = highestPropertyId+1;
+        long propRecordsTotal = highestPropertyId + 1;
 
-        try (Transaction tx = db.beginTx()) {
+        if (loadProperties) {
             start = System.nanoTime();
-            nodePages = loadRecords(nodesPerPage, highestNodeId, neoStore.getNodeStore(), new NodeRecord(-1));
-            timeNodes = System.nanoTime();
-
-            relPages = loadRecords(relsPerPage, highestRelationshipId, neoStore.getRelationshipStore(), new RelationshipRecord(-1));
-            timeRels = System.nanoTime();
-
-            timeProps = timeRels;
-            if (loadProperties) {
-                propPages = loadRecords(propsPerPage, highestPropertyId, neoStore.getPropertyStore(), new PropertyRecord(-1));
-                timeProps = System.nanoTime();
-            }
-            tx.success();
+            propPages = loadRecords(propsPerPage, highestPropertyId, neoStore.getPropertyStore(), new PropertyRecord(-1));
+            timeProps = System.nanoTime() - start;
         }
+
+        start = System.nanoTime();
+        relPages = loadRecords(relsPerPage, highestRelationshipId, neoStore.getRelationshipStore(), new RelationshipRecord(-1));
+        timeRels = System.nanoTime() - start;
+
+        start = System.nanoTime();
+        nodePages = loadRecords(nodesPerPage, highestNodeId, neoStore.getNodeStore(), new NodeRecord(-1));
+        timeNodes = System.nanoTime() - start;
 
         WarmupResult result = new WarmupResult(
                 pageSize,
-                nodesPerPage, nodesTotal, nodePages, NANOSECONDS.toSeconds(timeNodes - start),
-                relsPerPage, relsTotal, relPages, NANOSECONDS.toSeconds(timeRels - timeNodes),
-                loadProperties, propsPerPage, propRecordsTotal, propPages, NANOSECONDS.toSeconds(timeProps - timeRels),
-                NANOSECONDS.toSeconds(timeProps - start),
+                nodesPerPage, nodesTotal, nodePages, NANOSECONDS.toSeconds(timeNodes),
+                relsPerPage, relsTotal, relPages, NANOSECONDS.toSeconds(timeRels),
+                loadProperties, propsPerPage, propRecordsTotal, propPages, NANOSECONDS.toSeconds(timeProps),
+                NANOSECONDS.toSeconds(timeNodes+timeRels+timeProps),
                 Util.transactionIsTerminated(db));
         return Stream.of(result);
     }
 
     public <R extends AbstractBaseRecord> long loadRecords(int recordsPerPage, long highestRecordId, RecordStore<R> recordStore, R record) {
+        long[] ids = new long[BATCH_SIZE];
         long pages = 0;
+        int idx = 0;
+        List<Future<Long>> futures = new ArrayList<>(100);
         for (long id = 0; id <= highestRecordId; id += recordsPerPage) {
-            if (pages % 100_000 == 0) {
-                if (Util.transactionIsTerminated(db)) return pages;
+            ids[idx++] = id;
+            if (idx == BATCH_SIZE) {
+                long[] submitted = ids.clone();
+                idx = 0;
+                futures.add(Util.inTxFuture(Pools.DEFAULT, db, () -> loadRecords(submitted, record, recordStore)));
             }
-            pages++;
-            record.setId(id);
+            pages += removeDone(futures, false);
+        }
+        if (idx > 0) {
+            long[] submitted = Arrays.copyOf(ids, idx);
+            futures.add(Util.inTxFuture(Pools.DEFAULT, db, () -> loadRecords(submitted, record, recordStore)));
+        }
+        pages += removeDone(futures, true);
+        return pages;
+    }
+
+    public <R extends AbstractBaseRecord> long loadRecords(long[] submitted, R record, RecordStore<R> recordStore) {
+        if (Util.transactionIsTerminated(db)) return 0;
+        for (long recordId : submitted) {
+            record.setId(recordId);
             record.clear();
             try {
-                recordStore.getRecord(id, record, RecordLoad.NORMAL);
-            } catch(Exception ignore) {
+                recordStore.getRecord(recordId, record, RecordLoad.NORMAL);
+            } catch (Exception ignore) {
                 // ignore
+            }
+        }
+        return submitted.length;
+    }
+
+    public long removeDone(List<Future<Long>> futures, boolean wait) {
+        long pages = 0;
+        if (wait || futures.size() > 25) {
+            Iterator<Future<Long>> it = futures.iterator();
+            while (it.hasNext()) {
+                Future<Long> future = it.next();
+                if (wait || future.isDone()) {
+                    try {
+                        pages += future.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        log.warn("Error during task execution", e);
+                    }
+                    it.remove();
+                }
             }
         }
         return pages;
