47b4eee28e9c23148d8c8e8c6bae63634d051de5
Adam Cowley <adam@adamcowley.co.uk>
2017-09-11 11:05:58 +0100

    Added apoc.text.capitalize, apoc.text.capitalizeAll, apoc.text.decapitalize, apoc.text.swapCase, apoc.text.camelCase, apoc.text.snakeCase, apoc.text.toUpperCase functions - #530

--- src/main/java/apoc/text/Strings.java
+++ src/main/java/apoc/text/Strings.java
@@ -230,4 +230,123 @@ public class Strings {
         return output.toString();
     }
 
+    @UserFunction
+    @Description("apoc.text.capitalize(text) YIELD value - capitalise the first letter of the word")
+    public String capitalize(@Name("text") String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
+    }
+
+    @UserFunction
+    @Description("apoc.text.capitalizeAll(text) YIELD value - capitalise the first letter of every word in the text")
+    public String capitalizeAll(@Name("text") String text) {
+        String[] parts = text.split(" ");
+
+        StringBuilder output = new StringBuilder();
+
+        for (String part : parts) {
+            output.append( StringUtils.capitalize(part) + " " );
+
+        }
+
+        return output.toString().trim();
+    }
+
+    @UserFunction
+    @Description("apoc.text.decapitalize(text) YIELD value - decapitalize the first letter of the word")
+    public String decapitalize(@Name("text") String text) {
+        return StringUtils.uncapitalize(text);
+    }
+
+    @UserFunction
+    @Description("apoc.text.decapitalizeAll(text) YIELD value - decapitalize the first letter of the word")
+    public String decapitalizeAll(@Name("text") String text) {
+        String[] parts = text.split(" ");
+
+        StringBuilder output = new StringBuilder();
+
+        for (String part : parts) {
+            output.append( StringUtils.uncapitalize(part) + " " );
+
+        }
+
+        return output.toString().trim();
+    }
+
+    @UserFunction
+    @Description("apoc.text.swapCase(text) YIELD value - Swap the case of a string")
+    public String swapCase(@Name("text") String text) {
+        return StringUtils.swapCase(text);
+    }
+
+    @UserFunction
+    @Description("apoc.text.camelCase(text) YIELD value - Convert a string to camelCase")
+    public String camelCase(@Name("text") String text) {
+        text = text.replaceAll("\\W|_+", " ");
+
+        String[] parts = text.split("(\\s+)");
+        StringBuilder output = new StringBuilder();
+
+        for (String part : parts) {
+            part = part.toLowerCase();
+
+            output.append( StringUtils.capitalize( part ) );
+        }
+
+        return output.substring(0, 1).toLowerCase() + output.substring(1);
+    }
+
+    @UserFunction
+    @Description("apoc.text.upperCamelCase(text) YIELD value - Convert a string to camelCase")
+    public String upperCamelCase(@Name("text") String text) {
+        String output = camelCase(text);
+
+        return output.substring(0, 1).toUpperCase() + output.substring(1);
+    }
+
+    @UserFunction
+    @Description("apoc.text.snakeCase(text) YIELD value - Convert a string to snake-case")
+    public String snakeCase(@Name("text") String text) {
+        // Convert Snake Case
+        if ( text.matches("^([A-Z0-9_]+)$") ) {
+            text = text.toLowerCase().replace("_", " ");
+        }
+
+        String[] parts = text.split("(?=[^a-z0-9])");
+        StringBuilder output = new StringBuilder();
+
+        for (String part : parts) {
+            part = part.trim();
+
+            if (part.length() > 0) {
+                if (output.length() > 0) {
+                    output.append("-");
+                }
+
+                output.append( part.toLowerCase().trim().replace("(^[a-z0-9]+)", "-") );
+            }
+        }
+
+        return output.toString().toLowerCase().replaceAll("--", "-");
+    }
+
+    @UserFunction
+    @Description("apoc.text.toUpperCase(text) YIELD value - Convert a string to UPPER_CASE")
+    public String toUpperCase(@Name("text") String text) {
+        String[] parts = text.split("(?=[^a-z0-9]+)");
+        StringBuilder output = new StringBuilder();
+
+        for (String part : parts) {
+            part = part.trim().toUpperCase().replaceAll("[^A-Z0-9]+", "");
+
+            if (part.length() > 0) {
+                if (output.length() > 0) {
+                    output.append("_");
+                }
+
+                output.append(part);
+            }
+        }
+
+        return output.toString();
+    }
 }
