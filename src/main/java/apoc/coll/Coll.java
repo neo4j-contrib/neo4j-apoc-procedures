364bed939bc2fa476cba889c512e7cd0b4c8abfc
David Csakvari <dodiehun@gmail.com>
2017-10-08 23:16:30 +0200

    Fix class cast exception apoc.coll.max/min #586

--- src/main/java/apoc/coll/Coll.java
+++ src/main/java/apoc/coll/Coll.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.math.BigDecimal;
 
 import static java.util.Arrays.asList;
 import static org.neo4j.helpers.collection.Pair.*;
@@ -74,13 +75,62 @@ public class Coll {
     @UserFunction
     @Description("apoc.coll.min([0.5,1,2.3])")
     public Object min(@Name("values") List<Object> list) {
-        return Collections.min((List)list);
+        return Collections.min((List)list, Coll::compareAsNumbers);
     }
 
     @UserFunction
     @Description("apoc.coll.max([0.5,1,2.3])")
     public Object max(@Name("values") List<Object> list) {
-        return Collections.max((List)list);
+        return Collections.max((List)list, Coll::compareAsNumbers);
+    }
+
+    private static int compareAsNumbers(Object a, Object b) {
+        if (isNaN(a) && isNaN(b)) {
+            return 0;
+        }
+        if (isNaN(a)) {
+            return 1;
+        }
+        if (isNaN(b)) {
+            return -1;
+        }
+
+        if (isPositiveInfinity(a) && isPositiveInfinity(b)) {
+            return 0;
+        }
+        if (isPositiveInfinity(a)) {
+            return 1;
+        }
+        if (isPositiveInfinity(b)) {
+            return -1;
+        }
+
+        if (isNegativeInfinity(a) && isNegativeInfinity(b)) {
+            return 0;
+        }
+        if (isNegativeInfinity(a)) {
+            return -1;
+        }
+        if (isNegativeInfinity(b)) {
+            return 1;
+        }
+
+        return (new BigDecimal((a.toString())).compareTo(new BigDecimal(b.toString())));
+    }
+
+    private static boolean isPositiveInfinity(Object number) {
+        return (number instanceof Double && Double.isInfinite((Double) number) && 0 < (Double) number)
+                || (number instanceof Float && Float.isInfinite((Float) number) && 0 < (Float) number);
+    }
+
+    private static boolean isNaN(Object number) {
+        return (number instanceof Double && Double.isNaN((Double) number))
+                || (number instanceof Float && Float.isNaN((Float) number));
+    }
+
+    private static boolean isNegativeInfinity(Object number) {
+        return (number instanceof Double && Double.isInfinite((Double) number) && (Double) number < 0)
+                || (number instanceof Float && Float.isInfinite((Float) number) && (Float) number < 0);
     }
 
     @Procedure
