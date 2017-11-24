395d3f4a407a6a01a93c4f88892323dd43be170d
stdob <gleb+github+stdob@belokrys.org>
2017-11-25 02:34:27 +0300

    Added pagination

--- src/main/java/apoc/mongodb/MongoDBColl.java
+++ src/main/java/apoc/mongodb/MongoDBColl.java
@@ -114,12 +114,19 @@ class MongoDBColl implements MongoDB.Coll {
     }
 
     @Override
-    public Stream<Map<String, Object>> find(Map<String, Object> query, Map<String, Object> project, Map<String, Object> sort) {
+    public Stream<Map<String, Object>> find(Map<String, Object> query, Map<String, Object> project, Map<String, Object> sort, Map<String, Object> pagination) {
         FindIterable<Document> documents = query == null ? collection.find() : collection.find(new Document(query));
         if (project != null) documents = documents.projection(new Document(project));
         if (sort != null) documents = documents.sort(new Document(sort));
+        if (pagination != null) {
+            Object skip = pagination.get("skip");
+            Object limit = pagination.get("limit");
+            if (skip != null) documents = documents.skip(Integer.parseInt(String.valueOf(skip)));
+            if (limit != null) documents = documents.limit(Integer.parseInt(String.valueOf(limit)));
+        }
         return asStream(documents);
     }
+
 
     @Override
     public void insert(List<Map<String, Object>> docs) {
