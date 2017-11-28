d4df1392dc007462a70ebe6d953d511b4f766c8e
stdob <gleb+github+stdob@belokrys.org>
2017-11-28 03:16:42 +0300

    Add pagination as two separate parameters

--- src/main/java/apoc/mongodb/MongoDBColl.java
+++ src/main/java/apoc/mongodb/MongoDBColl.java
@@ -114,19 +114,14 @@ class MongoDBColl implements MongoDB.Coll {
     }
 
     @Override
-    public Stream<Map<String, Object>> find(Map<String, Object> query, Map<String, Object> project, Map<String, Object> sort, Map<String, Object> pagination) {
+    public Stream<Map<String, Object>> find(Map<String, Object> query, Map<String, Object> project, Map<String, Object> sort, Long skip, Long limit) {
         FindIterable<Document> documents = query == null ? collection.find() : collection.find(new Document(query));
         if (project != null) documents = documents.projection(new Document(project));
         if (sort != null) documents = documents.sort(new Document(sort));
-        if (pagination != null) {
-            Object skip = pagination.get("skip");
-            Object limit = pagination.get("limit");
-            if (skip != null) documents = documents.skip(Integer.parseInt(String.valueOf(skip)));
-            if (limit != null) documents = documents.limit(Integer.parseInt(String.valueOf(limit)));
-        }
+        if (skip != null) documents = documents.skip(skip.intValue());
+        if (limit != null) documents = documents.limit(limit.intValue());
         return asStream(documents);
     }
-
 
     @Override
     public void insert(List<Map<String, Object>> docs) {
