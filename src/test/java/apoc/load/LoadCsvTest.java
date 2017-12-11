67ce41b60fafb4f36ea654c83dee5fa9225ab47c
Michael Hunger <github@jexp.de>
2017-12-11 09:37:16 +0100

    Improvements for apoc.load.csv
    * handling of converted null values/null columns, fixes #693
    * explicit `nullValues` option to define values that will be replaced by null
    * nullValues is also supported per field-mapping
    * explicit `results` option to determine which output columns are provided, defaults to `map,list` also allows stringMap and strings as Cypher doesn't support mixed lists/maps

--- src/test/java/apoc/load/LoadCsvTest.java
+++ src/test/java/apoc/load/LoadCsvTest.java
@@ -1,6 +1,5 @@
 package apoc.load;
 
-import apoc.load.LoadCsv;
 import apoc.util.TestUtil;
 import org.junit.After;
 import org.junit.Before;
@@ -10,7 +9,9 @@ import org.neo4j.graphdb.Result;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static apoc.util.MapUtil.map;
@@ -18,6 +19,7 @@ import static apoc.util.TestUtil.testCall;
 import static apoc.util.TestUtil.testResult;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class LoadCsvTest {
@@ -34,15 +36,65 @@ public class LoadCsvTest {
 
     @Test public void testLoadCsv() throws Exception {
         URL url = ClassLoader.getSystemResource("test.csv");
-        testResult(db, "CALL apoc.load.csv({url},null)", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    assertRow(r, "Selma", "8", 0L);
-                    assertRow(r, "Rana", "11", 1L);
-                    assertRow(r, "Selina", "18", 2L);
+                    assertRow(r,0L,"name","Selma","age","8");
+                    assertRow(r,1L,"name","Rana","age","11");
+                    assertRow(r,2L,"name","Selina","age","18");
                     assertEquals(false, r.hasNext());
                 });
     }
 
+    /*
+    WITH 'file:///test.csv' AS url
+CALL apoc.load.csv(url,) YIELD map AS m
+RETURN m.col_1,m.col_2,m.col_3
+     */
+    @Test public void testLoadCsvWithEmptyColumns() throws Exception {
+        URL url = ClassLoader.getSystemResource("empty_columns.csv");
+        testResult(db, "CALL apoc.load.csv({url},{failOnError:false,mapping:{col_2:{type:'int'}}})", map("url",url.toString()), // 'file:test.csv'
+                (r) -> {
+                    Map<String, Object> row = r.next();
+                    assertEquals(map("col_1", "1","col_2", null,"col_3", "1"), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", "2","col_2", 2L,"col_3", ""), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", "3","col_2", 3L,"col_3", "3"), row.get("map"));
+                    assertEquals(false, r.hasNext());
+                });
+        testResult(db, "CALL apoc.load.csv({url},{failOnError:false,nullValues:[''], mapping:{col_1:{type:'int'}}})", map("url",url.toString()), // 'file:test.csv'
+                (r) -> {
+                    Map<String, Object> row = r.next();
+                    assertEquals(map("col_1", 1L,"col_2", null,"col_3", "1"), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", 2L,"col_2", "2","col_3", null), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", 3L,"col_2", "3","col_3", "3"), row.get("map"));
+                    assertEquals(false, r.hasNext());
+                });
+        testResult(db, "CALL apoc.load.csv({url},{failOnError:false,mapping:{col_3:{type:'int',nullValues:['']}}})", map("url",url.toString()), // 'file:test.csv'
+                (r) -> {
+                    Map<String, Object> row = r.next();
+                    assertEquals(map("col_1", "1","col_2", "","col_3", 1L), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", "2","col_2", "2","col_3", null), row.get("map"));
+                    row = r.next();
+                    assertEquals(map("col_1", "3","col_2", "3","col_3", 3L), row.get("map"));
+                    assertEquals(false, r.hasNext());
+                });
+    }
+
+    private void assertRow(Result r, long lineNo, Object...data) {
+        Map<String, Object> row = r.next();
+        Map<String, Object> map = map(data);
+        assertEquals(map, row.get("map"));
+        Map<Object, Object> stringMap = new LinkedHashMap<>(map.size());
+        map.forEach((k,v) -> stringMap.put(k,v == null ? null : v.toString()));
+        assertEquals(stringMap, row.get("stringMap"));
+        assertEquals(new ArrayList<>(map.values()), row.get("list"));
+        assertEquals(new ArrayList<>(stringMap.values()), row.get("strings"));
+        assertEquals(lineNo, row.get("lineNo"));
+    }
     private void assertRow(Result r, String name, String age, long lineNo) {
         Map<String, Object> row = r.next();
         assertEquals(map("name", name,"age", age), row.get("map"));
@@ -52,7 +104,7 @@ public class LoadCsvTest {
 
     @Test public void testLoadCsvSkip() throws Exception {
         URL url = ClassLoader.getSystemResource("test.csv");
-        testResult(db, "CALL apoc.load.csv({url},{skip:1,limit:1})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{skip:1,limit:1,results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
                     assertRow(r, "Rana", "11", 1L);
                     assertEquals(false, r.hasNext());
@@ -60,16 +112,16 @@ public class LoadCsvTest {
     }
     @Test public void testLoadCsvTabSeparator() throws Exception {
         URL url = ClassLoader.getSystemResource("test-tab.csv");
-        testResult(db, "CALL apoc.load.csv({url},{sep:'TAB'})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{sep:'TAB',results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    assertRow(r, "Rana", "11", 0L);
+                    assertRow(r, 0L,"name", "Rana", "age","11");
                     assertEquals(false, r.hasNext());
                 });
     }
 
     @Test public void testLoadCsvNoHeader() throws Exception {
         URL url = ClassLoader.getSystemResource("test-no-header.csv");
-        testResult(db, "CALL apoc.load.csv({url},{header:false})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{header:false,results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
                     Map<String, Object> row = r.next();
                     assertEquals(null, row.get("map"));
@@ -80,28 +132,25 @@ public class LoadCsvTest {
     }
     @Test public void testLoadCsvIgnoreFields() throws Exception {
         URL url = ClassLoader.getSystemResource("test-tab.csv");
-        testResult(db, "CALL apoc.load.csv({url},{ignore:['age'],sep:'TAB'})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{ignore:['age'],sep:'TAB',results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    Map<String, Object> row = r.next();
-                    assertEquals(map("name", "Rana"), row.get("map"));
-                    assertEquals(asList("Rana"), row.get("list"));
-                    assertEquals(0L, row.get("lineNo"));
+                    assertRow(r,0L,"name","Rana");
                     assertEquals(false, r.hasNext());
                 });
     }
 
     @Test public void testLoadCsvColonSeparator() throws Exception {
         URL url = ClassLoader.getSystemResource("test.dsv");
-        testResult(db, "CALL apoc.load.csv({url},{sep:':'})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{sep:':',results:['map','list','stringMap','strings']})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
-                    assertRow(r,"Rana","11",0L);
-                    assertEquals(false, r.hasNext());
+                    assertRow(r,0L,"name","Rana","age","11");
+                    assertFalse(r.hasNext());
                 });
     }
 
     @Test public void testPipeArraySeparator() throws Exception {
         URL url = ClassLoader.getSystemResource("test-pipe-column.csv");
-        testResult(db, "CALL apoc.load.csv({url},{mapping:{name:{type:'string'},beverage:{array:true,arraySep:'|',type:'string'}}})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{results:['map','list','stringMap','strings'],mapping:{name:{type:'string'},beverage:{array:true,arraySep:'|',type:'string'}}})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
                     assertEquals(asList("Selma", asList("Soda")), r.next().get("list"));
                     assertEquals(asList("Rana", asList("Tea", "Milk")), r.next().get("list"));
@@ -111,11 +160,13 @@ public class LoadCsvTest {
 
     @Test public void testMapping() throws Exception {
         URL url = ClassLoader.getSystemResource("test-mapping.csv");
-        testResult(db, "CALL apoc.load.csv({url},{mapping:{name:{type:'string'},age:{type:'int'},kids:{array:true,arraySep:':',type:'int'},pass:{ignore:true}}})", map("url",url.toString()), // 'file:test.csv'
+        testResult(db, "CALL apoc.load.csv({url},{results:['map','list','stringMap','strings'],mapping:{name:{type:'string'},age:{type:'int'},kids:{array:true,arraySep:':',type:'int'},pass:{ignore:true}}})", map("url",url.toString()), // 'file:test.csv'
                 (r) -> {
                     Map<String, Object> row = r.next();
                     assertEquals(map("name", "Michael", "age", 41L, "kids", asList(8L, 11L, 18L)), row.get("map"));
+                    assertEquals(map("name", "Michael", "age", "41", "kids", "8:11:18"), row.get("stringMap"));
                     assertEquals(asList("Michael", 41L, asList(8L, 11L, 18L)), row.get("list"));
+                    assertEquals(asList("Michael", "41", "8:11:18"), row.get("strings"));
                     assertEquals(0L, row.get("lineNo"));
                     assertEquals(false, r.hasNext());
                 });
@@ -124,11 +175,11 @@ public class LoadCsvTest {
     @Test
     public void testLoadCsvByUrl() throws Exception {
         URL url = new URL("https://raw.githubusercontent.com/neo4j-contrib/neo4j-apoc-procedures/3.1/src/test/resources/test.csv");
-        testResult(db, "CALL apoc.load.csv({url},null)", map("url", url.toString()),
+        testResult(db, "CALL apoc.load.csv({url},{results:['map','list','stringMap','strings']})", map("url", url.toString()),
                 (r) -> {
-                    assertRow(r, "Selma", "8", 0L);
-                    assertRow(r, "Rana", "11", 1L);
-                    assertRow(r, "Selina", "18", 2L);
+                    assertRow(r,0L,"name","Selma","age","8");
+                    assertRow(r,1L,"name","Rana","age","11");
+                    assertRow(r,2L,"name","Selina","age","18");
                     assertEquals(false, r.hasNext());
                 });
 
@@ -137,11 +188,11 @@ public class LoadCsvTest {
     @Test
     public void testLoadCsvByUrlRedirect() throws Exception {
         URL url = new URL("http://bit.ly/2nXgHA2");
-        testResult(db, "CALL apoc.load.csv({url},null)", map("url", url.toString()),
+        testResult(db, "CALL apoc.load.csv({url},{results:['map','list','stringMap','strings']})", map("url", url.toString()),
                 (r) -> {
-                    assertRow(r, "Selma", "8", 0L);
-                    assertRow(r, "Rana", "11", 1L);
-                    assertRow(r, "Selina", "18", 2L);
+                    assertRow(r,0L,"name","Selma","age","8");
+                    assertRow(r,1L,"name","Rana","age","11");
+                    assertRow(r,2L,"name","Selina","age","18");
                     assertEquals(false, r.hasNext());
                 });
     }
@@ -154,7 +205,9 @@ public class LoadCsvTest {
                     Map<String, Object> row = r.next();
                     assertEquals(0L, row.get("lineNo"));
                     assertEquals(Collections.emptyList(), row.get("list"));
+                    assertEquals(Collections.emptyList(), row.get("strings"));
                     assertEquals(Collections.emptyMap(), row.get("map"));
+                    assertEquals(Collections.emptyMap(), row.get("stringMap"));
                     assertEquals(false, r.hasNext());
                 });
     }
