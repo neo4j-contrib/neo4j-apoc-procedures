433ad36c73e2612e2bf59bfa73af262ca2647503
Stefan Armbruster <stefan@armbruster-it.de>
2018-03-29 12:58:42 +0200

    fixes #720: apoc.cypher.runTimeboxed uses separate thread (#774)
    * fixes #720: apoc.cypher.runTimeboxed uses separate thread
    
    * getting rid of System.out
    
    * code review suggtions applied

--- src/main/java/apoc/cypher/Timeboxed.java
+++ src/main/java/apoc/cypher/Timeboxed.java
@@ -4,17 +4,17 @@ import apoc.Pools;
 import apoc.result.MapResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
-import org.neo4j.kernel.api.KernelTransaction;
-import org.neo4j.kernel.api.exceptions.Status;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.TransactionTerminatedException;
 import org.neo4j.logging.Log;
-import org.neo4j.procedure.Context;
-import org.neo4j.procedure.Description;
-import org.neo4j.procedure.Name;
-import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.*;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
@@ -24,26 +24,95 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
  */
 public class Timeboxed {
 
-
     @Context
     public GraphDatabaseService db;
-    @Context
-    public KernelTransaction tx;
 
     @Context
     public Log log;
+
+    private final static Map<String,Object> POISON = Collections.singletonMap("__magic", "POISON");
 
     @Procedure
     @Description("apoc.cypher.runTimeboxed('cypherStatement',{params}, timeout) - abort statement after timeout ms if not finished")
     public Stream<MapResult> runTimeboxed(@Name("cypher") String cypher, @Name("params") Map<String, Object> params, @Name("timeout") long timeout) {
 
+        final BlockingQueue<Map<String, Object>> queue = new ArrayBlockingQueue<>(100);
+        final AtomicReference<Transaction> txAtomic = new AtomicReference<>();
+
+        // run query to be timeboxed in a separate thread to enable proper tx termination
+        // if we'd run this in current thread, a tx.terminate would kill the transaction the procedure call uses itself.
+        Pools.DEFAULT.submit(() -> {
+            try (Transaction tx = db.beginTx()) {
+                txAtomic.set(tx);
+                Result result = db.execute(cypher, params == null ? Collections.EMPTY_MAP : params);
+                while (result.hasNext()) {
+                    final Map<String, Object> map = result.next();
+                    offerToQueue(queue, map, timeout);
+                }
+                offerToQueue(queue, POISON, timeout);
+                tx.success();
+            } catch (TransactionTerminatedException e) {
+                log.warn("query " + cypher + " has been terminated");
+            } finally {
+                txAtomic.set(null);
+            }
+        });
+
+        //
         Pools.SCHEDULED.schedule(() -> {
-            String txString = tx == null ? "<null>" : tx.toString();
-            log.warn("marking " + txString + " for termination");
-            tx.markForTermination(Status.Transaction.Terminated);
+            Transaction tx = txAtomic.get();
+            if (tx==null) {
+                log.info("tx is null, either the other transaction finished gracefully or has not yet been start.");
+            } else {
+                tx.terminate();
+                offerToQueue(queue, POISON, timeout);
+                log.warn("terminating transaction, putting POISON onto queue");
+            }
         }, timeout, MILLISECONDS);
 
-        Result result = db.execute(cypher, params == null ? Collections.EMPTY_MAP : params);
-        return result.stream().map(MapResult::new);
+        // consume the blocking queue using a custom iterator finishing upon POISON
+        Iterator<Map<String,Object>> queueConsumer = new Iterator<Map<String, Object>>() {
+            Map<String,Object> nextElement = null;
+            boolean hasFinished = false;
+
+            @Override
+            public boolean hasNext() {
+                if (hasFinished) {
+                    return false;
+                } else {
+                    try {
+                        nextElement = queue.poll(timeout, MILLISECONDS);
+                        if (nextElement==null) {
+                            log.warn("couldn't grab queue element, aborting - this should never happen");
+                            hasFinished = true;
+                        } else {
+                            hasFinished = POISON.equals(nextElement);
+                        }
+                        return !hasFinished;
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public Map<String, Object> next() {
+                return nextElement;
+            }
+        };
+        return StreamSupport
+                .stream( Spliterators.spliteratorUnknownSize(queueConsumer, Spliterator.ORDERED), false)
+                .map(MapResult::new);
+    }
+
+    private void offerToQueue(BlockingQueue<Map<String, Object>> queue, Map<String, Object> map, long timeout)  {
+        try {
+            boolean hasBeenAdded = queue.offer(map, timeout, MILLISECONDS);
+            if (!hasBeenAdded) {
+                throw new IllegalStateException("couldn't add a value to a queue of size " + queue.size() + ". Either increase capacity or fix consumption of the queue");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
