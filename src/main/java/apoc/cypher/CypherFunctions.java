39559c80aeb510d4dfe9adb0354354eb23a34604
Michael Hunger <github@jexp.de>
2018-06-24 23:07:46 +0200

    fix runFirstColumn for neo4j-graphql-js, fixes #819 fixes #776

--- src/main/java/apoc/cypher/CypherFunctions.java
+++ src/main/java/apoc/cypher/CypherFunctions.java
@@ -9,7 +9,9 @@ import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static apoc.cypher.Cypher.withParamMapping;
 
@@ -21,16 +23,30 @@ public class CypherFunctions {
     public GraphDatabaseService db;
 
     @UserFunction
-    @Description("apoc.cypher.runFirstColumn(kernelTransaction, params, expectMultipleValues) - executes kernelTransaction with given parameters, returns first column only, if expectMultipleValues is true will collect results into an array")
+    @Deprecated
+    @Description("use either apoc.cypher.runFirstColumnMany for a list return or apoc.cypher.runFirstColumnSingle for returning the first row of the first column")
     public Object runFirstColumn(@Name("cypher") String statement, @Name("params") Map<String, Object> params, @Name(value = "expectMultipleValues",defaultValue = "true") boolean expectMultipleValues) {
         if (params == null) params = Collections.emptyMap();
-        try (Result result = db.execute(withParamMapping(statement, params.keySet()), params)) {
+        String resolvedStatement = withParamMapping(statement, params.keySet());
+        if (!resolvedStatement.contains(" runtime")) resolvedStatement = "cypher runtime=slotted " + resolvedStatement;
+        try (Result result = db.execute(resolvedStatement, params)) {
 
         String firstColumn = result.columns().get(0);
         try (ResourceIterator<Object> iter = result.columnAs(firstColumn)) {
-            if (expectMultipleValues) return iter.stream().toArray();
+            if (expectMultipleValues) return iter.stream().collect(Collectors.toList());
             return iter.hasNext() ? iter.next() : null;
         }
       }
+    }
+
+    @UserFunction
+    @Description("apoc.cypher.runFirstColumnMany(statement, params, expectMultipleValues) - executes statement with given parameters, returns first column only collected into a list, params are available as identifiers")
+    public List<Object> runFirstColumnMany(@Name("cypher") String statement, @Name("params") Map<String, Object> params) {
+        return (List)runFirstColumn(statement, params, true);
+    }
+    @UserFunction
+    @Description("apoc.cypher.runFirstColumnSingle(statement, params, expectMultipleValues) - executes statement with given parameters, returns first element of the first column only, params are available as identifiers")
+    public Object runFirstColumnSingle(@Name("cypher") String statement, @Name("params") Map<String, Object> params) {
+        return runFirstColumn(statement, params, false);
     }
 }
