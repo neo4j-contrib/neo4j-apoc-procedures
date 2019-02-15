cb4149f7a16ac5a1f26806eca94f1c56679f01af
Michael Hunger <github@jexp.de>
2019-02-15 12:50:07 +0100

    New Docs for 3.4 (Testing)

---
+++ 3.4/javascript/datatable.js
@@ -0,0 +1,9 @@
+/*
+<!--script src="https://code.jquery.com/jquery-1.12.4.js"></script-->
+<!--script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script-->
+<script>
+</script>
+*/
+$(document).ready(function() {
+    $('#table-all table').DataTable();
+} );
