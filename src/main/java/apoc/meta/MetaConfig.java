44dfa6dd985926243b52d9371a1961f796ac8721
Michael Hunger <github@jexp.de>
2019-02-15 14:42:03 +0100

    Default to sampling 1000 nodes and 100 relationships

--- src/main/java/apoc/meta/MetaConfig.java
+++ src/main/java/apoc/meta/MetaConfig.java
@@ -15,8 +15,8 @@ public class MetaConfig {
         this.includesLabels = new HashSet<>((Collection<String>)config.getOrDefault("labels",Collections.EMPTY_SET));
         this.includesRels = new HashSet<>((Collection<String>)config.getOrDefault("rels",Collections.EMPTY_SET));
         this.excludes = new HashSet<>((Collection<String>)config.getOrDefault("excludes",Collections.EMPTY_SET));
-        this.sample = (long) config.getOrDefault("sample", -1L);
-        this.maxRels = (long) config.getOrDefault("maxRels", -1L);
+        this.sample = (long) config.getOrDefault("sample", 1000L);
+        this.maxRels = (long) config.getOrDefault("maxRels", 100L);
     }
 
     public Set<String> getIncludesLabels() {
