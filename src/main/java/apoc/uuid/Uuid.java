12d9e9ea0d89609e68629d05c12829cfc5a75ca4
Andrea Santurbano <santand@gmail.com>
2019-03-06 13:32:05 +0100

    enhancements

--- src/main/java/apoc/uuid/Uuid.java
+++ src/main/java/apoc/uuid/Uuid.java
@@ -2,6 +2,7 @@ package apoc.uuid;
 
 import apoc.ApocConfiguration;
 import apoc.Description;
+import apoc.util.JsonUtil;
 import apoc.util.Util;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
@@ -11,6 +12,7 @@ import org.neo4j.procedure.Mode;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -19,30 +21,43 @@ public class Uuid {
     @Context
     public GraphDatabaseService db;
 
-    @Procedure(mode = Mode.WRITE)
-    @Description("CALL apoc.uuid.add(label, {config}) | it will ad the uuid for the Label")
-    public Stream<UuidInfo> add(@Name("label") String label, @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
-        Map<String, Object> removed = UuidHandler.add(label, config);
+    @Procedure(mode = Mode.DBMS)
+    @Description("CALL apoc.uuid.install(label, {addToExistingNodes: true/false, uuidProperty: 'uuid'}) yield label, installed, properties, batchComputationResult | it will add the uuid transaction handler\n" +
+            "for the provided `label` and `uuidProperty`, in case the UUID handler is already present it will be replaced by the new one")
+    public Stream<UuidInfo> install(@Name("label") String label, @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
+        UuidConfig uuidConfig = new UuidConfig(config);
+        UuidHandler.checkConstraintUuid(label, uuidConfig);
+        Map<String, Object> addToExistingNodesResult = Collections.emptyMap();
+        if (uuidConfig.isAddToExistingNodes()) {
+            addToExistingNodesResult = Util.inTx(db, () ->
+                    db.execute("CALL apoc.periodic.iterate(" +
+                            "\"MATCH (n:" + Util.sanitizeAndQuote(label) + ") RETURN n\",\n" +
+                            "\"SET n." + Util.sanitizeAndQuote(uuidConfig.getUuidProperty()) + " = apoc.create.uuid()\", {batchSize:10000, parallel:true})")
+                            .next()
+            );
+        }
+        UuidConfig removed = UuidHandler.add(label, uuidConfig);
+        config = JsonUtil.OBJECT_MAPPER.convertValue(uuidConfig, Map.class); // return the applied configuration (with defaults if the original config was null or empty)
         if (removed != null) {
             return Stream.of(
                     new UuidInfo(label, false),
-                    new UuidInfo(label, true));
+                    new UuidInstallInfo(label, true, config, addToExistingNodesResult));
         }
-        return Stream.of(new UuidInfo(label, true));
+        return Stream.of(new UuidInstallInfo(label, true, config, addToExistingNodesResult));
     }
 
     @Procedure(mode = Mode.WRITE)
-    @Description("CALL apoc.uuid.remove(label) | it remove the uuid")
+    @Description("CALL apoc.uuid.remove(label) yield label, installed, properties | remove previously added uuid handler and returns uuid information. All the existing uuid properties are left as-is")
     public Stream<UuidInfo> remove(@Name("label") String label) {
-        Map<String, Object> removed = UuidHandler.remove(label);
+        UuidConfig removed = UuidHandler.remove(label);
         if (removed == null) {
             return Stream.of(new UuidInfo(null, false));
         }
-        return Stream.of(new UuidInfo(label, false));
+        return Stream.of(new UuidInfo(label, false, JsonUtil.OBJECT_MAPPER.convertValue(removed, Map.class)));
     }
 
     @Procedure(mode = Mode.WRITE)
-    @Description("CALL apoc.uuid.removeAll() | it remove all the uuid")
+    @Description("CALL apoc.uuid.removeAll() yield label, installed, properties | it removes all previously added uuid handlers and returns uuids information. All the existing uuid properties are left as-is")
     public Stream<UuidInfo> removeAll() {
         Map<String, Object> removed = UuidHandler.removeAll();
         if (removed == null) {
@@ -52,20 +67,22 @@ public class Uuid {
     }
 
     @Procedure(mode = Mode.READ)
-    @Description("list all installed uuid")
+    @Description("CALL apoc.uuid.list() yield label, installed, properties | provides a list of all the uuid handlers installed with the related configuration")
     public Stream<UuidInfo> list() {
         return UuidHandler.list().entrySet().stream()
-                .map((e) -> new UuidInfo(e.getKey(),true));
+                .map((e) -> new UuidInfo(e.getKey(),true, JsonUtil.OBJECT_MAPPER.convertValue(e.getValue(), Map.class)));
     }
 
     private UuidInfo uuidInfo(Map.Entry<String, Object> e) {
         String label = e.getKey();
-        if (e.getValue() instanceof Map) {
-            try {
-                return new UuidInfo(label, false);
-            } catch (Exception ex) {
-                return new UuidInfo(null, false);
+        try {
+            if (e.getValue() instanceof Map) {
+                return new UuidInfo(label, false, (Map<String, Object>) e.getValue());
+            } else if (e.getValue() instanceof UuidConfig) {
+                return new UuidInfo(null, false, JsonUtil.OBJECT_MAPPER.convertValue(e.getValue(), Map.class));
             }
+        } catch (Exception ex) {
+            return new UuidInfo(null, false);
         }
         return new UuidInfo(null, false);
     }
@@ -73,11 +90,28 @@ public class Uuid {
     public static class UuidInfo {
         public final String label;
         public boolean installed;
+        public Map<String, Object> properties;
 
-        UuidInfo(String label, boolean installed) {
+        UuidInfo(String label, boolean installed, Map<String, Object> properties) {
             this.label = label;
             this.installed = installed;
+            this.properties = properties;
         }
+
+        UuidInfo(String label, boolean installed) {
+            this(label, installed, Collections.emptyMap());
+        }
+    }
+
+
+    public static class UuidInstallInfo extends UuidInfo {
+        public Map<String, Object> batchComputationResult;
+
+        UuidInstallInfo(String label, boolean installed, Map<String, Object> properties, Map<String, Object> batchComputationResult) {
+            super(label, installed, properties);
+            this.batchComputationResult = batchComputationResult;
+        }
+
     }
 
     public static class UuidLifeCycle {
--- src/main/java/apoc/uuid/UuidHandler.java
+++ src/main/java/apoc/uuid/UuidHandler.java
@@ -1,6 +1,8 @@
 package apoc.uuid;
 
+import apoc.util.JsonUtil;
 import apoc.util.Util;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
@@ -23,16 +25,15 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static apoc.util.Util.map;
-import static apoc.util.Util.toBoolean;
-
 public class UuidHandler implements TransactionEventHandler {
 
     private static final String APOC_UUID = "apoc.uuid";
-    private static final String UUID_PROPERTY = "uuid";
-    static ConcurrentHashMap<String, Map<String, Object>> uuid = new ConcurrentHashMap(map("", map()));
+    static ConcurrentHashMap<String, UuidConfig> uuid = new ConcurrentHashMap();
     private static GraphProperties properties;
     private final Log log;
+
+
+    private static final TypeReference<Map<String, UuidConfig>> typeRef = new TypeReference<Map<String, UuidConfig>>() {};
 
     private static final String NOT_ENABLED_ERROR = "UUID have not been enabled." +
             " Set 'apoc.uuid.enabled=true' in your neo4j.conf file located in the $NEO4J_HOME/conf/ directory.";
@@ -48,30 +49,20 @@ public class UuidHandler implements TransactionEventHandler {
         }
     }
 
-    public static Map<String, Object> add(String label, Map<String, Object> params) {
+    public static UuidConfig add(String label, UuidConfig config) {
         checkEnabled();
-        checkConstraintUuid(label);
-        Map<String, Object> stringObjectMap = updateUuid(label, map("params", params));
-        if (toBoolean(params.getOrDefault("addToAlreadyPresent", true))) {
-            GraphDatabaseService db = properties.getGraphDatabase();
-            try (Transaction tx = db.beginTx()) {
-                db.findNodes(Label.label(label)).stream().filter(node -> !node.hasProperty(UUID_PROPERTY)).forEach(node -> {
-                    String uuid = UUID.randomUUID().toString();
-                    node.setProperty(UUID_PROPERTY, uuid);
-                });
-                tx.success();
-            }
-        }
-        return stringObjectMap;
+        checkConstraintUuid(label, config);
+        UuidConfig oldConfig = updateUuid(label, config);
+        return oldConfig;
     }
 
-    private synchronized static Map<String, Object> updateUuid(String label, Map<String, Object> value) {
+    private synchronized static UuidConfig updateUuid(String label, UuidConfig value) {
         checkEnabled();
         try (Transaction tx = properties.getGraphDatabase().beginTx()) {
             uuid.clear();
             String uuidProperty = (String) properties.getProperty(APOC_UUID, "{}");
-            uuid.putAll(Util.fromJson(uuidProperty, Map.class));
-            Map<String, Object> previous = null;
+            uuid.putAll(JsonUtil.OBJECT_MAPPER.readValue(uuidProperty, typeRef));
+            UuidConfig previous = null;
             if (label != null) {
                 previous = (value == null) ? uuid.remove(label) : uuid.put(label, value);
                 if (value != null || previous != null) {
@@ -80,12 +71,13 @@ public class UuidHandler implements TransactionEventHandler {
             }
             tx.success();
             return previous;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
-    public Object beforeCommit(TransactionData txData) throws Exception {
-        if (uuid.containsKey("")) updateUuid(null,null);
+    public Object beforeCommit(TransactionData txData) {
         GraphDatabaseService db = properties.getGraphDatabase();
         Map<String, String> exceptions = new LinkedHashMap<>();
         Iterable<Node> createdNodes = txData.createdNodes();
@@ -96,15 +88,15 @@ public class UuidHandler implements TransactionEventHandler {
             try (Transaction tx = db.beginTx()) {
                 if (createdNodes.iterator().hasNext()) {
                     createdNodes.forEach(node -> {
-                        boolean containsLabel = containsLabel(label, node.getLabels());
-                        if (containsLabel && !node.hasProperty(UUID_PROPERTY)) {
+                        if (node.hasLabel(Label.label(label)) && !node.hasProperty(config.getUuidProperty())) {
                             String uuid = UUID.randomUUID().toString();
-                            node.setProperty(UUID_PROPERTY, uuid);
+                            node.setProperty(config.getUuidProperty(), uuid);
                         }
                     });
                 }
-                checkAndRestoreUuidProperty(assignedNodeProperties, label, (nodePropertyEntry) -> nodePropertyEntry.value().equals("") || nodePropertyEntry.value() == null);
-                checkAndRestoreUuidProperty(removedNodeProperties, label, null);
+                checkAndRestoreUuidProperty(assignedNodeProperties, label, config.getUuidProperty(),
+                        (nodePropertyEntry) -> nodePropertyEntry.value() == null || nodePropertyEntry.value().equals(""));
+                checkAndRestoreUuidProperty(removedNodeProperties, label, config.getUuidProperty());
                 tx.success();
             } catch (Exception e) {
                 log.warn("Error executing uuid " + label + " in phase before", e);
@@ -114,17 +106,20 @@ public class UuidHandler implements TransactionEventHandler {
         return null;
     }
 
-    private void checkAndRestoreUuidProperty(Iterable<PropertyEntry<Node>> nodeProperties, String label, Predicate<PropertyEntry<Node>> predicate) {
+    private void checkAndRestoreUuidProperty(Iterable<PropertyEntry<Node>> nodeProperties, String label, String uuidProperty) {
+        checkAndRestoreUuidProperty(nodeProperties, label, uuidProperty, null);
+    }
+
+    private void checkAndRestoreUuidProperty(Iterable<PropertyEntry<Node>> nodeProperties, String label, String uuidProperty, Predicate<PropertyEntry<Node>> predicate) {
         if (nodeProperties.iterator().hasNext()) {
             nodeProperties.forEach(nodePropertyEntry -> {
-                boolean containsLabel = containsLabel(label, nodePropertyEntry.entity().getLabels());
                 if (predicate == null) {
-                    if (containsLabel && nodePropertyEntry.key().equals(UUID_PROPERTY)) {
-                        nodePropertyEntry.entity().setProperty(UUID_PROPERTY, nodePropertyEntry.previouslyCommitedValue());
+                    if (nodePropertyEntry.entity().hasLabel(Label.label(label)) && nodePropertyEntry.key().equals(uuidProperty)) {
+                        nodePropertyEntry.entity().setProperty(uuidProperty, nodePropertyEntry.previouslyCommitedValue());
                     }
                 } else {
-                    if (containsLabel && nodePropertyEntry.key().equals(UUID_PROPERTY) && predicate.test(nodePropertyEntry)) {
-                        nodePropertyEntry.entity().setProperty(UUID_PROPERTY, nodePropertyEntry.previouslyCommitedValue());
+                    if (nodePropertyEntry.entity().hasLabel(Label.label(label)) && nodePropertyEntry.key().equals(uuidProperty) && predicate.test(nodePropertyEntry)) {
+                        nodePropertyEntry.entity().setProperty(uuidProperty, nodePropertyEntry.previouslyCommitedValue());
                     }
                 }
             });
@@ -139,34 +134,31 @@ public class UuidHandler implements TransactionEventHandler {
     public void afterRollback(TransactionData data, Object state) {
     }
 
-    private boolean containsLabel(String label, Iterable<Label> labelIterable) {
-        return StreamSupport.stream(labelIterable.spliterator(), false).anyMatch(name -> name.name().equals(label));
-    }
-
-    public static Map<String, Map<String, Object>> list() {
+    public static Map<String, UuidConfig> list() {
         checkEnabled();
         updateUuid(null, null);
         return uuid;
     }
 
-    private static void checkConstraintUuid(String label) {
+    public static void checkConstraintUuid(String label, UuidConfig config) {
         GraphDatabaseService db = properties.getGraphDatabase();
         try (Transaction tx = db.beginTx()) {
             Schema schema = db.schema();
             Stream<ConstraintDefinition> constraintDefinitionStream = StreamSupport.stream(schema.getConstraints(Label.label(label)).spliterator(), false);
             boolean exists = constraintDefinitionStream.anyMatch(constraint -> {
                 Stream<String> streamPropertyKeys = StreamSupport.stream(constraint.getPropertyKeys().spliterator(), false);
-                return streamPropertyKeys.anyMatch(property -> property.equals(UUID_PROPERTY));
+                return streamPropertyKeys.anyMatch(property -> property.equals(config.getUuidProperty()));
             });
             if (!exists) {
-                String error = String.format("`CREATE CONSTRAINT ON (%s:%s) ASSERT %s.uuid IS UNIQUE`", label.toLowerCase(), label, label.toLowerCase());
+                String error = String.format("`CREATE CONSTRAINT ON (%s:%s) ASSERT %s.%s IS UNIQUE`",
+                        label.toLowerCase(), label, label.toLowerCase(), config.getUuidProperty());
                 throw new RuntimeException("No constraint found for label: " + label + ", please add the constraint with the following : " + error);
             }
             tx.success();
         }
     }
 
-    public synchronized static Map<String, Object> remove(String label) {
+    public synchronized static UuidConfig remove(String label) {
         return updateUuid(label, null);
     }
 
