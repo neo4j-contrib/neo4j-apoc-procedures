78443431dac692a2079bb8684c7dcb014a103f23
Andrea Santurbano <santand@gmail.com>
2019-04-12 01:52:14 +0200

    fixes #1141 (#1155)

---
+++ src/main/java/apoc/util/SimpleRateLimiter.java
@@ -0,0 +1,27 @@
+package apoc.util;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class SimpleRateLimiter {
+
+    private final AtomicInteger countDownLatch = new AtomicInteger(0);
+    private final AtomicLong lastUpdate = new AtomicLong(0);
+
+    private final long timeWindow;
+    private final int operationPerWindow;
+
+    public SimpleRateLimiter(long timeWindow, int operationPerWindow) {
+        this.timeWindow = timeWindow;
+        this.operationPerWindow = operationPerWindow;
+    }
+
+    public boolean canExecute() {
+        long now = System.currentTimeMillis();
+        if ((now - lastUpdate.get()) > timeWindow) {
+            lastUpdate.set(now);
+            countDownLatch.set(operationPerWindow);
+        }
+        return countDownLatch.decrementAndGet() >= 0;
+    }
+}
