1369a6ca143527ae8db3198e5ce5b0ae141c2baa
Stefan Armbruster <stefan@armbruster-it.de>
2019-04-11 17:12:02 +0200

    fixes #1153 export to excel (#1162)

---
+++ src/main/java/apoc/export/xls/ExportXls.java
@@ -0,0 +1,275 @@
+package apoc.export.xls;
+
+import apoc.Description;
+import apoc.export.util.NodesAndRelsSubGraph;
+import apoc.export.util.ProgressReporter;
+import apoc.result.ProgressInfo;
+import apoc.util.Util;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.tuple.Triple;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.neo4j.cypher.export.DatabaseSubGraph;
+import org.neo4j.cypher.export.SubGraph;
+import org.neo4j.graphdb.*;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.io.OutputStream;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static apoc.util.FileUtils.checkWriteAllowed;
+import static apoc.util.FileUtils.getOutputStream;
+
+public class ExportXls {
+    @Context
+    public GraphDatabaseService db;
+
+    public ExportXls(GraphDatabaseService db) {
+        this.db = db;
+    }
+
+    public ExportXls() {
+    }
+
+    @Procedure
+    @Description("apoc.export.csv.all(file,config) - exports whole database as csv to the provided file")
+    public Stream<ProgressInfo> all(@Name("file") String fileName, @Name("config") Map<String, Object> config) throws Exception {
+
+        String source = String.format("database: nodes(%d), rels(%d)", Util.nodeCount(db), Util.relCount(db));
+        return exportXls(fileName, source, new DatabaseSubGraph(db), config);
+    }
+
+    @Procedure
+    @Description("apoc.export.csv.data(nodes,rels,file,config) - exports given nodes and relationships as csv to the provided file")
+    public Stream<ProgressInfo> data(@Name("nodes") List<Node> nodes, @Name("rels") List<Relationship> rels, @Name("file") String fileName, @Name("config") Map<String, Object> config) throws Exception {
+
+        String source = String.format("data: nodes(%d), rels(%d)", nodes.size(), rels.size());
+        return exportXls(fileName, source, new NodesAndRelsSubGraph(db, nodes, rels), config);
+    }
+
+    @Procedure
+    @Description("apoc.export.csv.graph(graph,file,config) - exports given graph object as csv to the provided file")
+    public Stream<ProgressInfo> graph(@Name("graph") Map<String,Object> graph, @Name("file") String fileName, @Name("config") Map<String, Object> config) throws Exception {
+
+        Collection<Node> nodes = (Collection<Node>) graph.get("nodes");
+        Collection<Relationship> rels = (Collection<Relationship>) graph.get("relationships");
+        String source = String.format("graph: nodes(%d), rels(%d)", nodes.size(), rels.size());
+        return exportXls(fileName, source, new NodesAndRelsSubGraph(db, nodes, rels), config);
+    }
+
+    @Procedure
+    @Description("apoc.export.csv.query(query,file,{config,...,params:{params}}) - exports results from the cypher statement as csv to the provided file")
+    public Stream<ProgressInfo> query(@Name("query") String query, @Name("file") String fileName, @Name("config") Map<String, Object> config) throws Exception {
+        Map<String,Object> params = config == null ? Collections.emptyMap() : (Map<String,Object>)config.getOrDefault("params", Collections.emptyMap());
+        Result result = db.execute(query,params);
+        String source = String.format("statement: cols(%d)", result.columns().size());
+        return exportXls(fileName, source,result,config);
+    }
+
+    private Stream<ProgressInfo> exportXls(@Name("file") String fileName, String source, Object data, Map<String,Object> configMap) throws Exception {
+        checkWriteAllowed();
+        try (Transaction tx = db.beginTx();
+             OutputStream out = getOutputStream(fileName, null);
+             SXSSFWorkbook wb = new SXSSFWorkbook(100)) {
+
+            XlsExportConfig config = new XlsExportConfig(configMap);
+            ProgressInfo progressInfo = new ProgressInfo(fileName, source, "xls");
+            progressInfo.batchSize = config.getBatchSize();
+            ProgressReporter reporter = new ProgressReporter(null, null, progressInfo);
+
+            Map<Class, CellStyle> styles = buildCellStyles(config, wb);
+
+            if (data instanceof SubGraph) {
+                dumpSubGraph((SubGraph) data, config, reporter, wb, styles);
+
+            } else if (data instanceof Result ) {
+                Result result = (Result) data;
+                dumpResult(result, config, wb, styles);
+            } else {
+                throw new UnsupportedOperationException("cannot handle " + data.getClass());
+            }
+
+            wb.write(out);
+            wb.dispose();
+            reporter.done();
+            tx.success();
+            return reporter.stream();
+        }
+    }
+
+    private void dumpResult(Result result, XlsExportConfig config, SXSSFWorkbook wb, Map<Class, CellStyle> styles) {
+        SXSSFSheet sheet = wb.createSheet();
+        sheet.trackAllColumnsForAutoSizing();
+
+        // header row
+        int columnNum = 0;
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+
+        for (String header: result.columns()) {
+            Cell cell = headerRow.createCell(columnNum);
+            sheet.autoSizeColumn(columnNum);
+            cell.setCellValue(header);
+            columnNum++;
+        }
+
+        while (result.hasNext()) {
+            Map<String, Object> map = result.next();
+            Row row = sheet.createRow(rowNum++);
+            columnNum = 0;
+            for (String header: result.columns()) {
+                columnNum = amendCell(row, columnNum, map.get(header), config, styles);
+            }
+        }
+    }
+
+    private void dumpSubGraph(SubGraph subgraph, XlsExportConfig config, ProgressReporter reporter, SXSSFWorkbook wb, Map<Class, CellStyle> styles) {
+        // what's in the triple used below?
+        // left: sheet instance
+        // middle: list of "magic" property keys: <id> for nodes, <startNodeId> and <endNodeId> for rels
+        // right: list of  "normal" property keys
+        Map<String, Triple<SXSSFSheet, List<String>, List<String>>> sheetAndPropsForName = new HashMap<>();
+
+        for (Node node : subgraph.getNodes()) {
+            for (Label label : node.getLabels()) {
+                String labelName = label.name();
+                createRowForPropertyContainer(wb, sheetAndPropsForName, node, labelName, reporter, config, styles);
+            }
+        }
+        for (Relationship relationship: subgraph.getRelationships()) {
+            String relationshipType = relationship.getType().name();
+            createRowForPropertyContainer(wb, sheetAndPropsForName, relationship, relationshipType, reporter, config,styles);
+        }
+
+        // spit out header lines
+        for (Triple<SXSSFSheet,List<String>, List<String>> triple: sheetAndPropsForName.values()) {
+            Sheet sheet = triple.getLeft();
+
+            List<String> magicKeys = triple.getMiddle();
+            List<String> keys = triple.getRight();
+            Row row = sheet.getRow(0);
+            int cellNum = 0;
+            for (String key: ListUtils.union(magicKeys,keys)) {
+                sheet.autoSizeColumn(cellNum);
+                Cell cell = row.createCell(cellNum++);
+                cell.setCellValue(key);
+
+            }
+        }
+    }
+
+    private Map<Class, CellStyle> buildCellStyles(XlsExportConfig config, SXSSFWorkbook wb) {
+        CreationHelper createHelper = wb.getCreationHelper();
+        CellStyle dateTimeCellStyle = wb.createCellStyle();
+        dateTimeCellStyle.setDataFormat(createHelper.createDataFormat().getFormat(config.getDateTimeStyle()));
+        CellStyle dateCellStyle = wb.createCellStyle();
+        dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat(config.getDateStyle()));
+
+        Map<Class, CellStyle> styles = new HashMap<>();
+        styles.put(ZonedDateTime.class, dateTimeCellStyle);
+        styles.put(LocalDate.class, dateCellStyle);
+        return styles;
+    }
+
+    private void createRowForPropertyContainer(Workbook wb, Map<String, Triple<SXSSFSheet, List<String>, List<String>>> sheetAndPropsForName, PropertyContainer propertyContainer, String sheetName, ProgressReporter reporter, XlsExportConfig config, Map<Class, CellStyle> styles) {
+        Triple<SXSSFSheet, List<String>, List<String>> triple = sheetAndPropsForName.computeIfAbsent(sheetName, s -> {
+            SXSSFSheet sheet = (SXSSFSheet) wb.createSheet(sheetName);
+            sheet.trackAllColumnsForAutoSizing();
+            sheet.createRow(0); // placeholder for header line
+            return Triple.of(
+                    sheet,
+                    propertyContainer instanceof Node ?
+                            Arrays.asList(config.getHeaderNodeId()) :
+                            Arrays.asList(config.getHeaderRelationshipId(), config.getHeaderStartNodeId(), config.getHeaderEndNodeId()),
+                    new ArrayList<>());
+        });
+        Sheet sheet = triple.getLeft();
+        List<String> propertyKeys = triple.getRight();
+
+        int lastRowNum = sheet.getLastRowNum();
+        Row row = sheet.createRow(lastRowNum+1);
+        int cellNum = 0;
+        SortedMap<String, Object> props = new TreeMap<>(propertyContainer.getAllProperties()); // copy props
+
+        if (propertyContainer instanceof Node) {
+            Node node = (Node) propertyContainer;
+            Cell idCell = row.createCell(cellNum++);
+            idCell.setCellValue(((Long)(node.getId())).doubleValue());
+            reporter.update(1, 0, props.size());
+        } else if (propertyContainer instanceof Relationship) {
+            Relationship relationship = (Relationship) propertyContainer;
+            Cell idCell = row.createCell(cellNum++);
+            idCell.setCellValue(((Long)(relationship.getId())).doubleValue());
+            Cell fromCell = row.createCell(cellNum++);
+            fromCell.setCellValue(((Long)(relationship.getStartNodeId())).doubleValue());
+            Cell toCell = row.createCell(cellNum++);
+            toCell.setCellValue(((Long)(relationship.getEndNodeId())).doubleValue());
+            reporter.update(0, 1, props.size());
+        }
+
+        // deal with property keys already being known
+        for (String key: propertyKeys) {
+            cellNum = amendCell(row, cellNum, props.remove(key), config, styles);
+        }
+
+        // add remaining properties as new keys
+        for (String key: props.keySet()) {
+            propertyKeys.add(key);
+            cellNum = amendCell(row, cellNum, props.get(key), config, styles);
+        }
+    }
+
+    private int amendCell(Row row, int cellNum, Object value, XlsExportConfig config, Map<Class, CellStyle> styles) {
+        Cell cell = row.createCell(cellNum++);
+
+        if (value == null) {
+            cell.setCellType(CellType.BLANK);
+        } else {
+
+            CellStyle cellStyle = styles.get(value.getClass());
+            if (cellStyle!=null) {
+                cell.setCellStyle(cellStyle);
+            }
+
+            if (value instanceof String) {
+                cell.setCellValue((String) value);
+            } else if (value instanceof Number) {
+                cell.setCellValue(((Number) value).doubleValue());
+            } else if (value instanceof Boolean) {
+                cell.setCellValue(((Boolean) value).booleanValue());
+            } else if (value instanceof String[]) {
+                String[] values = (String[]) value;
+                cell.setCellValue(Arrays.stream(values).collect(Collectors.joining(config.getArrayDelimiter())));
+            } else if (value instanceof long[]) {
+                long[] values = (long[]) value;
+                cell.setCellValue(Arrays.stream(values).mapToObj(Long::toString).collect(Collectors.joining(config.getArrayDelimiter())));
+            } else if (value instanceof double[]) {
+                double[] values = (double[]) value;
+                cell.setCellValue(Arrays.stream(values).mapToObj(Double::toString).collect(Collectors.joining(config.getArrayDelimiter())));
+            } else if (value instanceof List) {
+                List values = (List) value;
+                String collect = ((Stream<String>) values.stream().map(x -> x.toString())).collect(Collectors.joining(config.getArrayDelimiter()));
+                cell.setCellValue(collect);
+            } else if (value instanceof LocalDate) {
+                LocalDate localDate = (LocalDate) value;
+                cell.setCellValue( Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            } else if (value instanceof ZonedDateTime) {
+                ZonedDateTime zondedDateTime = (ZonedDateTime) value;
+                cell.setCellValue( Date.from(zondedDateTime.toInstant()));
+            } else {
+                cell.setCellValue(value.toString());
+                //throw new IllegalArgumentException("dunno know how to handle type " + value.getClass() + ". Please report this as a bug.");
+            }
+        }
+        return cellNum;
+    }
+
+}
---
+++ src/test/java/apoc/export/csv/ExportXlsTest.java
@@ -0,0 +1,184 @@
+package apoc.export.csv;
+
+import apoc.export.xls.ExportXls;
+import apoc.graph.Graphs;
+import apoc.util.TestUtil;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.helpers.collection.Iterables;
+import org.neo4j.helpers.collection.Iterators;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import static apoc.util.MapUtil.map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ExportXlsTest {
+
+    private static final String EXPECTED_QUERY_NODES = String.format("\"u\"%n" +
+            "\"{\"\"id\"\":0,\"\"labels\"\":[\"\"User\"\",\"\"User1\"\"],\"\"properties\"\":{\"\"name\"\":\"\"foo\"\",\"\"age\"\":42,\"\"male\"\":true,\"\"kids\"\":[\"\"a\"\",\"\"b\"\",\"\"c\"\"]}}\"%n" +
+            "\"{\"\"id\"\":1,\"\"labels\"\":[\"\"User\"\"],\"\"properties\"\":{\"\"name\"\":\"\"bar\"\",\"\"age\"\":42}}\"%n" +
+            "\"{\"\"id\"\":2,\"\"labels\"\":[\"\"User\"\"],\"\"properties\"\":{\"\"age\"\":12}}\"");
+    private static final String EXPECTED_QUERY = String.format("\"u.age\",\"u.name\",\"u.male\",\"u.kids\",\"labels(u)\"%n" +
+            "\"42\",\"foo\",\"true\",\"[\"\"a\"\",\"\"b\"\",\"\"c\"\"]\",\"[\"\"User1\"\",\"\"User\"\"]\"%n" +
+            "\"42\",\"bar\",\"\",\"\",\"[\"\"User\"\"]\"%n" +
+            "\"12\",\"\",\"\",\"\",\"[\"\"User\"\"]\"");
+    private static final String EXPECTED_QUERY_WITHOUT_QUOTES = String.format("u.age,u.name,u.male,u.kids,labels(u)%n" +
+            "42,foo,true,[\"a\",\"b\",\"c\"],[\"User1\",\"User\"]%n" +
+            "42,bar,,,[\"User\"]%n" +
+            "12,,,,[\"User\"]");
+    private static final String EXPECTED_QUERY_QUOTES_NONE= String.format( "a.name,a.city,a.street,labels(a)%n" +
+            "Andrea,Milano,Via Garibaldi, 7,[\"Address1\",\"Address\"]%n" +
+            "Bar Sport,,,[\"Address\"]%n" +
+            ",,via Benni,[\"Address\"]" );
+    private static final String EXPECTED_QUERY_QUOTES_ALWAYS= String.format( "\"a.name\",\"a.city\",\"a.street\",\"labels(a)\"%n" +
+            "\"Andrea\",\"Milano\",\"Via Garibaldi, 7\",\"[\"\"Address1\"\",\"\"Address\"\"]\"%n" +
+            "\"Bar Sport\",\"\",\"\",\"[\"\"Address\"\"]\"%n" +
+            "\"\",\"\",\"via Benni\",\"[\"\"Address\"\"]\"");
+    private static final String EXPECTED_QUERY_QUOTES_NEEDED= String.format( "a.name,a.city,a.street,labels(a)%n" +
+            "Andrea,Milano,\"Via Garibaldi, 7\",\"[\"Address1\",\"Address\"]\"%n" +
+            "Bar Sport,,,\"[\"Address\"]\"%n" +
+            ",,via Benni,\"[\"Address\"]\"");
+    private static final String EXPECTED = String.format("\"_id\",\"_labels\",\"name\",\"age\",\"male\",\"kids\",\"street\",\"city\",\"_start\",\"_end\",\"_type\"%n" +
+            "\"0\",\":User:User1\",\"foo\",\"42\",\"true\",\"[\"\"a\"\",\"\"b\"\",\"\"c\"\"]\",\"\",\"\",,,%n" +
+            "\"1\",\":User\",\"bar\",\"42\",\"\",\"\",\"\",\"\",,,%n" +
+            "\"2\",\":User\",\"\",\"12\",\"\",\"\",\"\",\"\",,,%n" +
+            "\"20\",\":Address:Address1\",\"Andrea\",\"\",\"\",\"\",\"Via Garibaldi, 7\",\"Milano\",,,%n" +
+            "\"21\",\":Address\",\"Bar Sport\",\"\",\"\",\"\",\"\",\"\",,,%n" +
+            "\"22\",\":Address\",\"\",\"\",\"\",\"\",\"via Benni\",\"\",,,%n" +
+            ",,,,,,,,\"0\",\"1\",\"KNOWS\"%n" +
+            ",,,,,,,,\"20\",\"21\",\"NEXT_DELIVERY\"");
+    private static final String EXPECTED_NONE_QUOTES = String.format("_id,_labels,name,age,male,kids,street,city,_start,_end,_type%n" +
+            "0,:User:User1,foo,42,true,[\"a\",\"b\",\"c\"],,,,,%n" +
+            "1,:User,bar,42,,,,,,,%n" +
+            "2,:User,,12,,,,,,,%n" +
+            "20,:Address:Address1,Andrea,,,,Via Garibaldi, 7,Milano,,,%n" +
+            "21,:Address,Bar Sport,,,,,,,,%n" +
+            "22,:Address,,,,,via Benni,,,,%n" +
+            ",,,,,,,,0,1,KNOWS%n" +
+            ",,,,,,,,20,21,NEXT_DELIVERY");
+    private static final String EXPECTED_NEEDED_QUOTES = String.format("_id,_labels,name,age,male,kids,street,city,_start,_end,_type%n" +
+            "0,:User:User1,foo,42,true,\"[\"a\",\"b\",\"c\"]\",,,,,%n" +
+            "1,:User,bar,42,,,,,,,%n" +
+            "2,:User,,12,,,,,,,%n" +
+            "20,:Address:Address1,Andrea,,,,\"Via Garibaldi, 7\",Milano,,,%n" +
+            "21,:Address,Bar Sport,,,,,,,,%n" +
+            "22,:Address,,,,,via Benni,,,,%n" +
+            ",,,,,,,,0,1,KNOWS%n" +
+            ",,,,,,,,20,21,NEXT_DELIVERY");
+
+    private static GraphDatabaseService db;
+    private static File directory = new File("target/import");
+//    private static MiniDFSCluster miniDFSCluster;
+
+    static { //noinspection ResultOfMethodCallIgnored
+        directory.mkdirs();
+    }
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        db = new TestGraphDatabaseFactory()
+                .newImpermanentDatabaseBuilder()
+                .setConfig(GraphDatabaseSettings.load_csv_file_url_root, directory.getAbsolutePath())
+                .setConfig("apoc.export.file.enabled", "true")
+                .newGraphDatabase();
+        TestUtil.registerProcedure(db, ExportXls.class, Graphs.class);
+        db.execute("CREATE (f:User1:User {name:'foo',age:42,male:true,kids:['a','b','c'],location:point({longitude: 11.8064153, latitude: 48.1716114}),dob:date({ year:1984, month:10, day:11 }), created: datetime()})-[:KNOWS]->(b:User {name:'bar',age:42}),(c:User {age:12})").close();
+        db.execute("CREATE (f:Address1:Address {name:'Andrea', city: 'Milano', street:'Via Garibaldi, 7'})-[:NEXT_DELIVERY]->(a:Address {name: 'Bar Sport'}), (b:Address {street: 'via Benni'})").close();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        db.shutdown();
+    }
+
+    @Test
+    public void testExportAllXls() throws Exception {
+        File output = new File(directory, "all.xlsx");
+        TestUtil.testCall(db, "CALL apoc.export.xls.all({file},null)", map("file", output.getAbsolutePath()),
+                (r) -> assertResults(output, r, "database"));
+
+        assertExcelFileForGraph(output);
+    }
+
+    @Test
+    public void testExportGraphXls() throws Exception {
+        File output = new File(directory, "graph.xlsx");
+        TestUtil.testCall(db, "CALL apoc.graph.fromDB('test',{}) yield graph " +
+                        "CALL apoc.export.xls.graph(graph, {file},null) " +
+                        "YIELD nodes, relationships, properties, file, source,format, time " +
+                        "RETURN *", map("file", output.getAbsolutePath()),
+                (r) -> assertResults(output, r, "graph"));
+        assertExcelFileForGraph(output);
+    }
+
+    @Test
+    public void testExportQueryXls() throws Exception {
+        File output = new File(directory, "query.xlsx");
+        String query = "MATCH (u:User) return u.age, u.name, u.male, u.kids, labels(u)";
+        TestUtil.testCall(db, "CALL apoc.export.xls.query({query},{file},null)", map("file", output.getAbsolutePath(),"query",query),
+                (r) -> {
+                    assertTrue("Should get statement",r.get("source").toString().contains("statement: cols(5)"));
+                    assertEquals(output.getAbsolutePath(), r.get("file"));
+                    assertEquals("xls", r.get("format"));
+
+                });
+        assertExcelFileForQuery(output);
+    }
+
+
+    private void assertResults(File output, Map<String, Object> r, final String source) {
+        assertEquals(8L, r.get("nodes")); // we're exporting nodes with multiple label multiple times
+        assertEquals(2L, r.get("relationships"));
+        assertEquals(25L, r.get("properties"));
+        assertEquals(source + ": nodes(6), rels(2)", r.get("source"));
+        assertEquals(output.getAbsolutePath(), r.get("file"));
+        assertEquals("xls", r.get("format"));
+        assertTrue("Should get time greater than 0", ((long) r.get("time")) >= 0);
+    }
+
+    private void assertExcelFileForGraph(File output) {
+        try (InputStream inp = new FileInputStream(output); Transaction tx = db.beginTx()) {
+            Workbook wb = WorkbookFactory.create(inp);
+
+            int numberOfSheets = wb.getNumberOfSheets();
+            assertEquals(Iterables.count(db.getAllLabels()) + Iterables.count(db.getAllRelationshipTypes()), numberOfSheets);
+
+            for (Label label: db.getAllLabels()) {
+                long numberOfNodes = Iterators.count(db.findNodes(label));
+                Sheet sheet = wb.getSheet(label.name());
+                assertEquals(numberOfNodes, sheet.getLastRowNum());
+            }
+            tx.success();
+        } catch (IOException|InvalidFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void assertExcelFileForQuery(File output) {
+        try (InputStream inp = new FileInputStream(output)) {
+            Workbook wb = WorkbookFactory.create(inp);
+
+            int numberOfSheets = wb.getNumberOfSheets();
+            assertEquals(1, numberOfSheets);
+
+        } catch (IOException|InvalidFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
