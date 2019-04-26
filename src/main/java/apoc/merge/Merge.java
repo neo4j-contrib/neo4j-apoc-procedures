550fd669dd54cb209b0d830d3b58015a3cdca64c
InverseFalcon <inverse.falcon@gmail.com>
2019-04-26 15:27:43 -0700

    Added apoc.merge.node.eager() and apoc.merge.relationship.eager()
    These both accept a config map that can take 'onMatchProps' and 'onCreateProps' map properties.
    
    These procs can also match to multiple existing nodes unlike apoc.merge.node() and apoc.merge.relationship().
    
    Documentation to be checked in separately.

--- src/main/java/apoc/merge/Merge.java
+++ src/main/java/apoc/merge/Merge.java
@@ -1,7 +1,7 @@
 package apoc.merge;
 
 import apoc.result.*;
-import apoc.util.Util;
+import com.google.common.collect.Lists;
 import org.neo4j.graphdb.*;
 import org.neo4j.helpers.collection.Iterators;
 import org.neo4j.procedure.*;
@@ -25,7 +25,8 @@ public class Merge {
         String labels = labelNames.stream().map(s -> wrapInBacktics(s)).collect(Collectors.joining(":"));
 
         Map<String, Object> params = new HashMap<>();
-        params.put("props", Util.merge(identProps, props));
+        params.put("identProps", identProps);
+        params.put("props", props);
         String identPropsString = buildIdentPropsString(identProps);
 
         final String cypher = "MERGE (n:" + labels + "{" + identPropsString + "}) ON CREATE SET n += $props RETURN n";
@@ -33,18 +34,40 @@ public class Merge {
         return Stream.of(new NodeResult(node));
     }
 
-    private String wrapInBacktics(String s) {
-        return "`" + s + "`";
+    @Procedure(value="apoc.merge.node.eager", mode = Mode.WRITE, eager = true)
+    @Description("apoc.merge.node.eager(['Label'], identProps:{key:value, ...}, config:{onCreateProps:{key:value,...}, onMatchProps:{key:value,...}}) - merge node with dynamic labels, with support for setting properties ON CREATE or ON MATCH")
+    public Stream<NodeResult> nodeEager(@Name("label") List<String> labelNames, @Name("identProps") Map<String, Object> identProps, @Name(value="config", defaultValue = "{}") Map<String, Object> config) {
+        if ((identProps==null) || (identProps.isEmpty())) {
+            throw new IllegalArgumentException("you need to supply at least one identifying property for a merge");
+        }
+
+        validateKeys(config.keySet(), Lists.newArrayList("onCreateProps", "onMatchProps"));
+
+        String labels = labelNames.stream().map(s -> wrapInBacktics(s)).collect(Collectors.joining(":"));
+
+        Map<String, Object> params = new HashMap<>();
+
+        Map<String, Object> onCreateProps = (Map<String, Object>) config.getOrDefault("onCreateProps", Collections.emptyMap());
+        Map<String, Object> onMatchProps = (Map<String, Object>) config.getOrDefault("onMatchProps", Collections.emptyMap());
+
+        params.put("identProps", identProps);
+        params.put("onCreateProps", onCreateProps);
+        params.put("onMatchProps", onMatchProps);
+        String identPropsString = buildIdentPropsString(identProps);
+
+        final String cypher = "MERGE (n:" + labels + "{" + identPropsString + "}) ON CREATE SET n += $onCreateProps ON MATCH SET n += $onMatchProps RETURN n";
+        return db.execute(cypher, params ).columnAs("n").stream().map(node -> new NodeResult((Node) node));
     }
 
     @Procedure(mode = Mode.WRITE)
-    @Description("apoc.merge.relationship(startNode, relType,  {key:value, ...}, {key:value, ...}, endNode) - merge relationship with dynamic type")
+    @Description("apoc.merge.relationship(startNode, relType,  identProps:{key:value, ...}, {key:value, ...}, endNode) - merge relationship with dynamic type")
     public Stream<RelationshipResult> relationship(@Name("startNode") Node startNode, @Name("relationshipType") String relType,
                                                    @Name("identProps") Map<String, Object> identProps, @Name("props") Map<String, Object> props, @Name("endNode") Node endNode) {
         String identPropsString = buildIdentPropsString(identProps);
 
         Map<String, Object> params = new HashMap<>();
-        params.put("props", Util.merge(identProps, props));
+        params.put("identProps", identProps);
+        params.put("props", props != null ? props : Collections.emptyMap());
         params.put("startNode", startNode);
         params.put("endNode", endNode);
 
@@ -53,18 +76,47 @@ public class Merge {
         return Stream.of(new RelationshipResult(rel));
     }
 
+    @Procedure(value = "apoc.merge.relationship.eager", mode = Mode.WRITE, eager = true)
+    @Description("apoc.merge.relationship.eager(startNode, relType,  identProps:{key:value, ...}, config:{onCreateProps:{key:value, ...}, onMatchProps:{key:value, ...}}, endNode) - merge relationship with dynamic type, with support for setting properties ON CREATE or ON MATCH")
+    public Stream<RelationshipResult> relationshipEager(@Name("startNode") Node startNode, @Name("relationshipType") String relType,
+                                                   @Name("identProps") Map<String, Object> identProps, @Name("config") Map<String, Object> config, @Name("endNode") Node endNode) {
+        validateKeys(config.keySet(), Lists.newArrayList("onCreateProps", "onMatchProps"));
+
+        String identPropsString = buildIdentPropsString(identProps);
+
+        Map<String, Object> params = new HashMap<>();
+
+        Map<String, Object> onCreateProps = (Map<String, Object>) config.getOrDefault("onCreateProps", Collections.emptyMap());
+        Map<String, Object> onMatchProps = (Map<String, Object>) config.getOrDefault("onMatchProps", Collections.emptyMap());
+
+        params.put("identProps", identProps);
+        params.put("onCreateProps", onCreateProps);
+        params.put("onMatchProps", onMatchProps);
+        params.put("startNode", startNode);
+        params.put("endNode", endNode);
+
+        final String cypher = "WITH $startNode as startNode, $endNode as endNode MERGE (startNode)-[r:"+ wrapInBacktics(relType) +"{"+identPropsString+"}]->(endNode) ON CREATE SET r+= $onCreateProps ON MATCH SET r+= $onMatchProps RETURN r";
+        return db.execute(cypher, params ).columnAs("r").stream().map(rel -> new RelationshipResult((Relationship) rel));
+    }
+
+    private void validateKeys(Set<String> mapKeys, List<String> allowedKeys) {
+        Set<String> copy = new HashSet<>(mapKeys);
+        copy.removeAll(allowedKeys);
+
+        if (!copy.isEmpty()) {
+            throw new IllegalArgumentException("Config map may only take the following: " + allowedKeys.toString() + ". Unknown keys found: " + copy.toString());
+        }
+    }
+
+    private String wrapInBacktics(String s) {
+        return "`" + s + "`";
+    }
+
     private String buildIdentPropsString(Map<String, Object> identProps) {
         if (identProps==null) {
             return "";
         } else {
-            return identProps.keySet().stream().map(s -> "`"+s+"`:$props.`" + s+"`").collect(Collectors.joining(","));
+            return identProps.keySet().stream().map(s -> "`"+s+"`:$identProps.`" + s+"`").collect(Collectors.joining(","));
         }
     }
-
-    private Map<String, Object> buildParams(Map<String, Object> identProps, Map<String, Object> props) {
-        Map<String, Object> map = identProps == null ? new HashMap<>() : new HashMap<>(identProps);
-        map.put("props", props == null ? Collections.EMPTY_MAP : props);
-        return map;
-    }
-
 }
