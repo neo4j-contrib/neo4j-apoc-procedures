f0b7b07c4ef3947a649ad8e233ff2cb4a63f4f20
Lucas Raab <tuftedocelot@fastmail.fm>
2020-02-02 08:20:49 -0600

    First pass at rewriting the LoadLdap module
    Primary changes:
    * Switch to Apache's LDAP API 2.0 library. jldap is effectively
      abandoned and fails to connect to directory servers like 389DS due to
      no relaxed schema handling
    * Use LDAP URLs to specify server and search information and to leverage
      the URL parsing for correctness
    * Better handling of single vs multivalued attributes. If an attribute
      is multivalued, but only has one value, a list is still returned

--- src/test/java/apoc/load/LoadLdapTest.java
+++ src/test/java/apoc/load/LoadLdapTest.java
@@ -1,38 +1,70 @@
 package apoc.load;
 
-
-import com.novell.ldap.LDAPEntry;
-import com.novell.ldap.LDAPSearchResults;
+import org.apache.directory.api.ldap.model.cursor.CursorException;
+import org.apache.directory.api.ldap.model.cursor.SearchCursor;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.exception.LdapURLEncodingException;
+import org.apache.directory.api.ldap.model.message.SearchResultEntry;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
 public class LoadLdapTest {
+    @Test
+    public void testAnonBindViaNoDNorPW() throws LdapException, CursorException {
+        Map<String, String> connParms = new HashMap<>();
+        connParms.put("ldapURL", "ldap://ldap.forumsys.com:389/dc=example,dc=com?uid?one?(&(objectClass=*)(uid=training))");
+        LoadLdap.LDAPManager mgr = new LoadLdap.LDAPManager(connParms);
+        SearchCursor results = mgr.doSearch();
+        results.next();
+        SearchResultEntry le = (SearchResultEntry) results.get();
+        assertEquals("uid=training,dc=example,dc=com", le.getEntry().getDn().toString());
+        assertEquals("training", le.getEntry().get("uid").get().toString());
+    }
 
     @Test
     public void testLoadLDAP() throws Exception {
-        Map<String, Object> connParms = new HashMap<>();
-        connParms.put("ldapHost", "ldap.forumsys.com");
-        connParms.put("ldapPort", 389l);
+        Map<String, String> connParms = new HashMap<>();
+        connParms.put("ldapURL", "ldap://ldap.forumsys.com:389/dc=example,dc=com?uid?one?(&(objectClass=*)(uid=training))");
         connParms.put("loginDN", "cn=read-only-admin,dc=example,dc=com");
         connParms.put("loginPW", "password");
-        LoadLdap.LDAPManager mgr = new LoadLdap.LDAPManager(LoadLdap.getConnectionMap(connParms));
-        Map<String, Object> searchParms = new HashMap<>();
-        searchParms.put("searchBase", "dc=example,dc=com");
-        searchParms.put("searchScope", "SCOPE_ONE");
-        searchParms.put("searchFilter", "(&(objectClass=*)(uid=training))");
-        ArrayList<String> ats = new ArrayList<>();
-        ats.add("uid");
-        searchParms.put("attributes", ats);
-        LDAPSearchResults results = mgr.doSearch(searchParms);
-        LDAPEntry le = results.next();
-        assertEquals("uid=training,dc=example,dc=com", le.getDN());
-        assertEquals("training", le.getAttribute("uid").getStringValue());
+        LoadLdap.LDAPManager mgr = new LoadLdap.LDAPManager(connParms);
+        SearchCursor results = mgr.doSearch();
+        results.next();
+        SearchResultEntry le = (SearchResultEntry) results.get();
+        assertEquals("uid=training,dc=example,dc=com", le.getEntry().getDn().toString());
+        assertEquals("training", le.getEntry().get("uid").get().toString());
     }
 
-}
+    @Test
+    public void testLDAPAggregation() throws LdapURLEncodingException {
+        Map<String, String> connParms = new HashMap<>();
+        connParms.put("ldapURL", "ldap://ipa.demo1.freeipa.org/dc=demo1,dc=freeipa,dc=org?uid?sub?(objectClass=posixAccount)");
+        connParms.put("loginDN", "uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org");
+        connParms.put("loginPW", "Secret123");
+        LoadLdap.LDAPManager mgr = new LoadLdap.LDAPManager(connParms);
+        Stream<LDAPResult> result = mgr.executeSearch();
+        /*
+            Note, the expect result is prone to changing as the server is refreshed
+            every so often. Best to manually verify the count using an LDAP client
+            before running the test
+         */
+        assertEquals(9, result.count());
+    }
 
+    @Test
+    public void testLDAPSConnection() throws LdapURLEncodingException {
+        // Assume that we're using a truststore that contains the Let's Encrypt CA (which is in the Java default)
+        Map<String, String> connParms = new HashMap<>();
+        connParms.put("ldapURL", "ldaps://db.debian.org/dc=debian,dc=org?uid?sub?(uid=dbharris)");
+        connParms.put("loginDN", "");
+        connParms.put("loginPW", "");
+        LoadLdap.LDAPManager mgr = new LoadLdap.LDAPManager(connParms);
+        Stream<LDAPResult> result = mgr.executeSearch();
+        assertEquals(1, result.count());
+    }
+}
