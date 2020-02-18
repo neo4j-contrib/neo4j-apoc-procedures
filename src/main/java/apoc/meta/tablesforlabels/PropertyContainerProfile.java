8553664f33040e509eed575d5549531c8446ad16
Shashi Dookhee <shashi@neo4j.com>
2020-02-17 16:32:39 -0800

    Fixed bug with constraint checking after transaction has closed

--- src/main/java/apoc/meta/tablesforlabels/PropertyContainerProfile.java
+++ src/main/java/apoc/meta/tablesforlabels/PropertyContainerProfile.java
@@ -45,6 +45,7 @@ public class PropertyContainerProfile {
             tracker.addObservation(n.getProperty(propName));
 
             tracker.mandatory = false;
+            this.isNode = isNode;
         }
     }
 
@@ -56,13 +57,11 @@ public class PropertyContainerProfile {
 
                 // Check for node constraints
 
-                for (Map.Entry<String,List<ConstraintDefinition>> entry : ConstraintTracker.nodeConstraints.entrySet()) {
-                    for (ConstraintDefinition cd : entry.getValue()) {
-                        for (String pk : cd.getPropertyKeys()) {
-                            if (this.profile.containsKey(pk)) {
-                                tracker = this.profile.get(pk);
-                                tracker.mandatory = true;
-                            }
+                for (Map.Entry<String,List<String>> entry : ConstraintTracker.nodeConstraints.entrySet()) {
+                    for (String pk : entry.getValue()) {
+                        if (this.profile.containsKey(pk)) {
+                            tracker = this.profile.get(pk);
+                            tracker.mandatory = true;
                         }
                     }
                 }
@@ -70,13 +69,11 @@ public class PropertyContainerProfile {
 
                 // Check for relationship constraints
 
-                for (Map.Entry<String,List<ConstraintDefinition>> entry : ConstraintTracker.relConstraints.entrySet()) {
-                    for (ConstraintDefinition cd : entry.getValue()) {
-                        for (String pk : cd.getPropertyKeys()) {
-                            if (this.profile.containsKey(pk)) {
-                                tracker = this.profile.get(pk);
-                                tracker.mandatory = true;
-                            }
+                for (Map.Entry<String,List<String>> entry : ConstraintTracker.relConstraints.entrySet()) {
+                    for (String pk : entry.getValue()) {
+                        if (this.profile.containsKey(pk)) {
+                            tracker = this.profile.get(pk);
+                            tracker.mandatory = true;
                         }
                     }
                 }
