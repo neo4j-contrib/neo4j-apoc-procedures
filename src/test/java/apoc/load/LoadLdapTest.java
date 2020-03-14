fcdb9a1fe4555dd2cbb62dc896fe784b25fb9fd9
Lucas Raab <tuftedocelot@fastmail.fm>
2020-03-13 21:20:24 -0500

    Use ApacheDS as a testing framework to test against a real ldap server
    Rather than (ab)use existing LDAP servers, use ApacheDS's testing
    framework to create LDAP servers on the fly

--- src/test/java/apoc/load/LoadLdapTest.java
+++ src/test/java/apoc/load/LoadLdapTest.java
@@ -2,9 +2,18 @@ package apoc.load;
 
 import apoc.ApocConfiguration;
 import apoc.util.TestUtil;
+import org.apache.directory.server.annotations.CreateLdapServer;
+import org.apache.directory.server.annotations.CreateTransport;
+import org.apache.directory.server.core.annotations.ApplyLdifFiles;
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.annotations.CreatePartition;
+import org.apache.directory.server.core.integ.AbstractLdapTestUnit;
+import org.apache.directory.server.core.integ.FrameworkRunner;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
@@ -18,24 +27,69 @@ import static apoc.util.MapUtil.map;
 import static apoc.util.TestUtil.testCall;
 import static apoc.util.TestUtil.testResult;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 
-public class LoadLdapTest {
+@RunWith(FrameworkRunner.class)
+@CreateDS(name = "testDS",
+        partitions = {
+            @CreatePartition(name = "test", suffix = "dc=neo4j,dc=test")
+        })
+@CreateLdapServer(
+        transports = {
+                @CreateTransport(protocol = "LDAP", address = "localhost")
+        },
+        allowAnonymousAccess = true
+)
+@ApplyLdifFiles({"users.ldif"})
+public class LoadLdapTest extends AbstractLdapTestUnit {
     private GraphDatabaseService db;
+    private int ldapServerPort = ldapServer.getPort();
+
+    private List<Map<String, Object>> consumeResults(Result rows) {
+        List<Map<String, Object>> results = new ArrayList<>();
+        while (rows.hasNext()) {
+            Map<String, Object> row = rows.next();
+            results.add(row);
+        }
+        return results;
+    }
 
     @Before
     public void setUp() throws Exception {
         db = TestUtil.apocGraphDatabaseBuilder().newGraphDatabase();
         ApocConfiguration.initialize((GraphDatabaseAPI) db);
-        ApocConfiguration.addToConfig(map("ldap.forumsys_noauth.url", "ldap://ldap.forumsys.com:389/dc=example,dc=com?uid?one?(&(objectClass=*)(uid=training))"));
-        ApocConfiguration.addToConfig(map("ldap.forumsys_auth.url", "ldap://ldap.forumsys.com:389/dc=example,dc=com?uid?one?(&(objectClass=*)(uid=training))"));
-        ApocConfiguration.addToConfig(map("ldap.forumsys_auth.username", "cn=read-only-admin,dc=example,dc=com"));
-        ApocConfiguration.addToConfig(map("ldap.forumsys_auth.password", "password"));
-        ApocConfiguration.addToConfig(map("ldap.freeipa_demo.url", "ldap://ipa.demo1.freeipa.org/dc=demo1,dc=freeipa,dc=org?uid?sub?(objectClass=posixAccount)"));
-        ApocConfiguration.addToConfig(map("ldap.freeipa_demo.username", "uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org"));
-        ApocConfiguration.addToConfig(map("ldap.freeipa_demo.password", "Secret123"));
-        ApocConfiguration.addToConfig(map("ldap.debian.url", "ldaps://db.debian.org/dc=debian,dc=org?uid?sub?(uid=dbharris)"));
+        String unsecuredUrl = String.format("ldap://localhost:%d/dc=neo4j,dc=test?cn?sub?(&(objectClass=person)(cn=Agent Smith))", ldapServerPort);
+        ApocConfiguration.addToConfig(map("ldap.localhost_noauth.url", unsecuredUrl));
+        ApocConfiguration.addToConfig(map("ldap.localhost_auth.url", unsecuredUrl));
+        ApocConfiguration.addToConfig(map("ldap.localhost_auth.user", "uid=admin,ou=system"));
+        ApocConfiguration.addToConfig(map("ldap.localhost_auth.password", "secret"));
         TestUtil.registerProcedure(db, LoadLdap.class);
+    }
+
+    @Test
+    public void testLoadNoAuth() {
+        Map<String, Object> agentSmith = new HashMap<>();
+        agentSmith.put("dn", "cn=Agent Smith,ou=Users,dc=neo4j,dc=test");
+        agentSmith.put("cn", "Agent Smith");
+        testCall(db, "CALL apoc.load.ldapfromconfig('localhost_noauth')", (row) -> assertEquals(row, map("entry", agentSmith)));
+    }
+
+    @Test
+    public void testLoadAuth() {
+        Map<String, Object> agentSmith = new HashMap<>();
+        agentSmith.put("dn", "cn=Agent Smith,ou=Users,dc=neo4j,dc=test");
+        agentSmith.put("cn", "Agent Smith");
+        testCall(db, "CALL apoc.load.ldapfromconfig('localhost_auth')", (row) -> assertEquals(row, map("entry", agentSmith)));
+    }
+
+    @Test
+    public void testInlineConfig() {
+        Map<String, Object> agentSmith = new HashMap<>();
+        agentSmith.put("dn", "cn=Agent Smith,ou=Users,dc=neo4j,dc=test");
+        agentSmith.put("cn", "Agent Smith");
+        testCall(db, String.format("CALL apoc.load.ldap('ldap://localhost:%d/dc=neo4j,dc=test?cn?sub?(&(objectClass=person)(cn=Agent Smith))', ", ldapServerPort) +
+                        "{credentials: {user: 'uid=admin,ou=system', password: 'secret'}})",
+                (row) -> assertEquals(row, map("entry", agentSmith)));
     }
 
     @After
@@ -43,50 +97,9 @@ public class LoadLdapTest {
         db.shutdown();
     }
 
-    private List<Map<String, Object>> consumeResults(Result rows) {
-        List<Map<String, Object>> results = new ArrayList<>();
-        while (rows.hasNext()) {
-            Map<String, Object> row = rows.next();
-            System.out.println(row);
-            results.add(row);
-        }
-        return results;
-    }
-
-    @Test
-    public void testLoadNoAuth() {
-        Map<String, Object> training = new HashMap<>();
-        training.put("dn", "uid=training,dc=example,dc=com");
-        training.put("uid", "training");
-        testCall(db, "CALL apoc.load.ldapfromconfig('forumsys_noauth')", (row) -> assertEquals(row, map("entry", training)));
-    }
-
-    @Test
-    public void testLoadAuth() {
-        Map<String, Object> training = new HashMap<>();
-        training.put("dn", "uid=training,dc=example,dc=com");
-        training.put("uid", "training");
-        testCall(db, "CALL apoc.load.ldapfromconfig('forumsys_auth')", (row) -> assertEquals(row, map("entry", training)));
-    }
-
-    @Test
-    public void testNonconformantSchema() {
-        // if we got results back, then the search succeeded
-        testResult(db, "CALL apoc.load.ldapfromconfig('freeipa_demo')", (row) -> assertTrue(row.hasNext()));
-    }
-
-    @Test
-    public void testInlineConfig() {
-        Map<String, Object> training = new HashMap<>();
-        training.put("dn", "uid=training,dc=example,dc=com");
-        training.put("uid", "training");
-        testCall(db, "CALL apoc.load.ldap('ldap://ldap.forumsys.com:389/dc=example,dc=com?uid?one?(&(objectClass=*)(uid=training))', " +
-                "{credentials: {user: 'cn=read-only-admin,dc=example,dc=com', password: 'password'}})",
-                (row) -> assertEquals(row, map("entry", training)));
-    }
-
+    /*
     @Test
     public void testPagingSupport() {
         testResult(db, "CALL apoc.load.ldap('ldaps://db.debian.org/dc=debian,dc=org?uid?sub?(objectclass=inetorgperson)')", (row)->assertTrue(consumeResults(row).size()>1000));
-    }
+    }*/
 }
