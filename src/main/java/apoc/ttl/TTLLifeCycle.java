33c1a5426b1b52df23fa8f4ac2eff486dcbce6d7
Mark Needham <m.h.needham@gmail.com>
2020-03-27 14:23:12 +0000

    TTL procs in own file

--- src/main/java/apoc/ttl/TTLLifeCycle.java
+++ src/main/java/apoc/ttl/TTLLifeCycle.java
@@ -2,23 +2,15 @@ package apoc.ttl;
 
 import apoc.ApocConfiguration;
 import apoc.util.Util;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.QueryStatistics;
 import org.neo4j.graphdb.Result;
-import org.neo4j.procedure.Description;
-import org.neo4j.procedure.Mode;
-import org.neo4j.procedure.Name;
-import org.neo4j.procedure.Procedure;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.Log;
 import org.neo4j.scheduler.Group;
 import org.neo4j.scheduler.JobHandle;
 import org.neo4j.scheduler.JobScheduler;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
-import org.neo4j.logging.Log;
 
 import java.util.concurrent.TimeUnit;
-
-import static apoc.date.Date.unit;
 
 /**
  * @author mh
@@ -52,20 +44,6 @@ public class TTLLifeCycle {
 
         ttlJobHandle = scheduler.scheduleRecurring(TTL_GROUP, () -> expireNodes(limit), ttlSchedule, ttlSchedule, TimeUnit.SECONDS);
     }
-
-    @Procedure(mode = Mode.WRITE)
-	@Description("CALL apoc.ttl.expireAtInstant(node,time,'time-unit') - expire node at specified time by setting :TTL label and `ttl` property")
-	public void expire(@Name("node") Node node, @Name("time") long time, @Name("timeUnit") String timeUnit) {
-		node.addLabel(Label.label("TTL"));
-		node.setProperty("ttl",unit(timeUnit).toMillis(time));
-	}
-
-	@Procedure(mode = Mode.WRITE)
-	@Description("CALL apoc.ttl.expireAfterTimeLength(node,timeDelta,'time-unit') - expire node after specified length of time time by setting :TTL label and `ttl` property")
-	public void expireIn(@Name("node") Node node, @Name("timeDelta") long time, @Name("timeUnit") String timeUnit) {
-		node.addLabel(Label.label("TTL"));
-		node.setProperty("ttl",System.currentTimeMillis() + unit(timeUnit).toMillis(time));
-	}
 
     public void expireNodes(long limit) {
         try {
