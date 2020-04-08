3be176ebd893cf5735015cb10170cda565756e96
Mark Needham <m.h.needham@gmail.com>
2020-04-08 17:50:52 +0100

    fixing import JSON compilation issues

--- src/test/java/apoc/export/json/ImportJsonTest.java
+++ src/test/java/apoc/export/json/ImportJsonTest.java
@@ -1,16 +1,17 @@
 package apoc.export.json;
 
+import apoc.ApocSettings;
+import apoc.graph.Graphs;
 import apoc.util.TestUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.spatial.Point;
-import org.neo4j.test.TestGraphDatabaseFactory;
+import org.neo4j.test.rule.DbmsRule;
+import org.neo4j.test.rule.ImpermanentDbmsRule;
 import org.neo4j.values.storable.CoordinateReferenceSystem;
 import org.neo4j.values.storable.DurationValue;
 import org.neo4j.values.storable.PointValue;
@@ -24,22 +25,16 @@ import static apoc.util.MapUtil.map;
 
 public class ImportJsonTest {
 
-    private static GraphDatabaseService db;
     private static File directory = new File("docs/asciidoc/data/exportJSON");
+
+    @Rule
+    public DbmsRule db = new ImpermanentDbmsRule()
+            .withSetting(GraphDatabaseSettings.load_csv_file_url_root, directory.toPath().toAbsolutePath())
+            .withSetting(ApocSettings.apoc_import_file_enabled, true);
 
     @Before
     public void setUp() throws Exception {
-        db = new TestGraphDatabaseFactory()
-                .newImpermanentDatabaseBuilder()
-                .setConfig(GraphDatabaseSettings.load_csv_file_url_root, directory.getAbsolutePath())
-                .setConfig("apoc.import.file.enabled", "true")
-                .newGraphDatabase();
         TestUtil.registerProcedure(db, ImportJson.class);
-    }
-
-    @After
-    public void tearDown() {
-        db.shutdown();
     }
 
     @Test
@@ -61,25 +56,28 @@ public class ImportJsonTest {
                     Assert.assertEquals(4L, r.get("rows"));
                     Assert.assertEquals(true, r.get("done"));
 
-                    final long countNodes = db.execute("MATCH (n:User) RETURN count(n) AS count")
-                            .<Long>columnAs("count")
-                            .next();
-                    Assert.assertEquals(3L, countNodes);
+                    try(Transaction tx = db.beginTx()) {
+                        final long countNodes = tx.execute("MATCH (n:User) RETURN count(n) AS count")
+                                .<Long>columnAs("count")
+                                .next();
+                        Assert.assertEquals(3L, countNodes);
 
-                    final long countRels = db.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
-                            .<Long>columnAs("count")
-                            .next();
-                    Assert.assertEquals(1L, countRels);
+                        final long countRels = tx.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
+                                .<Long>columnAs("count")
+                                .next();
+                        Assert.assertEquals(1L, countRels);
 
-                    final Map<String, Object> props = db.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
-                            .<Node>columnAs("n")
-                            .next()
-                            .getAllProperties();
-                    Assert.assertEquals(9, props.size());
-                    Assert.assertEquals("wgs-84", props.get("place.crs"));
-                    Assert.assertEquals(33.46789D, props.get("place.latitude"));
-                    Assert.assertEquals(13.1D, props.get("place.longitude"));
-                    Assert.assertFalse(props.containsKey("place"));
+                        final Map<String, Object> props = tx.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
+                                .<Node>columnAs("n")
+                                .next()
+                                .getAllProperties();
+
+                        Assert.assertEquals(9, props.size());
+                        Assert.assertEquals("wgs-84", props.get("place.crs"));
+                        Assert.assertEquals(33.46789D, props.get("place.latitude"));
+                        Assert.assertEquals(13.1D, props.get("place.longitude"));
+                        Assert.assertFalse(props.containsKey("place"));
+                    }
                 }
         );
     }
@@ -105,32 +103,34 @@ public class ImportJsonTest {
                     Assert.assertEquals(4L, r.get("rows"));
                     Assert.assertEquals(true, r.get("done"));
 
-                    final long countNodes = db.execute("MATCH (n:User) RETURN count(n) AS count")
-                            .<Long>columnAs("count")
-                            .next();
-                    Assert.assertEquals(3L, countNodes);
+                    try(Transaction tx = db.beginTx()) {
+                        final long countNodes = tx.execute("MATCH (n:User) RETURN count(n) AS count")
+                                .<Long>columnAs("count")
+                                .next();
+                        Assert.assertEquals(3L, countNodes);
 
-                    final long countRels = db.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
-                            .<Long>columnAs("count")
-                            .next();
-                    Assert.assertEquals(1L, countRels);
+                        final long countRels = tx.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
+                                .<Long>columnAs("count")
+                                .next();
+                        Assert.assertEquals(1L, countRels);
 
-                    final Map<String, Object> props = db.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
-                            .<Node>columnAs("n")
-                            .next()
-                            .getAllProperties();
-                    Assert.assertEquals(7, props.size());
-                    Assert.assertTrue(props.get("place") instanceof PointValue);
-                    PointValue point = (PointValue) props.get("place");
-                    final PointValue pointValue = Values.pointValue(CoordinateReferenceSystem.WGS84, 33.46789D, 13.1D);
-                    Assert.assertTrue(point.equals((Point) pointValue));
-                    Assert.assertTrue(props.get("born") instanceof LocalDateTime);
+                        final Map<String, Object> props = tx.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
+                                .<Node>columnAs("n")
+                                .next()
+                                .getAllProperties();
+                        Assert.assertEquals(7, props.size());
+                        Assert.assertTrue(props.get("place") instanceof PointValue);
+                        PointValue point = (PointValue) props.get("place");
+                        final PointValue pointValue = Values.pointValue(CoordinateReferenceSystem.WGS84, 33.46789D, 13.1D);
+                        Assert.assertTrue(point.equals((Point) pointValue));
+                        Assert.assertTrue(props.get("born") instanceof LocalDateTime);
 
-                    Relationship rel = db.execute("MATCH ()-[r:KNOWS]->() RETURN r")
-                            .<Relationship>columnAs("r")
-                            .next();
-                    Assert.assertTrue(rel.getProperty("bffSince") instanceof DurationValue);
-                    Assert.assertEquals("P5M1DT12H", rel.getProperty("bffSince").toString());
+                        Relationship rel = tx.execute("MATCH ()-[r:KNOWS]->() RETURN r")
+                                .<Relationship>columnAs("r")
+                                .next();
+                        Assert.assertTrue(rel.getProperty("bffSince") instanceof DurationValue);
+                        Assert.assertEquals("P5M1DT12H", rel.getProperty("bffSince").toString());
+                    }
                 }
         );
     }
