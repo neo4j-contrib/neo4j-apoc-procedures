9f54111840337bbb106bcba795afb8e166990704
Mark Needham <m.h.needham@gmail.com>
2020-05-27 08:58:31 +0100

    Docs path manipulation (#1527)
    * add path functions docs
    
    * fix headings

--- src/main/java/apoc/path/Paths.java
+++ src/main/java/apoc/path/Paths.java
@@ -1,11 +1,11 @@
 package apoc.path;
 
-import apoc.Description;
 import org.neo4j.driver.internal.util.Iterables;
 import org.neo4j.graphalgo.impl.util.PathImpl;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
 
@@ -25,7 +25,9 @@ public class Paths {
         PathImpl.Builder builder = new PathImpl.Builder(startNode);
         if (rels != null) {
             for (Relationship rel : rels) {
-                builder = builder.push(rel);
+                if(rel != null) {
+                    builder = builder.push(rel);
+                }
             }
         }
         return builder.build();
