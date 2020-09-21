dcc80ebc549490e90fd3a73a3d2c09aca3e1ddec
Stefan Armbruster <stefan@armbruster-it.de>
2020-09-21 11:42:54 +0200

    fix #1666: expose meta data to trigger parameters

--- core/src/test/java/apoc/trigger/TriggerTest.java
+++ core/src/test/java/apoc/trigger/TriggerTest.java
@@ -5,9 +5,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.impl.coreapi.TransactionImpl;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
+import java.util.Collections;
 import java.util.Map;
 
 import static apoc.ApocSettings.apoc_trigger_enabled;
@@ -121,9 +125,6 @@ public class TriggerTest {
         });
     }
 
-
-
-
     @Test
     public void testTxId() throws Exception {
         db.executeTransactionally("CALL apoc.trigger.add('txinfo','UNWIND $createdNodes AS n SET n.txId = $transactionId, n.txTime = $commitTime',{phase:'after'})");
@@ -131,6 +132,29 @@ public class TriggerTest {
         TestUtil.testCall(db, "MATCH (f:Bar) RETURN f", (row) -> {
             assertEquals(true, (Long) ((Node) row.get("f")).getProperty("txId") > -1L);
             assertEquals(true, (Long) ((Node) row.get("f")).getProperty("txTime") > start);
+        });
+    }
+
+    @Test
+    public void testMetaDataBefore() {
+        testMetaData("before");
+    }
+
+    @Test
+    public void testMetaDataAfter() {
+        testMetaData("after");
+    }
+
+    private void testMetaData(String phase) {
+        db.executeTransactionally("CALL apoc.trigger.add('txinfo','UNWIND $createdNodes AS n SET n += $metaData',{phase:$phase})", Collections.singletonMap("phase", phase));
+        try (Transaction tx = db.beginTx()) {
+            KernelTransaction ktx = ((TransactionImpl)tx).kernelTransaction();
+            ktx.setMetaData(Collections.singletonMap("txMeta", "hello"));
+            tx.execute("CREATE (f:Bar)");
+            tx.commit();
+        }
+        TestUtil.testCall(db, "MATCH (f:Bar) RETURN f", (row) -> {
+            assertEquals("hello",  ((Node) row.get("f")).getProperty("txMeta") );
         });
     }
 
