d7e021f96feb1eda6af886635744a528616778bb
Jacob Davis-Hansson <jakewins@gmail.com>
2020-10-06 12:59:01 -0500

    Handle Optional.empty() function descriptions (#1682)
    * Handle Optional.empty() function descriptions
    
    This handles loading a stored APOC custom state where
    a function or procedure had an Optional.empty() description.
    
    Jackson serialized those to {"present::false}, which then broke
    Apocs assumptions about the schema here.
    
    This makes it so we handle any in-the-wild incarnations of this,
    and modifies our write path to always ensure the value is a string
    
    * Fix review comments on handling Optional.empty()
    
    Standardize on null for strings that were serialized to maps.

--- src/test/java/apoc/custom/CypherProceduresStorageTest.java
+++ src/test/java/apoc/custom/CypherProceduresStorageTest.java
@@ -1,21 +1,35 @@
 package apoc.custom;
 
+import apoc.util.JsonUtil;
+import apoc.util.MapUtil;
 import apoc.util.TestUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.internal.kernel.api.exceptions.KernelException;
+import org.neo4j.internal.kernel.api.procs.Neo4jTypes;
+import org.neo4j.internal.kernel.api.procs.QualifiedName;
+import org.neo4j.internal.kernel.api.procs.UserFunctionSignature;
 import org.neo4j.io.fs.FileUtils;
+import org.neo4j.kernel.impl.core.GraphPropertiesProxy;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.Log;
+import org.neo4j.scheduler.JobScheduler;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import static apoc.util.Util.map;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author mh
@@ -127,5 +141,65 @@ public class CypherProceduresStorageTest {
                 "[['int','int'],['float','float'],['string','string'],['map','map'],['list int','list int'],['bool','bool'],['date','date'],['datetime','datetime'],['point','point']], true)");
         restartDb();
         TestUtil.testCall(db, "return custom.answer(42,3.14,'foo',{a:1},[1],true,date(),datetime(),point({x:1,y:2})) as data", (row) -> assertEquals(9, ((List)row.get("data")).size()));
+    }
+
+    @Test
+    public void handleFunctionsWithNoneDescriptions() throws Exception {
+        // Anyone that knows the right knobs can store whatever they like in the graph properties store;
+        // hence we should make sure we handle the case of someone having modified our JSON blob
+        JobScheduler fakeScheduler = Mockito.mock(JobScheduler.class);
+        Log fakeLog = Mockito.mock(Log.class);
+        GraphDatabaseAPI gds = (GraphDatabaseAPI) this.db;
+
+        // Given I've stored a function with no description..
+        CypherProcedures.CustomProcedureStorage.storeFunction(gds, new UserFunctionSignature( new QualifiedName(new String[]{"apoc"}, "nodescription"),
+                Collections.emptyList(), Neo4jTypes.NTBoolean, "", new String[]{}, null, false), "my query", false);
+
+        // When I load the stored state..
+        CypherProcedures.CustomProcedureStorage store = new CypherProcedures.CustomProcedureStorage(fakeScheduler, gds, fakeLog);
+        store.available();
+
+        // Then its ok
+        List<CypherProcedures.CustomProcedureInfo> result = store.list();
+        assertEquals(1, result.size());
+        CypherProcedures.CustomProcedureInfo myFuncInfo = result.get(0);
+        assertNull(myFuncInfo.description);
+    }
+
+    @Test
+    public void handlePersistedStateWithMapAsFunctionDescription() throws Exception {
+        // Anyone that knows the right knobs can store whatever they like in the graph properties store;
+        // hence we should make sure we handle the case of someone having modified our JSON blob
+        JobScheduler fakeScheduler = Mockito.mock(JobScheduler.class);
+        Log fakeLog = Mockito.mock(Log.class);
+        GraphDatabaseAPI gds = (GraphDatabaseAPI) this.db;
+
+        // Given the database contains an old state blob with a map in the `description` field of a function..
+        UserFunctionSignature signature = new UserFunctionSignature(new QualifiedName(new String[]{"apoc"}, "nodescription"),
+                Collections.emptyList(), Neo4jTypes.NTBoolean, "", new String[]{}, null, false);
+        try(Transaction tx = gds.beginTx()) {
+            GraphPropertiesProxy props = CypherProcedures.CustomProcedureStorage.getProperties(gds);
+            props.setProperty(CypherProcedures.CustomProcedureStorage.APOC_CUSTOM, JsonUtil.writeValueAsString(MapUtil.map(
+                "procedures", MapUtil.map(),
+                    "functions", MapUtil.map(
+                            "myfunc", MapUtil.map(
+                                    "statement", "..",
+                                    "forceSingle", false,
+                                    "signature", signature.toString(),
+                                    "description", MapUtil.map("present", false))
+                    )
+            )));
+            tx.success();
+        }
+
+        // When I load the stored state..
+        CypherProcedures.CustomProcedureStorage store = new CypherProcedures.CustomProcedureStorage(fakeScheduler, gds, fakeLog);
+        store.available();
+
+        // Then its ok
+        List<CypherProcedures.CustomProcedureInfo> result = store.list();
+        assertEquals(1, result.size());
+        CypherProcedures.CustomProcedureInfo myFuncInfo = result.get(0);
+        assertNull(myFuncInfo.description);
     }
 }
