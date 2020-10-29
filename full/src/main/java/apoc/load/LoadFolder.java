8fc372d9b984ddac7b898778d307a1bf2e669671
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2020-10-29 13:17:05 +0100

    removed async procedures

--- full/src/main/java/apoc/load/LoadFolder.java
+++ full/src/main/java/apoc/load/LoadFolder.java
@@ -4,8 +4,6 @@ import apoc.Extended;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 import org.apache.commons.io.FileUtils;
@@ -13,7 +11,6 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.nio.file.*;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -25,12 +22,6 @@ public class LoadFolder {
     @Context
     public Log log;
 
-    @Context
-    public GraphDatabaseService db;
-
-    public static final HashMap<String, FolderListener> folderListenerList = new HashMap<>();
-
-
     public static class UrlResult {
         public final String url;
 
@@ -38,71 +29,6 @@ public class LoadFolder {
             this.url = url;
         }
     }
-
-    public static class FolderListenerResult {
-        public final String name;
-        public final String cypher;
-        public final String pattern;
-
-        public FolderListenerResult(String name, String cypher, String pattern) {
-            this.name = name;
-            this.cypher = cypher;
-            this.pattern = pattern;
-        }
-    }
-
-
-    @Procedure(name = "apoc.load.folder.add", mode = Mode.WRITE)
-    @Description("apoc.load.folder.add(name, pattern, cypher, {}) YIELD name, pattern, cypher - Add or replace a WatchListener with a specific name, pattern and cypher")
-    public Stream<FolderListenerResult> add(@Name("name") String name, @Name("pattern") String pattern, @Name("cypher") String cypher, @Name(value = "config", defaultValue = "{}") Map<String, Object> config) {
-
-        FolderListenerResult folderListenerResult = new FolderListenerResult(name, cypher, pattern);
-
-        // replace eventual listener with same name
-        FolderListener folderListenerFromMap = folderListenerList.get(name);
-        if (folderListenerFromMap != null) {
-            folderListenerFromMap.stop();
-        }
-
-        FolderListener folderListener = new FolderListener((kind, file) -> {
-            try (Transaction tx = db.beginTx()) {
-                tx.execute(cypher);
-                log.info("Executing query - " + cypher);
-                tx.commit();
-            } catch (Exception e) {
-                log.info("Error while executing query - " + cypher);
-                throw new RuntimeException(e);
-            }
-            return null;
-        }, log, folderListenerResult);
-        folderListener.start();
-        folderListenerList.put(name, folderListener);
-
-        return Stream.of(folderListenerResult);
-    }
-
-
-    @Procedure("apoc.load.folder.remove")
-    @Description("apoc.load.folder.remove(name) YIELD name, pattern, cypher - Remove a WatchListener by name")
-    public Stream<FolderListenerResult> remove(@Name("name") String name) {
-
-        FolderListener folderListener = folderListenerList.remove(name);
-        if (folderListener == null) {
-            throw new RuntimeException("Listener with name: " + name + " doesn't exists");
-        }
-
-        folderListener.stop();
-        log.info("Stopped listener with name: " + name);
-        return Stream.of(folderListener.getFolderListenerResult());
-    }
-
-
-    @Procedure("apoc.load.folder.list")
-    @Description("apoc.load.folder.list() YIELD name, pattern, cypher - List of all folder listener")
-    public Stream<FolderListenerResult> list() {
-        return folderListenerList.values().stream().map(FolderListener::getFolderListenerResult);
-    }
-
 
     @Procedure("apoc.load.folder")
     @Description("apoc.load.folder('pattern',{config}) YIELD url - List of all files in import folder")
