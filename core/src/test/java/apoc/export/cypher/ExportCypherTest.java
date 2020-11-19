c53d0e2a94c7f6379d0aad3d5c49c038e6c62dcb
Phoosha <futur.andy@googlemail.com>
2020-11-19 15:13:47 +0100

    fixes #1705: Quote identifiers starting with dollar (#1708)

--- core/src/test/java/apoc/export/cypher/ExportCypherTest.java
+++ core/src/test/java/apoc/export/cypher/ExportCypherTest.java
@@ -651,6 +651,24 @@ public class ExportCypherTest {
                 });
     }
 
+    @Test
+    public void shouldQuotePropertyNameStartingWithDollarCharacter() {
+        db.executeTransactionally("CREATE (n:Bar:Baz{name: 'A', `$lock`: true})");
+        String query = "MATCH (n:Baz) RETURN n";
+        final String expected = "UNWIND [{name:\"A\", properties:{`$lock`:true}}] AS row\n" +
+                "CREATE (n:Bar{name: row.name}) SET n += row.properties SET n:Baz";
+        TestUtil.testCall(db, "CALL apoc.export.cypher.query($query, $file, $config)",
+                map("file", null, "query", query, "config", map("format", "plain", "stream", true)), (r) -> {
+                    final String cypherStatements = (String) r.get("cypherStatements");
+                    String unwind = Stream.of(cypherStatements.split(";"))
+                            .map(String::trim)
+                            .filter(s -> s.startsWith("UNWIND"))
+                            .findFirst()
+                            .orElse(null);
+                    assertEquals(expected, unwind);
+                });
+    }
+
     private void assertResultsOptimized(String fileName, Map<String, Object> r) {
         assertEquals(7L, r.get("nodes"));
         assertEquals(2L, r.get("relationships"));
