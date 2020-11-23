b70a700172fb71dd009294ff6898d9f29764a7e9
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2020-11-23 09:10:17 +0100

    clean format

--- full/src/main/java/apoc/custom/CypherProceduresHandler.java
+++ full/src/main/java/apoc/custom/CypherProceduresHandler.java
@@ -247,7 +247,7 @@ public class CypherProceduresHandler extends LifecycleAdapter implements Availab
                     Pair.of(SystemPropertyKeys.name.name(), signature.name().name())
             );
 
-            node.setProperty(SystemPropertyKeys.prefix.name(),  signature.name().namespace());
+            node.setProperty(SystemPropertyKeys.prefix.name(), signature.name().namespace());
             node.setProperty(SystemPropertyKeys.description.name(), signature.description().orElse(null));
             node.setProperty(SystemPropertyKeys.statement.name(), statement);
             node.setProperty(SystemPropertyKeys.inputs.name(), serializeSignatures(signature.inputSignature()));
@@ -266,7 +266,7 @@ public class CypherProceduresHandler extends LifecycleAdapter implements Availab
                     Pair.of(SystemPropertyKeys.database.name(), api.databaseName()),
                     Pair.of(SystemPropertyKeys.name.name(), signature.name().name())
             );
-            node.setProperty(SystemPropertyKeys.prefix.name(),  signature.name().namespace());
+            node.setProperty(SystemPropertyKeys.prefix.name(), signature.name().namespace());
             node.setProperty(SystemPropertyKeys.description.name(), signature.description().orElse(null));
             node.setProperty(SystemPropertyKeys.statement.name(), statement);
             node.setProperty(SystemPropertyKeys.inputs.name(), serializeSignatures(signature.inputSignature()));
