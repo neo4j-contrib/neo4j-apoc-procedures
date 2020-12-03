11c07d845641cb57b56cbbd29b2715a524ed5567
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2020-12-03 12:34:49 +0100

    refactor compressionAlgo with Factory pattern

--- core/src/main/java/apoc/util/CompressionConfig.java
+++ core/src/main/java/apoc/util/CompressionConfig.java
@@ -1,7 +1,20 @@
 package apoc.util;
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+import org.apache.commons.compress.compressors.deflate.DeflateCompressorInputStream;
+import org.apache.commons.compress.compressors.deflate.DeflateCompressorOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.compress.compressors.lz4.BlockLZ4CompressorInputStream;
+import org.apache.commons.compress.compressors.lz4.BlockLZ4CompressorOutputStream;
+import org.apache.commons.compress.compressors.snappy.FramedSnappyCompressorInputStream;
+import org.apache.commons.compress.compressors.snappy.FramedSnappyCompressorOutputStream;
+import java.io.*;
+import java.lang.reflect.Constructor;
 import java.nio.charset.Charset;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 
