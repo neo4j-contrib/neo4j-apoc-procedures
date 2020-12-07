7426c393fd752d8f71403310c00f4fc26be17ec0
Mark Needham <m.h.needham@gmail.com>
2020-12-07 08:08:57 +0000

    these tests cases need to be (and alrleady are) in TriggerExtendedTest, as they rely on code that's in APOC Full

--- core/src/test/java/apoc/trigger/TriggerTest.java
+++ core/src/test/java/apoc/trigger/TriggerTest.java
@@ -132,40 +132,6 @@ public class TriggerTest {
     }
 
     @Test
-    public void testTimeStampTriggerForUpdatedProperties() throws Exception {
-        db.executeTransactionally("CALL apoc.trigger.add('timestamp','UNWIND apoc.trigger.nodesByLabel($assignedNodeProperties,null) AS n SET n.ts = timestamp()',{})");
-        db.executeTransactionally("CREATE (f:Foo) SET f.foo='bar'");
-        TestUtil.testCall(db, "MATCH (f:Foo) RETURN f", (row) -> {
-            assertEquals(true, ((Node) row.get("f")).hasProperty("ts"));
-        });
-    }
-
-    @Test
-    public void testLowerCaseName() throws Exception {
-        db.executeTransactionally("create constraint on (p:Person) assert p.id is unique");
-        db.executeTransactionally("CALL apoc.trigger.add('lowercase','UNWIND apoc.trigger.nodesByLabel($assignedLabels,\"Person\") AS n SET n.id = toLower(n.name)',{})");
-        db.executeTransactionally("CREATE (f:Person {name:'John Doe'})");
-        TestUtil.testCall(db, "MATCH (f:Person) RETURN f", (row) -> {
-            assertEquals("john doe", ((Node) row.get("f")).getProperty("id"));
-            assertEquals("John Doe", ((Node) row.get("f")).getProperty("name"));
-        });
-    }
-
-    @Test
-    public void testSetLabels() throws Exception {
-        db.executeTransactionally("CREATE (f {name:'John Doe'})");
-        db.executeTransactionally("CALL apoc.trigger.add('setlabels','UNWIND apoc.trigger.nodesByLabel($assignedLabels,\"Person\") AS n SET n:Man',{})");
-        db.executeTransactionally("MATCH (f) SET f:Person");
-        TestUtil.testCall(db, "MATCH (f:Man) RETURN f", (row) -> {
-            assertEquals("John Doe", ((Node) row.get("f")).getProperty("name"));
-            assertEquals(true, ((Node) row.get("f")).hasLabel(Label.label("Person")));
-        });
-
-        long count = TestUtil.singleResultFirstColumn(db, "MATCH (f:Man) RETURN count(*) as c");
-        assertEquals(1L, count);
-    }
-
-    @Test
     public void testTxId() throws Exception {
         db.executeTransactionally("CALL apoc.trigger.add('txinfo','UNWIND $createdNodes AS n SET n.txId = $transactionId, n.txTime = $commitTime',{phase:'after'})");
         db.executeTransactionally("CREATE (f:Bar)");
@@ -253,21 +219,6 @@ public class TriggerTest {
             assertEquals(true, ((Node) row.get("f")).hasProperty("txTime"));
             assertEquals(true, ((Node) row.get("f")).hasProperty("name"));
         });
-    }
-
-
-    @Test
-    public void testTxIdAfterAsync() throws Exception {
-        db.executeTransactionally("CALL apoc.trigger.add('triggerTest','UNWIND apoc.trigger.propertiesByKey($assignedNodeProperties, \"_executed\") as prop " +
-                "	WITH prop.node as n " +
-                "	CREATE (z:SON {father:id(n)}) " +
-                "	CREATE (n)-[:GENERATED]->(z)', " +
-                "{phase:'afterAsync'})");
-        db.executeTransactionally("CREATE (:TEST {name:'x', _executed:0})");
-        db.executeTransactionally("CREATE (:TEST {name:'y', _executed:0})");
-        org.neo4j.test.assertion.Assert.assertEventually((Callable<Long>) () -> db.executeTransactionally("MATCH p = ()-[r:GENERATED]->() RETURN count(p) AS count",
-                Collections.emptyMap(), (r) -> r.<Long>columnAs("count").next()),
-                value -> value.equals(2L), 30L, TimeUnit.SECONDS);
     }
 
     @Test(expected = QueryExecutionException.class)
