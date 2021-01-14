7ed0c270d9906136443b324a25130318dd04febe
Niels Oertel <nioertel@googlemail.com>
2021-01-14 13:50:26 +0100

    Fix #1758 by updating neo4j-java-driver and Neo4j versions. (#1764)

--- test-utils/src/main/java/apoc/util/TestContainerUtil.java
+++ test-utils/src/main/java/apoc/util/TestContainerUtil.java
@@ -46,7 +46,7 @@ public class TestContainerUtil {
         importFolder.mkdirs();
 
         // read neo4j version from build.gradle and use this as default
-        String neo4jDockerImageVersion = System.getProperty("neo4jDockerImage", "neo4j:4.2.1-enterprise");
+        String neo4jDockerImageVersion = System.getProperty("neo4jDockerImage", "neo4j:4.2.2-enterprise");
 
         // use a separate folder for mounting plugins jar - build/libs might contain other jars as well.
         File pluginsFolder = new File("build/plugins");
