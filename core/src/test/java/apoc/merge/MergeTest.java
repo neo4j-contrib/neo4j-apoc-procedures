98c55b846101830b3d471a8ea2d220bce791db49
Simon Högl <30871340+simon-hoegl@users.noreply.github.com>
2021-01-26 00:23:04 +0100

    Fixes test case
    Fixed incompatible types and symbol error

--- core/src/test/java/apoc/merge/MergeTest.java
+++ core/src/test/java/apoc/merge/MergeTest.java
@@ -98,12 +98,13 @@ public class MergeTest {
     @Test
     public void testEscapeIdentityPropertiesWithSpecialCharactersShouldWork() {
         for (String key: new String[]{"normal", "i:d", "i-d", "i d"}) {
-            Map<String, String> identProps = MapUtil.map(key, "value");
+            Map<String, Object> identProps = MapUtil.map(key, "value");
             Map<String, Object> params = MapUtil.map("identProps", identProps);
             
             testCall(db, "CALL apoc.merge.node(['Person'], $identProps) YIELD node RETURN node", params,
                         (row) -> {
-                            assertTrue(row.get("node") instanceof Node);
+                            Node node = (Node) row.get("node");
+                            assertTrue(node instanceof Node);
                             assertTrue(node.hasProperty(key));
                             assertEquals("value", node.getProperty(key));
                         });
