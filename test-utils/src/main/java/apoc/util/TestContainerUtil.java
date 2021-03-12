af3aeccbdef7a15d414eea4d86a7d09d932af277
Andrea Santurbano <santand@gmail.com>
2021-03-12 13:04:40 +0100

    fixes #1336: Create apoc.export.cypher format and outputs optimised for passing to apoc.bolt.execute

--- test-utils/src/main/java/apoc/util/TestContainerUtil.java
+++ test-utils/src/main/java/apoc/util/TestContainerUtil.java
@@ -42,7 +42,7 @@ public class TestContainerUtil {
         importFolder.mkdirs();
 
         // read neo4j version from build.gradle and use this as default
-        String neo4jDockerImageVersion = System.getProperty("neo4jDockerImage", "neo4j:4.1.0-enterprise");
+        String neo4jDockerImageVersion = System.getProperty("neo4jDockerImage", "neo4j:4.1.6-enterprise");
 
         // use a separate folder for mounting plugins jar - build/libs might contain other jars as well.
         File pluginsFolder = new File("build/plugins");
