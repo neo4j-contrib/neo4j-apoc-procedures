4ce21e01809ce916245b0742059656f9e77cdb60
Andrea Santurbano <santand@gmail.com>
2021-04-02 18:32:32 +0200

    fixed testDeleteRelationshipsAsync

--- core/src/test/java/apoc/trigger/TriggerTest.java
+++ core/src/test/java/apoc/trigger/TriggerTest.java
@@ -246,7 +246,7 @@ public class TriggerTest {
         db.executeTransactionally("CREATE (a:A {name: \"A\"})-[:R1]->(z:Z {name: \"Z\"}), (a)-[:R2]->(z)");
         db.executeTransactionally("CALL apoc.trigger.add('trigger-after-async', 'UNWIND $deletedRelationships AS r\n" +
                 "MATCH (a)-[r1:R1]->(z)\n" +
-                "SET r1.triggerAfterAsync = size($deletedRelationships) > 0, r1.size = size($deletedRelationships), r1.deleted = type(r) RETURN *', {phase: 'afterAsync'})");
+                "SET r1.triggerAfterAsync = true, r1.deleted = type(r) RETURN *', {phase: 'afterAsync'})");
         db.executeTransactionally("MATCH (a:A {name: \"A\"})-[r:R2]->(z:Z {name: \"Z\"})\n" +
                 "DELETE r");
 
@@ -257,7 +257,7 @@ public class TriggerTest {
                                     return (boolean) r.getProperty("triggerAfterAsync", false)
                                             && r.getProperty("deleted", "").equals("R2");
                                 })
-                , (value) -> value, 60L, TimeUnit.SECONDS);
+                , (value) -> value, 30L, TimeUnit.SECONDS);
     }
 
     @Test
@@ -265,7 +265,7 @@ public class TriggerTest {
         db.executeTransactionally("CREATE (a:A {name: \"A\"})-[:R1]->(z:Z {name: \"Z\"}), (a)-[:R2]->(z)");
         db.executeTransactionally("CALL apoc.trigger.add('trigger-after', 'UNWIND $deletedRelationships AS r\n" +
                 "MERGE (a:AA{name: \"AA\"})\n" +
-                "SET a.triggerAfter = size($deletedRelationships) = 1, a.deleted = type(r)', {phase: 'after'})");
+                "SET a.triggerAfter = true, a.deleted = type(r)', {phase: 'after'})");
         db.executeTransactionally("MATCH (a:A {name: \"A\"})-[r:R2]->(z:Z {name: \"Z\"})\n" +
                 "DELETE r");
 
