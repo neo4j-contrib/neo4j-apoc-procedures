9264e47809158de6fb1b52646742bba969e8a552
Sergey Zaporozhtsev <sergey.zaporozhtsev@neotechnology.com>
2021-05-07 15:25:43 +0100

    Fixes ExportCypherTest for token indexes

--- core/src/main/java/apoc/export/cypher/MultiStatementCypherSubGraphExporter.java
+++ core/src/main/java/apoc/export/cypher/MultiStatementCypherSubGraphExporter.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.neo4j.cypher.export.SubGraph;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.graphdb.schema.IndexType;
 import org.neo4j.internal.helpers.collection.Iterables;
 
 import java.io.PrintWriter;
