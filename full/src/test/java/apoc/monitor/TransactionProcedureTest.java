acb8ef6feb958f90a49ecf57af1408540933fa80
Sergey Zaporozhtsev <sergey.zaporozhtsev@neotechnology.com>
2021-05-07 18:25:04 +0100

    Fix more tests to work with token indexes

--- full/src/test/java/apoc/monitor/TransactionProcedureTest.java
+++ full/src/test/java/apoc/monitor/TransactionProcedureTest.java
@@ -4,6 +4,8 @@ import apoc.util.TestUtil;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import org.neo4j.kernel.impl.index.schema.RelationshipTypeScanStoreSettings;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
@@ -13,7 +15,8 @@ import static org.junit.Assert.assertEquals;
 public class TransactionProcedureTest {
 
     @Rule
-    public DbmsRule db = new ImpermanentDbmsRule();
+    public DbmsRule db = new ImpermanentDbmsRule()
+            .withSetting( RelationshipTypeScanStoreSettings.enable_scan_stores_as_token_indexes, true );
 
     @Before
     public void setup() {
@@ -25,20 +28,20 @@ public class TransactionProcedureTest {
         testCall(db, "CALL apoc.monitor.tx()", (row) -> {
             assertEquals(0l, row.get("rolledBackTx"));
             assertEquals(1l, row.get("peakTx"));
-            assertEquals(1l, row.get("lastTxId"));
+            assertEquals(3l, row.get("lastTxId"));
             assertEquals(1l, row.get("currentOpenedTx"));
-            assertEquals(2l, row.get("totalOpenedTx"));
-            assertEquals(1l, row.get("totalTx"));
+            assertEquals(4l, row.get("totalOpenedTx"));
+            assertEquals(3l, row.get("totalTx"));
         });
 
         db.executeTransactionally("create ()");
         testCall(db, "CALL apoc.monitor.tx()", (row) -> {
             assertEquals(0l, row.get("rolledBackTx"));
             assertEquals(1l, row.get("peakTx"));
-            assertEquals(2l, row.get("lastTxId"));
+            assertEquals(4l, row.get("lastTxId"));
             assertEquals(1l, row.get("currentOpenedTx"));
-            assertEquals(4l, row.get("totalOpenedTx"));
-            assertEquals(3l, row.get("totalTx"));
+            assertEquals(6l, row.get("totalOpenedTx"));
+            assertEquals(5l, row.get("totalTx"));
         });
     }
 }
