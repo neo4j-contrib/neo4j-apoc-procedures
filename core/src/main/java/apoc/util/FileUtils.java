16eb7a402a72835f065bae9547a6d625083a3800
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-05-27 17:15:34 +0200

    Bug in CALL apoc.metrics.storage('dbms.directories.data') due to APOC configuration check
    Fixes #1829

--- core/src/main/java/apoc/util/FileUtils.java
+++ core/src/main/java/apoc/util/FileUtils.java
@@ -282,7 +282,7 @@ public class FileUtils {
     //
     // More likely, they'll be largely similar metrics.
     public static final List<String> NEO4J_DIRECTORY_CONFIGURATION_SETTING_NAMES = Arrays.asList(
-            "dbms.directories.certificates",
+//            "dbms.directories.certificates",  // not in 4.x version
             "dbms.directories.data",
             "dbms.directories.import",
             "dbms.directories.lib",
@@ -291,7 +291,7 @@ public class FileUtils {
             "dbms.directories.plugins",
             "dbms.directories.run",
             "dbms.directories.transaction.logs.root", // in Neo4j 5.0 GraphDatabaseSettings.transaction_logs_root_path changed from tx_log to this config
-            "unsupported.dbms.directories.neo4j_home"
+            "dbms.directories.neo4j_home"
     );
 
     public static void closeReaderSafely(CountingReader reader) {
