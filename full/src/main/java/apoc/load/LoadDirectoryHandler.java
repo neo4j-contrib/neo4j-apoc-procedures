91313164ef0e556aaed2fd55d217aed458b08e7a
Andrea Santurbano <santand@gmail.com>
2021-05-29 13:26:49 +0200

    improved logging of test errors

--- full/src/main/java/apoc/load/LoadDirectoryHandler.java
+++ full/src/main/java/apoc/load/LoadDirectoryHandler.java
@@ -8,13 +8,6 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.lifecycle.LifecycleAdapter;
 import org.neo4j.logging.Log;
 
-import static apoc.util.FileUtils.getDirImport;
-import static apoc.util.FileUtils.isImportUsingNeo4jConfig;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-import static java.nio.file.WatchEvent.Kind;
-
 import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -30,7 +23,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
+import static apoc.util.FileUtils.getDirImport;
 import static apoc.util.FileUtils.getPathFromUrlString;
+import static apoc.util.FileUtils.isImportUsingNeo4jConfig;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.WatchEvent.Kind;
 import static org.apache.commons.lang3.StringUtils.replaceOnce;
 
 public class LoadDirectoryHandler extends LifecycleAdapter {
@@ -103,7 +102,6 @@ public class LoadDirectoryHandler extends LifecycleAdapter {
     }
 
     public void removeAll() {
-
         Set<LoadDirectoryItem> keys = new HashSet<>(storage.keySet());
         keys.forEach(this::remove);
     }
