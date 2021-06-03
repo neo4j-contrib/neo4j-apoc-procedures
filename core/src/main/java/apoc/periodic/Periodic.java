ee813e4f0f6019bebd2750030b530440b3e5c8a4
JMHReif <jennifer@thehecklers.org>
2021-06-03 10:27:36 -0500

    Fix param name

--- core/src/main/java/apoc/periodic/Periodic.java
+++ core/src/main/java/apoc/periodic/Periodic.java
@@ -151,7 +151,7 @@ public class Periodic {
 
     @Procedure(mode = Mode.WRITE)
     @Description("apoc.periodic.submit('name',statement,params) - submit a one-off background statement; parameter 'params' is optional and can contain query parameters for Cypher statement")
-    public Stream<JobInfo> submit(@Name("name") String name, @Name("statement") String statement, @Name(value = "config", defaultValue = "{}") Map<String,Object> config) {
+    public Stream<JobInfo> submit(@Name("name") String name, @Name("statement") String statement, @Name(value = "params", defaultValue = "{}") Map<String,Object> config) {
         validateQuery(statement);
         Map<String,Object> params = (Map)config.getOrDefault("params", Collections.emptyMap());
         JobInfo info = submit(name, () -> {
