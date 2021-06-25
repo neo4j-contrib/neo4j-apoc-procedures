aa55d348851ececd04498b06dd245510baba901b
vga91 <giuseppe.villani@larus-ba.it>
2021-06-25 16:25:28 +0200

    removed import

--- full/src/test/java/apoc/cypher/CypherExtendedTest.java
+++ full/src/test/java/apoc/cypher/CypherExtendedTest.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 import static apoc.ApocConfig.APOC_IMPORT_FILE_ENABLED;
 import static apoc.ApocConfig.apocConfig;
