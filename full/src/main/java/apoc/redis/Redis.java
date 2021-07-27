a85ab3364fcdd9e0046fc61cd693d015d62be071
vga91 <giuseppe.villani@larus-ba.it>
2021-07-27 11:41:18 +0200

    added @Extended and procs in extended.txt

--- full/src/main/java/apoc/redis/Redis.java
+++ full/src/main/java/apoc/redis/Redis.java
@@ -1,5 +1,6 @@
 package apoc.redis;
 
+import apoc.Extended;
 import apoc.result.BooleanResult;
 import apoc.result.ListResult;
 import apoc.result.LongResult;
@@ -15,6 +16,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+@Extended
 public class Redis {
 
     // -- String
