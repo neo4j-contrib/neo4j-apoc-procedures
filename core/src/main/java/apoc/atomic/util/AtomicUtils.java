be58bb71f555c933ff7cf24a4ae0f2c37b5dda50
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-08-04 18:15:54 +0200

    Make apoc.atomic.add and related procedures work with Integer properties
    Fixes #1157

--- core/src/main/java/apoc/atomic/util/AtomicUtils.java
+++ core/src/main/java/apoc/atomic/util/AtomicUtils.java
@@ -10,8 +10,12 @@ public class AtomicUtils {
 	public static Number sum(Number oldValue, Number number){
 		if(oldValue instanceof Long)
 			return oldValue.longValue() + number.longValue();
+		if(oldValue instanceof Integer)
+			return oldValue.intValue() + number.intValue();
 		if(oldValue instanceof Double)
 			return oldValue.doubleValue() + number.doubleValue();
+		if(oldValue instanceof Float)
+			return oldValue.floatValue() + number.floatValue();
 		if(oldValue instanceof Short)
 			return oldValue.shortValue() + number.shortValue();
 		if(oldValue instanceof Byte)
@@ -22,8 +26,12 @@ public class AtomicUtils {
 	public static Number sub(Number oldValue, Number number){
 		if(oldValue instanceof Long)
 			return oldValue.longValue() - number.longValue();
+		if(oldValue instanceof Integer)
+			return oldValue.intValue() - number.intValue();
 		if(oldValue instanceof Double)
 			return oldValue.doubleValue() - number.doubleValue();
+		if(oldValue instanceof Float)
+			return oldValue.floatValue() - number.floatValue();
 		if(oldValue instanceof Short)
 			return oldValue.shortValue() - number.shortValue();
 		if(oldValue instanceof Byte)
