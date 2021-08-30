b5db3c3788bc7758f530108381fb3d325eb4f094
Andrea Santurbano <santand@gmail.com>
2021-08-30 15:13:39 +0200

    added config params

--- full/src/main/java/apoc/dv/CSVResource.java
+++ full/src/main/java/apoc/dv/CSVResource.java
@@ -12,12 +12,12 @@ public class CSVResource extends VirtualizedResource {
     }
 
     @Override
-    public Pair<String, Map<String, Object>> getProcedureCallWithParams(Object queryParams) {
-        final Map<String, Object> base = Map.of("url", this.url, "params", params, "labels", labels);
+    public Pair<String, Map<String, Object>> getProcedureCallWithParams(Object queryParams, Map<String, Object> config) {
+        final Map<String, Object> base = Map.of("url", this.url, "config", config, "labels", labels);
         Map<String, Object> map = new HashMap<>();
         map.putAll((Map<String, Object>) queryParams);
         map.putAll(base);
-        return Pair.of("CALL apoc.load.csv($url, $params) YIELD map WHERE " + query
+        return Pair.of("CALL apoc.load.csv($url, $config) YIELD map WHERE " + query
                     + " RETURN apoc.create.vNode($labels, map) AS node",
                 map);
     }
