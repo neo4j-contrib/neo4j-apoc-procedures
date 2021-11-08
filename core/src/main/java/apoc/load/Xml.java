a36468ddd97c444a75bf53505ee3400a7c69eaaf
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-11-08 17:29:12 +0100

    Remove deprecated procedure apoc.load.xmlSimple
    Fixes #2256

--- core/src/main/java/apoc/load/Xml.java
+++ core/src/main/java/apoc/load/Xml.java
@@ -103,14 +103,7 @@ public class Xml {
                 .map(mr -> mr.value).findFirst().orElse(null);
     }
 
-    @Procedure(deprecatedBy = "apoc.load.xml")
-    @Deprecated
-    @Description("apoc.load.xmlSimple('http://example.com/test.xml') YIELD value as doc CREATE (p:Person) SET p.name = doc.name - load from XML URL (e.g. web-api) to import XML as single nested map with attributes and _type, _text and _children fields. This method does intentionally not work with XML mixed content.")
-    public Stream<MapResult> xmlSimple(@Name("url") String url) throws Exception {
-        return xmlToMapResult(url, true);
-    }
-
-    private Stream<MapResult> xmlXpathToMapResult(@Name("urlOrBinary") Object urlOrBinary, boolean simpleMode, String path, Map<String, Object> config) throws Exception {
+    private Stream<MapResult> xmlXpathToMapResult(@Name("url") Object urlOrBinary, boolean simpleMode, String path, Map<String, Object> config) throws Exception {
         if (config == null) config = Collections.emptyMap();
         boolean failOnError = (boolean) config.getOrDefault("failOnError", true);
         try {
@@ -174,33 +167,13 @@ public class Xml {
         return result.stream();
     }
 
-    private Stream<MapResult> xmlToMapResult(@Name("url") String url, boolean simpleMode) {
-        try {
-            XMLStreamReader reader = getXMLStreamReader(url, new XmlImportConfig(Collections.EMPTY_MAP));
-            final Deque<Map<String, Object>> stack = new LinkedList<>();
-            do {
-                handleXmlEvent(stack, reader, simpleMode);
-            } while (proceedReader(reader));
+    private XMLStreamReader getXMLStreamReaderFromUrl(String url, XmlImportConfig config) throws IOException, XMLStreamException {
+        apocConfig.checkReadAllowed(url);
+        url = FileUtils.changeFileUrlIfImportDirectoryConstrained(url);
+        URLConnection urlConnection = new URL(url).openConnection();
+        FACTORY.setProperty(XMLInputFactory.IS_COALESCING, true);
+        InputStream inputStream = urlConnection.getInputStream();
 
-            return Stream.of(new MapResult(stack.getFirst()));
-        } catch (IOException | XMLStreamException e) {
-            throw new RuntimeException("Can't read url " + cleanUrl(url) + " as XML", e);
-        }
-    }
-
-    private XMLStreamReader getXMLStreamReader(Object urlOrBinary, XmlImportConfig config) throws IOException, XMLStreamException {
-        InputStream inputStream;
-        if (urlOrBinary instanceof String) {
-            String url = (String) urlOrBinary; 
-            apocConfig.checkReadAllowed(url);
-            url = FileUtils.changeFileUrlIfImportDirectoryConstrained(url);
-            URLConnection urlConnection = new URL(url).openConnection();
-            inputStream = urlConnection.getInputStream();
-        } else if (urlOrBinary instanceof byte[]) {
-            inputStream = getInputStreamFromBinary((byte[]) urlOrBinary, config.getCompressionAlgo());
-        } else {
-            throw new RuntimeException(ERROR_BYTES_OR_STRING);
-        }
         if (config.isFilterLeadingWhitespace()) {
             inputStream = new SkipWhitespaceInputStream(inputStream);
         }
@@ -216,55 +189,6 @@ public class Xml {
             return true;
         } else {
             return false;
-        }
-    }
-
-    private void handleXmlEvent(Deque<Map<String, Object>> stack, XMLStreamReader reader, boolean simpleMode) throws XMLStreamException {
-
-        Map<String, Object> elementMap;
-        switch (reader.getEventType()) {
-            case START_DOCUMENT:
-            case END_DOCUMENT:
-                // intentionally empty
-                break;
-            case START_ELEMENT:
-                int attributes = reader.getAttributeCount();
-                elementMap = new LinkedHashMap<>(attributes + 3);
-                elementMap.put("_type", reader.getLocalName());
-                for (int a = 0; a < attributes; a++) {
-                    elementMap.put(reader.getAttributeLocalName(a), reader.getAttributeValue(a));
-                }
-                if (!stack.isEmpty()) {
-                    final Map<String, Object> last = stack.getLast();
-                    String key = simpleMode ? "_" + reader.getLocalName() : "_children";
-                    amendToList(last, key, elementMap);
-                }
-                stack.addLast(elementMap);
-                break;
-
-            case END_ELEMENT:
-                elementMap = stack.size() > 1 ? stack.removeLast() : stack.getLast();
-
-                // maintain compatibility with previous implementation:
-                // if we only have text childs, return them in "_text" and not in "_children"
-                Object children = elementMap.get("_children");
-                if (children != null) {
-                    if ((children instanceof String) || collectionIsAllStrings(children)) {
-                        elementMap.put("_text", children);
-                        elementMap.remove("_children");
-                    }
-                }
-                break;
-
-            case CHARACTERS:
-                final String text = reader.getText().trim();
-                if (!text.isEmpty()) {
-                    Map<String, Object> map = stack.getLast();
-                    amendToList(map, "_children", text);
-                }
-                break;
-            default:
-                throw new RuntimeException("dunno know how to handle xml event type " + reader.getEventType());
         }
     }
 
