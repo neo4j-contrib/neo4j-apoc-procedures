7704078993663450cbf88520b72bfa51ae786088
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-11-09 19:42:28 +0100

    Fixes #1471: Support spatial and temporal datatypes in CSV import (e.g. point, datetime)

--- core/src/test/java/apoc/export/json/ImportJsonTest.java
+++ core/src/test/java/apoc/export/json/ImportJsonTest.java
@@ -1,10 +1,15 @@
 package apoc.export.json;
 
 import apoc.ApocSettings;
+import apoc.schema.Schemas;
 import apoc.util.CompressionAlgo;
 import apoc.schema.Schemas;
 import apoc.util.JsonUtil;
 import apoc.util.TestUtil;
+import apoc.util.Util;
+import com.google.common.collect.Iterables;
+import junit.framework.TestCase;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import apoc.util.Util;
 import com.google.common.collect.Iterables;
 import junit.framework.TestCase;
@@ -21,6 +26,7 @@ import org.junit.Test;
 import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.spatial.Point;
 import org.neo4j.test.rule.DbmsRule;
@@ -35,12 +41,17 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static apoc.export.json.JsonImporter.MISSING_CONSTRAINT_ERROR_MSG;
 import static apoc.util.BinaryTestUtil.fileToBinary;
 import static apoc.util.CompressionConfig.COMPRESSION;
 import static apoc.export.json.JsonImporter.MISSING_CONSTRAINT_ERROR_MSG;
 import static apoc.util.MapUtil.map;
+import static java.lang.String.format;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.neo4j.test.assertion.Assert.assertEventually;
 
 
 public class ImportJsonTest {
@@ -60,7 +71,7 @@ public class ImportJsonTest {
 
     @Before
     public void setUp() throws Exception {
-        TestUtil.registerProcedure(db, ImportJson.class, Utils.class);
+        TestUtil.registerProcedure(db, ImportJson.class, Schemas.class, Utils.class);
     }
 
     @Test
@@ -75,41 +86,8 @@ public class ImportJsonTest {
                 map("file", filename),
                 (r) -> assertionsAllJsonProgressInfo(r, false)
         );
-                (r) -> {
-                    // then
-                    Assert.assertEquals("all.json", r.get("file"));
-                    Assert.assertEquals("file", r.get("source"));
-                    Assert.assertEquals("json", r.get("format"));
-                    Assert.assertEquals(3L, r.get("nodes"));
-                    Assert.assertEquals(1L, r.get("relationships"));
-                    Assert.assertEquals(15L, r.get("properties"));
-                    Assert.assertEquals(4L, r.get("rows"));
-                    Assert.assertEquals(true, r.get("done"));
 
-                    try(Transaction tx = db.beginTx()) {
-                        final long countNodes = tx.execute("MATCH (n:User) RETURN count(n) AS count")
-                                .<Long>columnAs("count")
-                                .next();
-                        Assert.assertEquals(3L, countNodes);
-
-                        final long countRels = tx.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
-                                .<Long>columnAs("count")
-                                .next();
-                        Assert.assertEquals(1L, countRels);
-
-                        final Map<String, Object> props = tx.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
-                                .<Node>columnAs("n")
-                                .next()
-                                .getAllProperties();
-
-                        Assert.assertEquals(9, props.size());
-                        Assert.assertEquals("wgs-84", props.get("place.crs"));
-                        Assert.assertEquals(33.46789D, props.get("place.latitude"));
-                        Assert.assertEquals(13.1D, props.get("place.longitude"));
-                        Assert.assertFalse(props.containsKey("place"));
-                    }
-                }
-        );
+        assertionsAllJsonDbResult();
     }
 
     @Test
@@ -134,36 +112,37 @@ public class ImportJsonTest {
                     Assert.assertEquals(4L, r.get("rows"));
                     Assert.assertEquals(true, r.get("done"));
 
-                    try(Transaction tx = db.beginTx()) {
-                        final long countNodes = tx.execute("MATCH (n:User) RETURN count(n) AS count")
-                                .<Long>columnAs("count")
-                                .next();
-                        Assert.assertEquals(3L, countNodes);
-
-                        final long countRels = tx.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
-                                .<Long>columnAs("count")
-                                .next();
-                        Assert.assertEquals(1L, countRels);
-
-                        final Map<String, Object> props = tx.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
-                                .<Node>columnAs("n")
-                                .next()
-                                .getAllProperties();
-                        Assert.assertEquals(7, props.size());
-                        Assert.assertTrue(props.get("place") instanceof PointValue);
-                        PointValue point = (PointValue) props.get("place");
-                        final PointValue pointValue = Values.pointValue(CoordinateReferenceSystem.WGS84, 33.46789D, 13.1D);
-                        Assert.assertTrue(point.equals((Point) pointValue));
-                        Assert.assertTrue(props.get("born") instanceof LocalDateTime);
-
-                        Relationship rel = tx.execute("MATCH ()-[r:KNOWS]->() RETURN r")
-                                .<Relationship>columnAs("r")
-                                .next();
-                        Assert.assertTrue(rel.getProperty("bffSince") instanceof DurationValue);
-                        Assert.assertEquals("P5M1DT12H", rel.getProperty("bffSince").toString());
-                    }
                 }
         );
+        
+        try(Transaction tx = db.beginTx()) {
+            final long countNodes = tx.execute("MATCH (n:User) RETURN count(n) AS count")
+                    .<Long>columnAs("count")
+                    .next();
+            Assert.assertEquals(3L, countNodes);
+
+            final long countRels = tx.execute("MATCH ()-[r:KNOWS]->() RETURN count(r) AS count")
+                    .<Long>columnAs("count")
+                    .next();
+            Assert.assertEquals(1L, countRels);
+
+            final Map<String, Object> props = tx.execute("MATCH (n:User {name: 'Adam'}) RETURN n")
+                    .<Node>columnAs("n")
+                    .next()
+                    .getAllProperties();
+            Assert.assertEquals(7, props.size());
+            Assert.assertTrue(props.get("place") instanceof PointValue);
+            PointValue point = (PointValue) props.get("place");
+            final PointValue pointValue = Values.pointValue(CoordinateReferenceSystem.WGS84, 33.46789D, 13.1D);
+            Assert.assertTrue(point.equals((Point) pointValue));
+            Assert.assertTrue(props.get("born") instanceof LocalDateTime);
+
+            Relationship rel = tx.execute("MATCH ()-[r:KNOWS]->() RETURN r")
+                    .<Relationship>columnAs("r")
+                    .next();
+            Assert.assertTrue(rel.getProperty("bffSince") instanceof DurationValue);
+            Assert.assertEquals("P5M1DT12H", rel.getProperty("bffSince").toString());
+        }
     }
 
     @Test
@@ -189,21 +168,21 @@ public class ImportJsonTest {
                     Assert.assertEquals(2L, r.get("properties"));
                     Assert.assertEquals(1L, r.get("rows"));
                     Assert.assertEquals(true, r.get("done"));
-                    try (Transaction tx = db.beginTx()) {
-                        Node node = tx.execute("MATCH (n) WHERE n.neo4jImportId = '5016999' RETURN n")
-                                .<Node>columnAs("n")
-                                .next();
-                        Assert.assertNotNull("node should be not null", node);
-                        final double[] actual = (double[]) node.getProperty("bbox");
-                        Assert.assertArrayEquals(expected, actual, 0.05D);
-                    }
                 }
         );
+
+        try (Transaction tx = db.beginTx()) {
+            Node node = tx.execute("MATCH (n) WHERE n.neo4jImportId = '5016999' RETURN n")
+                    .<Node>columnAs("n")
+                    .next();
+            Assert.assertNotNull("node should be not null", node);
+            final double[] actual = (double[]) node.getProperty("bbox");
+            Assert.assertArrayEquals(expected, actual, 0.05D);
+        }
     }
 
     @Test
     public void shouldTerminateImportWhenTransactionIsTimedOut() {
-
         createConstraints(List.of("Stream", "User", "Game", "Team", "Language"));
 
         String filename = "https://devrel-data-science.s3.us-east-2.amazonaws.com/twitch_all.json";
@@ -291,6 +270,8 @@ public class ImportJsonTest {
 
     @Test
     public void shouldImportAllJsonFromBinary()  {
+        db.executeTransactionally("CREATE CONSTRAINT ON (n:User) assert n.neo4jImportId IS UNIQUE");
+
         TestUtil.testCall(db, "CALL apoc.import.json($file, $config)",
                 map("config", map(COMPRESSION, CompressionAlgo.DEFLATE.name()),
                         "file", fileToBinary(new File(directory, "all.json"), CompressionAlgo.DEFLATE.name())),
