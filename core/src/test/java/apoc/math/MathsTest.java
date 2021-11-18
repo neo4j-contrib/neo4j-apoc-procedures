c7140ce2b606a54eda40da881e5a3868a63a8057
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-11-18 13:23:25 +0100

    Fixes #494: add a sigmoid UDF (#2276)
    * Fixes #494: add a sigmoid UDF
    
    * add docs page

--- core/src/test/java/apoc/math/MathsTest.java
+++ core/src/test/java/apoc/math/MathsTest.java
@@ -12,6 +12,7 @@ import java.math.RoundingMode;
 import static apoc.util.TestUtil.testCall;
 import static apoc.util.Util.map;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class MathsTest {
 
@@ -97,5 +98,55 @@ public class MathsTest {
 
     @Test public void testMinByte(){
         testCall(db, "RETURN apoc.math.minByte() as min",(row) -> assertEquals(Long.valueOf(Byte.MIN_VALUE),row.get("min")));
+    }
+    
+    @Test
+    public void testSigmoid() {
+        testCall(db, "RETURN apoc.math.sigmoid(2.5) as value",(row) -> assertEquals(0.92D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.sigmoid(null) as value",(row) -> assertNull(row.get("value")));
+    }
+    
+    @Test
+    public void testSigmoidPrime() {
+        testCall(db, "RETURN apoc.math.sigmoidPrime(2.5) as value",(row) -> assertEquals(0.07D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.sigmoidPrime(null) as value",(row) -> assertNull(row.get("value")));
+    }
+    
+    @Test
+    public void testHyperbolicTan() {
+        testCall(db, "RETURN apoc.math.tanh(1.5) as value",(row) -> assertEquals(0.90D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.tanh(null) as value",(row) -> assertNull(row.get("value")));
+    }
+    
+    @Test
+    public void testHyperbolicCotan() {
+        testCall(db, "RETURN apoc.math.coth(3.5) as value",(row) -> assertEquals(1.00D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.coth(0) as value",(row) -> assertNull(row.get("value")));
+        testCall(db, "RETURN apoc.math.coth(null) as value",(row) -> assertNull(row.get("value")));
+    }
+    
+    @Test
+    public void testHyperbolicSin() {
+        testCall(db, "RETURN apoc.math.sinh(1.5) as value",(row) -> assertEquals(2.13D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.sinh(null) as value",(row) -> assertNull(row.get("value")));
+    }
+
+    @Test
+    public void testHyperbolicCos() {
+        testCall(db, "RETURN apoc.math.cosh(1.5) as value",(row) -> assertEquals(2.35D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.cosh(null) as value",(row) -> assertNull(row.get("value")));
+    }
+    
+    @Test
+    public void testHyperbolicSecant() {
+        testCall(db, "RETURN apoc.math.sech(1.5) as value",(row) -> assertEquals(0.43D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.sech(null) as value",(row) -> assertNull(row.get("value")));
+    }
+
+    @Test
+    public void testHyperbolicCosecant() {
+        testCall(db, "RETURN apoc.math.csch(1.5) as value",(row) -> assertEquals(0.47D, (double) row.get("value"), 0.01D));
+        testCall(db, "RETURN apoc.math.csch(0) as value",(row) -> assertNull(row.get("value")));
+        testCall(db, "RETURN apoc.math.csch(null) as value",(row) -> assertNull(row.get("value")));
     }
 }
