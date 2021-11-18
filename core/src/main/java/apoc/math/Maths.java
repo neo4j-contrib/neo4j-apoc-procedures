0e0810e4fc5743fc9fabcc1c3e1d65861a9a502e
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-11-18 13:23:25 +0100

    Fixes #494: add a sigmoid UDF (#2276)
    * Fixes #494: add a sigmoid UDF
    
    * add docs page

--- core/src/main/java/apoc/math/Maths.java
+++ core/src/main/java/apoc/math/Maths.java
@@ -70,5 +70,60 @@ public class Maths {
         return Long.valueOf(Byte.MIN_VALUE);
     }
 
+    @UserFunction
+    @Description("apoc.math.sigmoid(val) | returns the sigmoid value")
+    public Double sigmoid(@Name("value") Double value) {
+        if (value == null) return null;
+        return 1.0 / (1.0 + Math.exp(-value));
+    }
+
+    @UserFunction
+    @Description("apoc.math.sigmoidPrime(val) | returns the sigmoid prime [ sigmoid(val) * (1 - sigmoid(val)) ]")
+    public Double sigmoidPrime(@Name("value") Double value) {
+        if (value == null) return null;
+        return sigmoid(value) * (1 - sigmoid(value));
+    }
+
+    @UserFunction
+    @Description("apoc.math.tanh(val) | returns the hyperbolic tangent")
+    public Double tanh(@Name("value") Double value) {
+        if (value == null) return null;
+        return sinh(value) / cosh(value);
+    }
+
+    @UserFunction
+    @Description("apoc.math.coth(val) | returns the hyperbolic cotangent")
+    public Double coth(@Name("value") Double value) {
+        if (value == null || value.equals(0D)) return null;
+        return cosh(value) / sinh(value);
+    }
+
+    @UserFunction
+    @Description("apoc.math.cosh(val) | returns the hyperbolic cosin")
+    public Double cosh(@Name("value") Double value) {
+        if (value == null) return null;
+        return (Math.exp(value) + Math.exp(-value)) / 2;
+    }
+
+    @UserFunction
+    @Description("apoc.math.sinh(val) | returns the hyperbolic sin")
+    public Double sinh(@Name("value") Double value) {
+        if (value == null) return null;
+        return (Math.exp(value) - Math.exp(-value)) / 2;
+    }
+
+    @UserFunction
+    @Description("apoc.math.sech(val) | returns the hyperbolic secant")
+    public Double sech(@Name("value") Double value) {
+        if (value == null) return null;
+        return 1 / cosh(value);
+    }
+
+    @UserFunction
+    @Description("apoc.math.csch(val) | returns the hyperbolic cosecant")
+    public Double csch(@Name("value") Double value) {
+        if (value == null || value.equals(0D)) return null;
+        return 1 / sinh(value);
+    }
 
 }
