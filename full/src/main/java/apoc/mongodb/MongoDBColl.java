7a815b96a200a2d42cdeb19bcc8d4c872ba0d370
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-01-13 14:07:11 +0100

    Fixes #1790: Support MongoDB Extended JSON (v2) in apoc.mongodb.* procedures parameters (#2430)

--- full/src/main/java/apoc/mongodb/MongoDBColl.java
+++ full/src/main/java/apoc/mongodb/MongoDBColl.java
@@ -1,19 +1,32 @@
 package apoc.mongodb;
 
+import apoc.util.Util;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCommandException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import org.apache.commons.lang.StringUtils;
+import org.bson.BsonDouble;
+import org.bson.BsonInt32;
+import org.bson.BsonInt64;
+import org.bson.BsonNumber;
+import org.bson.BsonRegularExpression;
+import org.bson.BsonTimestamp;
 import org.bson.Document;
 import org.bson.types.Binary;
+import org.bson.types.Code;
+import org.bson.types.MaxKey;
+import org.bson.types.MinKey;
 import org.bson.types.ObjectId;
+import org.bson.types.Symbol;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
@@ -21,11 +34,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import static java.lang.String.format;
+
 /**
  * @author mh
  * @since 30.06.16
  */
-class MongoDBColl implements MongoDB.Coll {
+class MongoDBColl implements MongoDBUtils.Coll {
 
     private static final ObjectMapper jsonMapper = new ObjectMapper().enable(DeserializationFeature.USE_LONG_FOR_INTS);
     public static final String ID = "_id";
@@ -37,7 +52,10 @@ class MongoDBColl implements MongoDB.Coll {
     private boolean extractReferences = false;
     private boolean objectIdAsMap = true;
 
-    public MongoDBColl(String url, String db, String coll) {
+    // visible for testing
+    public static final String ERROR_MESSAGE = "The connection string must have %s name";
+
+    private MongoDBColl(String url, String db, String coll) {
         MongoClientURI connectionString = new MongoClientURI(url);
         mongoClient = new MongoClient(connectionString);
         database = mongoClient.getDatabase(db);
@@ -54,13 +72,60 @@ class MongoDBColl implements MongoDB.Coll {
     public MongoDBColl(String url, String db, String coll, boolean compatibleValues,
                        boolean extractReferences, boolean objectIdAsMap) {
         this(url, db, coll);
+        getConfigs(compatibleValues, extractReferences, objectIdAsMap);
+    }
+
+    /**
+     *
+     * @param uri the string Uri to convert in connectionString
+     * @see MongoClientURI
+     * @param conf the configuration
+     * @see MongoDbConfig
+     */
+    public MongoDBColl(String uri, MongoDbConfig conf) {
+
+        MongoClientURI connectionString = new MongoClientURI(uri);
+
+        if (connectionString.getDatabase() == null) {
+            throw new RuntimeException(format(ERROR_MESSAGE, "db"));
+        }
+        final String collectionName;
+        final String confCollection = conf.getCollection();
+        
+        if (StringUtils.isNotBlank(confCollection)) {
+            collectionName = confCollection;
+        } else {
+            final String collectionFromUri = connectionString.getCollection();
+            if (collectionFromUri == null) {
+                throw new RuntimeException(format(ERROR_MESSAGE, "collection"));
+            }
+            collectionName = collectionFromUri;
+        }
+
+        mongoClient = new MongoClient(connectionString);
+        database = mongoClient.getDatabase(connectionString.getDatabase());
+
+        try {
+            // check if correctly authenticated
+            database.runCommand(new Document("listCollections", 1));
+        } catch (MongoCommandException e) {
+            mongoClient.close();
+            throw new RuntimeException(e);
+        }
+        this.collection = database.getCollection(collectionName);
+
+        // with new procedure we return always Neo4j values
+        getConfigs(true, conf.isExtractReferences(), conf.isObjectIdAsMap());
+    }
+
+    private void getConfigs(boolean compatibleValues, boolean extractReferences, boolean objectIdAsMap) {
         this.compatibleValues = compatibleValues;
         this.extractReferences = extractReferences;
         this.objectIdAsMap = objectIdAsMap;
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         if (doorStop) return;
         mongoClient.close();
     }
@@ -119,11 +184,32 @@ class MongoDBColl implements MongoDB.Coll {
             if (data instanceof Integer) {
                 return ((Integer) data).longValue();
             }
-            if (data instanceof Float) {
-                return ((Float) data).doubleValue();
+            if (data instanceof BsonInt64 || data instanceof BsonInt32) {
+                return ((BsonNumber) data).longValue();
+            }
+            if (data instanceof BsonDouble) {
+                return ((BsonDouble) data).doubleValue();
             }
             if (data instanceof Binary) {
                 return ((Binary) data).getData();
+            }
+            if (data instanceof Float) {
+                return ((Float) data).doubleValue();
+            }
+            if (data instanceof BsonTimestamp) {
+                return (long) ((BsonTimestamp) data).getTime();
+            }
+            if (data instanceof MinKey || data instanceof MaxKey) {
+                return data.toString();
+            }
+            if (data instanceof BsonRegularExpression) {
+                return ((BsonRegularExpression) data).getPattern();
+            }
+            if (data instanceof Code) {
+                return ((Code) data).getCode();
+            }
+            if (data instanceof Symbol) {
+                return ((Symbol) data).getSymbol();
             }
         }
 
@@ -164,16 +250,14 @@ class MongoDBColl implements MongoDB.Coll {
         return query == null ? collection.count() : collection.count(new Document(query));
     }
 
-    private Stream<Map<String, Object>> asStream(FindIterable<Document> result) {
-        this.doorStop = true;
-        Iterable<Document> it = () -> result.iterator();
-        return StreamSupport
-                .stream(it.spliterator(), false)
-                .map(doc -> this.documentToPackableMap(doc))
-                .onClose( () -> {
-                        result.iterator().close();
-                        mongoClient.close();
-                    } );
+    @Override
+    public long count(Document query) {
+        return collection.count(query);
+    }
+
+    @Override
+    public Stream<Map<String, Object>> aggregate(List<Document> pipeline) {
+        return asStream(collection.aggregate(pipeline));
     }
 
     @Override
@@ -187,6 +271,32 @@ class MongoDBColl implements MongoDB.Coll {
     }
 
     @Override
+    public Stream<Map<String, Object>> find(Document query, Document project, Document sort, int skip, int limit) {
+        FindIterable<Document> documents = collection.find(query)
+                .projection(project).sort(sort)
+                .skip(skip).limit(limit);
+        return asStream(documents);
+    }
+
+    private FindIterable<Document> getDocuments(Map<String, Object> query) {
+        return query == null
+                ? collection.find()
+                : collection.find(new Document(query));
+    }
+
+    private Stream<Map<String, Object>> asStream(MongoIterable<Document> result) {
+        this.doorStop = true;
+        Iterable<Document> it = () -> result.iterator();
+        return StreamSupport
+                .stream(it.spliterator(), false)
+                .map(doc -> this.documentToPackableMap(doc))
+                .onClose(() -> {
+                        Util.close(result.iterator());
+                        Util.close(mongoClient);
+                });
+    }
+
+    @Override
     public void insert(List<Map<String, Object>> docs) {
         for (Map<String, Object> doc : docs) {
             collection.insertOne(new Document(doc));
@@ -194,16 +304,29 @@ class MongoDBColl implements MongoDB.Coll {
     }
 
     @Override
+    public void insertDocs(List<Document> documents) {
+        collection.insertMany(documents);
+    }
+
+    @Override
     public long update(Map<String, Object> query, Map<String, Object> update) {
-        UpdateResult updateResult = collection.updateMany(new Document(query), new Document(update));
-        return updateResult.wasAcknowledged() ? updateResult.getModifiedCount() : -updateResult.getModifiedCount();
+        return update(new Document(query), new Document(update));
+    }
+
+    @Override
+    public long update(Document query, Document update) {
+        UpdateResult updateResult = collection.updateMany(query, update);
+        return updateResult.getModifiedCount();
     }
 
     @Override
     public long delete(Map<String, Object> query) {
-        DeleteResult result = collection.deleteMany(new Document(query));
-        return result.wasAcknowledged() ? result.getDeletedCount() : -result.getDeletedCount();
+        return delete(new Document(query));
     }
 
-
+    @Override
+    public long delete(Document query) {
+        DeleteResult result = collection.deleteMany(query);
+        return result.getDeletedCount();
+    }
 }
