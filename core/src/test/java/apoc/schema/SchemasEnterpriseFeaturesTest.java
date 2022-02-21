5d86b530f48b30e1ddd0de25961bf20f061fd626
Louise Söderström <louise.matematik@gmail.com>
2022-02-21 11:48:45 +0100

    Fixes a failing test due to too new index syntax. (#2554)

--- core/src/test/java/apoc/schema/SchemasEnterpriseFeaturesTest.java
+++ core/src/test/java/apoc/schema/SchemasEnterpriseFeaturesTest.java
@@ -404,7 +404,7 @@ public class SchemasEnterpriseFeaturesTest {
         
         testResult(session, "CALL apoc.schema.relationships()", (result) -> {
             Map<String, Object> r = result.next();
-            assertEquals("CONSTRAINT ON ()-[liked:LIKED]-() ASSERT (liked.day) IS NOT NULL", r.get("name"));
+            assertEquals("CONSTRAINT ON ()-[liked:LIKED]-() ASSERT exists(liked.day)", r.get("name"));
             assertEquals("RELATIONSHIP_PROPERTY_EXISTENCE", r.get("type"));
             assertEquals(asList("day"), r.get("properties"));
             assertEquals(StringUtils.EMPTY, r.get("status"));
