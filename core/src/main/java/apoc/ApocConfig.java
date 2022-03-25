40de642606994d7b3af88f1d4536c1458de57a60
Grzegorz Aniol <grzegorz-aniol@users.noreply.github.com>
2022-03-25 21:54:06 +0100

    Support for UUID encoded in Base64
    Fixes #2378

--- core/src/main/java/apoc/ApocConfig.java
+++ core/src/main/java/apoc/ApocConfig.java
@@ -57,6 +57,8 @@ public class ApocConfig extends LifecycleAdapter {
     public static final String APOC_TRIGGER_ENABLED = "apoc.trigger.enabled";
     public static final String APOC_UUID_ENABLED = "apoc.uuid.enabled";
     public static final String APOC_UUID_ENABLED_DB = "apoc.uuid.enabled.%s";
+    public static final String APOC_UUID_FORMAT = "apoc.uuid.format";
+    public enum UuidFormatType { hex, base64 }
     public static final String APOC_JSON_ZIP_URL = "apoc.json.zip.url";  // TODO: check if really needed
     public static final String APOC_JSON_SIMPLE_JSON_URL = "apoc.json.simpleJson.url"; // TODO: check if really needed
     public static final String APOC_IMPORT_FILE_ALLOW__READ__FROM__FILESYSTEM = "apoc.import.file.allow_read_from_filesystem";
@@ -313,6 +315,16 @@ public class ApocConfig extends LifecycleAdapter {
 
     public boolean getBoolean(String key, boolean defaultValue) {
         return getConfig().getBoolean(key, defaultValue);
+    }
+
+    public <T extends Enum<T>> T getEnumProperty(String key, Class<T> cls, T defaultValue) {
+        var value = getConfig().getString(key, defaultValue.toString()).trim();
+        try {
+            return T.valueOf(cls, value);
+        } catch (IllegalArgumentException e) {
+            log.error("Wrong value '{}' for parameter '{}' is provided. Default value is used: '{}'", value, key, defaultValue);
+            return defaultValue;
+        }
     }
 
     public boolean isImportFolderConfigured() {
