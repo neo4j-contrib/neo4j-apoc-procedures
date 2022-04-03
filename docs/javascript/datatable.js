6ce8afc190c0bcf6ced49a5d2ece691dcf967c7c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-03 18:25:05 +0800

    Delete docs directory

--- docs/javascript/datatable.js
+++
@@ -1,9 +0,0 @@
-/*
-<!--script src="https://code.jquery.com/jquery-1.12.4.js"></script-->
-<!--script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script-->
-<script>
-</script>
-*/
-$(document).ready(function() {
-    $('#table-all table').DataTable({"aLengthMenu": [[20,40, 80, -1], [20,40, 80, "All"]],"pageLength": 20});
-} );
