5eff6eed1967a3cf9ba3e9f029a28b5084a48eb3
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-04-21 10:43:18 +0200

    Fixes #2405: The apoc.schema.assert procedure fails with multi-label fulltext index (#2648)

--- core/src/main/java/apoc/schema/Schemas.java
+++ core/src/main/java/apoc/schema/Schemas.java
@@ -178,6 +178,8 @@ public class Schemas {
                 continue;
             if (definition.isConstraintIndex())
                 continue;
+            if (definition.isMultiTokenIndex())
+                continue;
 
             Object label = getLabelForAssert(definition, definition.isNodeIndex());
             List<String> keys = new ArrayList<>();
