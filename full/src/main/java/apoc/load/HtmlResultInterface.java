97219dafa9e9137f11cf0f72225838df38ff47ec
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-05-02 20:07:27 +0200

    Add Scraper procedures to apoc.load.html
    Fixes #2384

---
+++ full/src/main/java/apoc/load/HtmlResultInterface.java
@@ -0,0 +1,26 @@
+package apoc.load;
+
+import org.jsoup.nodes.Document;
+import org.neo4j.logging.Log;
+
+import java.util.List;
+import java.util.Map;
+
+public interface HtmlResultInterface {
+    
+    enum Type {
+        DEFAULT(new SelectElement()), 
+        PLAIN_TEXT(new PlainText());
+
+        private final HtmlResultInterface resultInterface;
+        Type(HtmlResultInterface resultInterface) {
+            this.resultInterface = resultInterface;
+        }
+
+        public HtmlResultInterface get() {
+            return resultInterface;
+        }
+    }
+
+    Object getResult(Document document, String selector, Map<String, Object> config, List<String> errorList, Log log);
+}
