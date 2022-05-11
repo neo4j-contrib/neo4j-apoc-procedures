10f5ab9c3093e7ef70546c36223d731970107461
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-05-11 08:09:06 +0200

    Fix flaky TransactionProcedureTest (#2869)

--- full/src/test/java/apoc/monitor/TransactionProcedureTest.java
+++ full/src/test/java/apoc/monitor/TransactionProcedureTest.java
@@ -1,5 +1,6 @@
 package apoc.monitor;
 
+import apoc.cypher.CypherInitializer;
 import apoc.util.TestUtil;
 import org.junit.Before;
 import org.junit.Rule;
@@ -7,8 +8,13 @@ import org.junit.Test;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import static apoc.util.CypherInitializerUtil.getInitializer;
 import static apoc.util.TestUtil.testCall;
+import static org.hamcrest.Matchers.isOneOf;
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TransactionProcedureTest {
 
@@ -18,27 +24,47 @@ public class TransactionProcedureTest {
     @Before
     public void setup() {
         TestUtil.registerProcedure(db, Transaction.class);
+        // we need to wait until every CypherInitializer transaction is finished to make sure tests are not flaky
+        waitForInitializerBeingFinished(db);
     }
 
     @Test
     public void testGetStoreInfo() {
+        AtomicLong peakTx = new AtomicLong();
+        AtomicLong lastTxId = new AtomicLong();
+        AtomicLong totalOpenedTx = new AtomicLong();
+        AtomicLong totalTx = new AtomicLong();
         testCall(db, "CALL apoc.monitor.tx()", (row) -> {
             assertEquals(0l, row.get("rolledBackTx"));
-            assertEquals(1l, row.get("peakTx"));
-            assertEquals(1l, row.get("lastTxId"));
+            peakTx.set((long) row.get("peakTx"));
+            assertThat(peakTx.get(), isOneOf(1l, 2l));
+            assertEquals(1l, lastTxId.addAndGet((long) row.get("lastTxId")));
             assertEquals(1l, row.get("currentOpenedTx"));
-            assertEquals(2l, row.get("totalOpenedTx"));
-            assertEquals(1l, row.get("totalTx"));
+            assertEquals(3l, totalOpenedTx.addAndGet((long) row.get("totalOpenedTx")));
+            assertEquals(2l, totalTx.addAndGet((long )row.get("totalTx")));
         });
 
         db.executeTransactionally("create ()");
         testCall(db, "CALL apoc.monitor.tx()", (row) -> {
             assertEquals(0l, row.get("rolledBackTx"));
-            assertEquals(1l, row.get("peakTx"));
-            assertEquals(2l, row.get("lastTxId"));
+            assertEquals(peakTx.get(), row.get("peakTx"));
+            assertEquals(lastTxId.incrementAndGet(), row.get("lastTxId"));
             assertEquals(1l, row.get("currentOpenedTx"));
-            assertEquals(4l, row.get("totalOpenedTx"));
-            assertEquals(3l, row.get("totalTx"));
+            assertEquals(totalOpenedTx.addAndGet(2L), row.get("totalOpenedTx"));
+            assertEquals(totalTx.addAndGet(2L), row.get("totalTx"));
         });
+    }
+
+    // equivalent to CypherInitializerTest.waitForInitializerBeingFinished
+    private static void waitForInitializerBeingFinished(DbmsRule dbmsRule) {
+        CypherInitializer initializer = getInitializer(dbmsRule.databaseName(), dbmsRule, CypherInitializer.class);
+        while (!initializer.isFinished()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
