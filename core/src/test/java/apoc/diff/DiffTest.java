db7c5c58ab0993c461e4ac8c8f4cabea2c8f178c
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-08-01 15:25:20 +0200

    code clean

--- core/src/test/java/apoc/diff/DiffTest.java
+++ core/src/test/java/apoc/diff/DiffTest.java
@@ -12,8 +12,6 @@ import org.neo4j.internal.helpers.collection.Iterators;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
