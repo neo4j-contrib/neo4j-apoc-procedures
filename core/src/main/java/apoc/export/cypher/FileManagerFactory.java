7e2e65caf83df724546e1e1e1a754c7919c0384e
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-08-01 12:34:17 +0200

    Fixes #3070: The apoc.export.csv.all with bulkImport config doesn't work with a file name without extension

--- core/src/main/java/apoc/export/cypher/FileManagerFactory.java
+++ core/src/main/java/apoc/export/cypher/FileManagerFactory.java
@@ -2,8 +2,8 @@ package apoc.export.cypher;
 
 import java.io.OutputStream;
 import apoc.export.util.ExportConfig;
-import apoc.util.CompressionAlgo;
 import apoc.util.Util;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.PrintWriter;
@@ -19,6 +19,8 @@ import static apoc.util.FileUtils.getOutputStream;
  * @since 06.12.17
  */
 public class FileManagerFactory {
+    private final static String DOT = ".";
+    
     public static ExportFileManager createFileManager(String fileName, boolean separatedFiles) {
         return createFileManager(fileName, separatedFiles, ExportConfig.EMPTY);
     }
@@ -29,13 +31,7 @@ public class FileManagerFactory {
         }
         fileName = fileName.trim();
 
-        final CompressionAlgo compressionAlgo = CompressionAlgo.valueOf(config.getCompressionAlgo());
-
-        // In case of export with separated files (with bulkImport: true) in case of compressed file retrieve also the compression extension
-        // e.g. from test.one.two.csv to test.one.two.nodes.LABELNAME.csv.gz, test.one.two.relationships.RELTYPE.csv.gz, etc...
-        // otherwise it becomes test.one.two.nodes.LABELNAME.csv, etc..
-        int indexOfDot = StringUtils.lastOrdinalIndexOf(fileName, ".", compressionAlgo.equals(CompressionAlgo.NONE) ? 1 : 2);
-        String fileType = fileName.substring(indexOfDot + 1);
+        String fileType = FilenameUtils.getExtension(fileName);
         return new PhysicalExportFileManager(fileType, fileName, separatedFiles, config);
     }
 
@@ -48,7 +44,7 @@ public class FileManagerFactory {
         private ExportConfig config;
 
         public PhysicalExportFileManager(String fileType, String fileName, boolean separatedFiles, ExportConfig config) {
-            this.fileType = fileType;
+            this.fileType = StringUtils.isBlank(fileType) ? "" : fileType;
             this.fileName = fileName;
             this.separatedFiles = separatedFiles;
             this.config = config;
@@ -70,8 +66,15 @@ public class FileManagerFactory {
         }
 
         private String normalizeFileName(final String fileName, String suffix) {
+            if (StringUtils.isBlank(suffix)) {
+                return fileName;
+            }
+            // in case of file without dot extension, we just add the suffix 
+            if (StringUtils.isEmpty(fileType)) {
+                return fileName + DOT + suffix;
+            }
             // TODO check if this should be follow the same rules of FileUtils.readerFor
-            return fileName.replace("." + fileType, "." + (suffix != null ? suffix + "." + fileType : fileType));
+            return fileName.replace(fileType, suffix + DOT + fileType);
         }
 
         @Override
