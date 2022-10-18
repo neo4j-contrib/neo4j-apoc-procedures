b4da1c83035df5e076a85b6d535902f0356d58d7
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-10-18 15:34:08 +0200

    changed msg wording

--- core/src/main/java/apoc/trigger/Trigger.java
+++ core/src/main/java/apoc/trigger/Trigger.java
@@ -23,9 +23,7 @@ import java.util.stream.Stream;
 
 public class Trigger {
     // public for testing purpose
-    public static final String SYS_NON_LEADER_ERROR = "It's not possible to write into a cluster member with a non-LEADER system database.\n" +
-            "Either the procedure using the bolt against a core protocol with LEADER system database, \n" +
-            "or ";
+    public static final String SYS_NON_LEADER_ERROR = "It's not possible to write into a cluster member with a non-LEADER system database.\n";
 
     public static class TriggerInfo {
         public String name;
@@ -60,11 +58,11 @@ public class Trigger {
     @Context public Log log;
 
     private void preprocessDeprecatedProcedures() {
-        final String msgDeprecation =
-                "use the `apoc.trigger.install`, `apoc.trigger.drop`, `apoc.trigger.dropAll`, `apoc.trigger.stop`, and `apoc.trigger.start` procedures \n" + 
+        final String msgDeprecation = "Please note that the current procedure is deprecated, \n" +
+                "it's recommended to use the `apoc.trigger.install`, `apoc.trigger.drop`, `apoc.trigger.dropAll`, `apoc.trigger.stop`, and `apoc.trigger.start` procedures \n" + 
                 "instead of, respectively, `apoc.trigger.add`, `apoc.trigger.remove`, `apoc.trigger.removeAll`, `apoc.trigger.pause`, and `apoc.trigger.resume`.";
                 
-        log.warn("Please note that the current procedure is deprecated, \n" + msgDeprecation);
+        log.warn(msgDeprecation);
         
         if (!Util.isWriteableInstance(db, GraphDatabaseSettings.SYSTEM_DATABASE_NAME)) {
             throw new RuntimeException(SYS_NON_LEADER_ERROR + msgDeprecation);
