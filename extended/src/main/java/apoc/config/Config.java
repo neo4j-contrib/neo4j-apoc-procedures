0124b5ff8bfbf7eff376bc8d8806a0c0fc387d6f
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-01-30 11:12:42 +0100

    [PL7XdSMV] apoc.config.list and apoc.config.map leak evars (#3400)

--- extended/src/main/java/apoc/config/Config.java
+++ extended/src/main/java/apoc/config/Config.java
@@ -14,8 +14,24 @@ import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Procedure;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static apoc.ApocConfig.APOC_CONFIG_JOBS_POOL_NUM_THREADS;
+import static apoc.ApocConfig.APOC_CONFIG_JOBS_QUEUE_SIZE;
+import static apoc.ApocConfig.APOC_CONFIG_JOBS_SCHEDULED_NUM_THREADS;
+import static apoc.ApocConfig.APOC_EXPORT_FILE_ENABLED;
+import static apoc.ApocConfig.APOC_IMPORT_FILE_ALLOW__READ__FROM__FILESYSTEM;
+import static apoc.ApocConfig.APOC_IMPORT_FILE_ENABLED;
+import static apoc.ApocConfig.APOC_IMPORT_FILE_USE_NEO4J_CONFIG;
+import static apoc.ApocConfig.APOC_TRIGGER_ENABLED;
+import static apoc.ExtendedApocConfig.APOC_TTL_ENABLED;
+import static apoc.ExtendedApocConfig.APOC_TTL_LIMIT;
+import static apoc.ExtendedApocConfig.APOC_TTL_SCHEDULE;
+import static apoc.ExtendedApocConfig.APOC_UUID_ENABLED;
+import static apoc.ExtendedApocConfig.APOC_UUID_FORMAT;
+import static apoc.custom.CypherProceduresHandler.CUSTOM_PROCEDURES_REFRESH;
 
 /**
  * @author mh
@@ -23,7 +39,47 @@ import java.util.stream.Stream;
  */
 @Extended
 public class Config {
+    
+    // some config keys are hard-coded because belong to `core`, which is no longer accessed from `extended`
+    private static final Set<String> WHITELIST_CONFIGS = Set.of(
+            // apoc.import.
+            APOC_IMPORT_FILE_ENABLED,
+            APOC_IMPORT_FILE_USE_NEO4J_CONFIG,
+            APOC_IMPORT_FILE_ALLOW__READ__FROM__FILESYSTEM,
 
+            // apoc.export.
+            APOC_EXPORT_FILE_ENABLED,
+            
+            // apoc.trigger.
+            APOC_TRIGGER_ENABLED,
+            "apoc.trigger.refresh",
+
+            // apoc.uuid.
+            APOC_UUID_ENABLED,
+            APOC_UUID_FORMAT,
+            
+            // apoc.ttl.
+            APOC_TTL_SCHEDULE,
+            APOC_TTL_ENABLED,
+            APOC_TTL_LIMIT,
+            
+            // apoc.jobs.
+            APOC_CONFIG_JOBS_SCHEDULED_NUM_THREADS,
+            APOC_CONFIG_JOBS_POOL_NUM_THREADS,
+            APOC_CONFIG_JOBS_QUEUE_SIZE,
+            
+            // apoc.http.
+            "apoc.http.timeout.connect",
+            "apoc.http.timeout.read",
+            
+            // apoc.custom.
+            CUSTOM_PROCEDURES_REFRESH,
+
+            // apoc.spatial. - other configs can have sensitive credentials
+            "apoc.spatial.geocode.osm.throttle",
+            "apoc.spatial.geocode.google.throttle"
+    );
+    
     public static class ConfigResult {
         public final String key;
         public final Object value;
@@ -48,7 +104,8 @@ public class Config {
     public Stream<ConfigResult> list() {
         Util.checkAdmin(securityContext, callContext,"apoc.config.list");
         Configuration config = dependencyResolver.resolveDependency(ApocConfig.class).getConfig();
-        return Iterators.stream(config.getKeys()).map(s -> new ConfigResult(s, config.getString(s)));
+        return getApocConfigs(config)
+                .map(s -> new ConfigResult(s, config.getString(s)));
     }
 
     @Description("apoc.config.map | Lists the Neo4j configuration as map")
@@ -56,8 +113,14 @@ public class Config {
     public Stream<MapResult> map() {
         Util.checkAdmin(securityContext,callContext, "apoc.config.map");
         Configuration config = dependencyResolver.resolveDependency(ApocConfig.class).getConfig();
-        Map<String, Object> configMap = Iterators.stream(config.getKeys())
+        Map<String, Object> configMap = getApocConfigs(config)
                 .collect(Collectors.toMap(s -> s, s -> config.getString(s)));
         return Stream.of(new MapResult(configMap));
+    }
+
+    private static Stream<String> getApocConfigs(Configuration config) {
+        // we use startsWith(..) because we can have e.g. a config `apoc.uuid.enabled.<dbName>`
+        return Iterators.stream(config.getKeys())
+                .filter(conf -> WHITELIST_CONFIGS.stream().anyMatch(conf::startsWith));
     }
 }
