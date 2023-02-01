5a1ff56dff55df53576e3969156e9e6b922825dd
Gem Lamont <106068376+gem-neo4j@users.noreply.github.com>
2023-02-01 13:46:16 +0100

    [HQABUD5U] Improve coverNodes speed (#3439)

--- core/src/main/java/apoc/algo/Cover.java
+++ core/src/main/java/apoc/algo/Cover.java
@@ -12,6 +12,7 @@ import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,25 +27,16 @@ public class Cover {
     @Description("apoc.algo.cover(nodes) yield rel - returns all relationships between this set of nodes")
     public Stream<RelationshipResult> cover(@Name("nodes") Object nodes) {
         Set<Node> nodeSet = Util.nodeStream(tx, nodes).collect(Collectors.toSet());
-        /*return nodeSet.parallelStream()
-                .flatMap(n ->
-                        Util.inTx(db,() ->
-                        StreamSupport.stream(n.getRelationships(Direction.OUTGOING)
-                                .spliterator(),false)
-                                .filter(r -> nodeSet.contains(r.getEndNode()))
-                                .map(RelationshipResult::new)));*/
-        // NB prallel approach doesn't work in 3.4 (see SubgraphTest.testSubgraphAllShouldContainExpectedNodesAndRels
-        // so falling back to single threaded
-        // TODO: consider using multithreading and maybe kernel API here
         return coverNodes(nodeSet).map(RelationshipResult::new);
     }
 
     // non-parallelized utility method for use by other procedures
     public static Stream<Relationship> coverNodes(Collection<Node> nodes) {
+        Set<Node> nodeSet = new HashSet<>(nodes);
         return nodes.stream()
                 .flatMap(n ->
                         StreamSupport.stream(n.getRelationships(Direction.OUTGOING)
                                 .spliterator(),false)
-                                .filter(r -> nodes.contains(r.getEndNode())));
+                                .filter(r -> nodeSet.contains(r.getEndNode())));
     }
 }
