90b100407242025828fba1e85d403a4dae0ec0d4
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-02-21 10:38:22 +0100

    [NOID] Upgraded jsoup to v.1.15.3 to mitigate CVE-2022-36033 (#3305) (#3460)
    * [EHgXqsEW] Upgraded jsoup to v.1.15.3 to mitigate CVE-2022-36033 (#3305)
    * [EHgXqsEW] removed unused import

--- extended/src/main/java/apoc/load/LoadHtml.java
+++ extended/src/main/java/apoc/load/LoadHtml.java
@@ -4,6 +4,7 @@ import apoc.Extended;
 import apoc.result.MapResult;
 import apoc.util.MissingDependencyException;
 import apoc.util.FileUtils;
+import java.nio.charset.UnsupportedCharsetException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
@@ -19,7 +20,6 @@ import org.neo4j.procedure.Procedure;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,6 +35,8 @@ public class LoadHtml {
 
     // public for test purpose
     public static final String KEY_ERROR = "errorList";
+    public static final String INVALID_CONFIG_ERR = "Invalid config: ";
+    public static final String UNSUPPORTED_CHARSET_ERR = "Unsupported charset: ";
 
     @Context
     public GraphDatabaseService db;
@@ -74,12 +76,12 @@ public class LoadHtml {
             }
 
             return Stream.of(new MapResult(output));
+        } catch ( UnsupportedCharsetException e) {
+            throw new RuntimeException(UNSUPPORTED_CHARSET_ERR + config.getCharset());
         } catch (IllegalArgumentException | ClassCastException e) {
-            throw new RuntimeException("Invalid config: " + config);
+            throw new RuntimeException(INVALID_CONFIG_ERR + config);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found from: " + url);
-        } catch(UnsupportedEncodingException e) {
-            throw new RuntimeException("Unsupported charset: " + config.getCharset());
         } catch(Exception e) {
             throw new RuntimeException("Can't read the HTML from: "+ url, e);
         }
