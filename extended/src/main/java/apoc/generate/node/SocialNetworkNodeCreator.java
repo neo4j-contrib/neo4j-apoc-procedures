19dee42cbb932c0193ae6e6d0936fa20d169dc02
vga91 <giuseppe.villani@larus-ba.it>
2023-05-24 15:25:06 +0200

    [tW7Gyubn] Removed javafaker lib and replaced with custom class

--- extended/src/main/java/apoc/generate/node/SocialNetworkNodeCreator.java
+++ extended/src/main/java/apoc/generate/node/SocialNetworkNodeCreator.java
@@ -1,9 +1,10 @@
 package apoc.generate.node;
 
-import com.github.javafaker.Faker;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+
+import static apoc.util.Faker.getRandomFullName;
 
 /**
  * A {@link NodeCreator} that assigns every {@link Node} a "Person" {@link Label}, and a randomly generated English
@@ -14,15 +15,13 @@ public class SocialNetworkNodeCreator implements NodeCreator {
     private static final Label PERSON_LABEL = Label.label("Person");
     private static final String NAME = "name";
 
-    private final Faker faker = new Faker();
-
     /**
      * {@inheritDoc}
      */
     @Override
     public Node createNode(Transaction tx) {
         Node node = tx.createNode(PERSON_LABEL);
-        node.setProperty(NAME, faker.name().fullName());
+        node.setProperty(NAME, getRandomFullName());
         return node;
     }
 }
