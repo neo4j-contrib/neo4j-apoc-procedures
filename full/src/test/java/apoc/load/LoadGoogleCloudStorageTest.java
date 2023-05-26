20698ed5d17455ef320e823ea7f63c12b70016db
Gemma Lamont <gemma.lamont@neotechnology.com>
2023-05-26 15:21:07 +0200

    [NrNk9YvB] Bring over changes to GCS tests from 5.x

--- full/src/test/java/apoc/load/LoadGoogleCloudStorageTest.java
+++ full/src/test/java/apoc/load/LoadGoogleCloudStorageTest.java
@@ -1,25 +1,9 @@
-/*
- * Copyright (c) "Neo4j"
- * Neo4j Sweden AB [http://neo4j.com]
- *
- * This file is part of Neo4j.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package apoc.load;
 
+import apoc.util.GoogleCloudStorageContainerExtension;
 import apoc.util.TestUtil;
 import org.junit.*;
+
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
@@ -33,26 +17,30 @@ import static org.junit.Assert.assertFalse;
 
 public class LoadGoogleCloudStorageTest {
 
-    private static final String BUCKET_NAME = System.getenv("APOC_GC_BUCKET_NAME");
+    public static GoogleCloudStorageContainerExtension gcs = new GoogleCloudStorageContainerExtension()
+            .withMountedResourceFile("test.csv", "/folder/test.csv")
+            .withMountedResourceFile("map.json", "/folder/map.json");
+
+    @ClassRule
+    public static DbmsRule db = new ImpermanentDbmsRule();
 
     @BeforeClass
-    public static void before() {
-        Assume.assumeNotNull(BUCKET_NAME);
-    }
-
-
-    @Rule
-    public DbmsRule db = new ImpermanentDbmsRule();
-
-    @Before
-    public void setUp() throws Exception {
+    public static void setUp() throws Exception {
+        gcs.start();
         TestUtil.registerProcedure(db, LoadCsv.class, LoadJson.class);
     }
 
+    @AfterClass
+    public static void tearDown() {
+        gcs.close();
+        db.shutdown();
+    }
+
     @Test
-    public void testLoadCsvWithoutAuth() throws Exception {
-        String url = "gs://" + BUCKET_NAME + "/test.csv";
-        testResult(db, "CALL apoc.load.csv({url})", map("url", url), (r) -> {
+    public void testLoadCsv() {
+        String url = gcsUrl("b/folder/o/test.csv?alt=media");
+
+        testResult(db, "CALL apoc.load.csv($url)", map("url", url), (r) -> {
             assertRow(r, "Selma", "8", 0L);
             assertRow(r, "Rana", "11", 1L);
             assertRow(r, "Selina", "18", 2L);
@@ -61,35 +49,14 @@ public class LoadGoogleCloudStorageTest {
     }
 
     @Test
-    public void testLoadCsvFromFolderWithoutAuth() throws Exception {
-        String url = "gs://" + BUCKET_NAME + "/folder/test-folder.csv";
-        testResult(db, "CALL apoc.load.csv({url})", map("url", url), (r) -> {
-            assertRow(r, "Selma", "8", 0L);
-            assertRow(r, "Rana", "11", 1L);
-            assertRow(r, "Selina", "18", 2L);
-            assertFalse("It should be the last record", r.hasNext());
+    public void testLoadJSON() {
+        String url = gcsUrl("b/folder/o/map.json?alt=media");
+        testCall(db, "CALL apoc.load.jsonArray($url, '$.foo')", map("url", url), (r) -> {
+            assertEquals(asList(1L,2L,3L), r.get("value"));
         });
     }
 
-    @Test
-    public void testLoadCsvWithAuth() throws Exception { // N.B. This needs auth
-        String url = "gs://" + BUCKET_NAME + "/test-with-auth.csv?authenticationType=SERVICE";
-        testResult(db, "CALL apoc.load.csv({url})", map("url", url), (r) -> {
-            assertRow(r, "Selma", "8", 0L);
-            assertRow(r, "Rana", "11", 1L);
-            assertRow(r, "Selina", "18", 2L);
-            assertFalse("It should be the last record", r.hasNext());
-        });
+    private String gcsUrl(String path) {
+        return String.format("http://%s:%d/storage/v1/%s", gcs.getContainerIpAddress(), gcs.getMappedPort(4443), path);
     }
-
-    @Test
-    public void testLoadJSON() throws Exception {
-        String url = "gs://" + BUCKET_NAME + "/map.json";
-        testCall(db, "CALL apoc.load.jsonArray({url},'$.foo')",
-                map("url", url),
-                (row) -> {
-                    assertEquals(asList(1L,2L,3L), row.get("value"));
-                });
-    }
-
 }
