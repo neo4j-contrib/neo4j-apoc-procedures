129de7ba2a0b2158b9aeb12920efddd64de3f64e
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-07-24 12:54:25 +0200

    [drqYtb6r] apoc.cypher.run* procedures don't return results (extended) (#3624) (#3676)
    * [drqYtb6r] apoc.cypher.run* procedures don't return results (extended) (#3624)
    
    * [drqYtb6r] changed importFolder with the correct one provided by TestContainerUtil

--- extended/src/main/java/apoc/cypher/CypherExtended.java
+++ extended/src/main/java/apoc/cypher/CypherExtended.java
@@ -4,6 +4,7 @@ import apoc.Extended;
 import apoc.Pools;
 import apoc.result.MapResult;
 import apoc.util.CompressionAlgo;
+import apoc.util.EntityUtil;
 import apoc.util.FileUtils;
 import apoc.util.QueueBasedSpliterator;
 import apoc.util.QueueUtil;
@@ -242,7 +243,8 @@ public class CypherExtended {
             int row = 0;
             while (result.hasNext()) {
                 terminationGuard.check();
-                queue.put(new RowResult(row++, result.next()));
+                Map<String, Object> res = EntityUtil.anyRebind(tx, result.next());
+                queue.put(new RowResult(row++, res));
             }
             if (addStatistics) {
                 queue.put(new RowResult(-1, toMap(result.getQueryStatistics(), System.currentTimeMillis() - time, row)));
@@ -425,7 +427,7 @@ public class CypherExtended {
         }
         return futures.stream().flatMap(f -> {
             try {
-                return f.get().stream().map(MapResult::new);
+                return EntityUtil.anyRebind(tx, f.get()).stream().map(MapResult::new);
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException("Error executing in parallel " + statement, e);
             }
