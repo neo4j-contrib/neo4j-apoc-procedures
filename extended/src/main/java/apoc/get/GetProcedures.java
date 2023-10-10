c48fc34043dd90087e20c98df258e8690a45f24e
vga91 <giuseppe.villani@larus-ba.it>
2023-10-10 14:30:06 +0200

    Cast (InternalTransaction) consistent with core ones

--- extended/src/main/java/apoc/get/GetProcedures.java
+++ extended/src/main/java/apoc/get/GetProcedures.java
@@ -4,6 +4,7 @@ import apoc.Extended;
 import apoc.result.NodeResult;
 import apoc.result.RelationshipResult;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.impl.coreapi.InternalTransaction;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
@@ -20,13 +21,13 @@ public class GetProcedures {
     @Procedure
     @Description("apoc.get.nodes(node|id|[ids]) - quickly returns all nodes with these id's")
     public Stream<NodeResult> nodes(@Name("nodes") Object ids) {
-        return new Get(tx).nodes(ids);
+        return new Get((InternalTransaction) tx).nodes(ids);
     }
 
     @Procedure
     @Description("apoc.get.rels(rel|id|[ids]) - quickly returns all relationships with these id's")
     public Stream<RelationshipResult> rels(@Name("relationships") Object ids) {
-        return new Get(tx).rels(ids);
+        return new Get((InternalTransaction) tx).rels(ids);
     }
 
 }
