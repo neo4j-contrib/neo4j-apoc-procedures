68cc5cb6156e0a3c088400eab1e9c1029363227e
vga91 <giuseppe.villani@larus-ba.it>
2023-10-25 14:25:10 +0200

    restore GetProcedures.java

--- extended/src/main/java/apoc/ml/bedrock/BedrockUtil.java
+++ extended/src/main/java/apoc/ml/bedrock/BedrockUtil.java
@@ -4,6 +4,5 @@ package apoc.ml.bedrock;
 public class BedrockUtil {
     public static final String JURASSIC_2_ULTRA = "ai21.j2-ultra-v1";
     public static final String TITAN_EMBED_TEXT = "amazon.titan-embed-text-v1";
-    public static final String ANTHROPIC_CLAUDE_V2 = "anthropic.claude-v2";
     public static final String STABILITY_STABLE_DIFFUSION_XL = "stability.stable-diffusion-xl-v0";
 }
