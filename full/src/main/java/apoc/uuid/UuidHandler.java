b5ec61182a25742459aef7b5b351e6aaea3d35e7
Love Leifland <love.leifland@neotechnology.com>
2023-11-09 16:03:37 +0100

    [NOID] Code style

--- full/src/main/java/apoc/uuid/UuidHandler.java
+++ full/src/main/java/apoc/uuid/UuidHandler.java
@@ -43,11 +43,9 @@ import org.neo4j.scheduler.Group;
 import org.neo4j.scheduler.JobHandle;
 import org.neo4j.scheduler.JobScheduler;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Predicate;
