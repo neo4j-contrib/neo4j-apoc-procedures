de6c57ba218e6659d8baf766c48653c94c66f6e3
vga91 <giuseppe.villani@larus-ba.it>
2024-01-15 16:44:41 +0100

    Fixes #3426: add apache arrow import procedure to extended

---
+++ extended/src/main/java/apoc/export/arrow/ImportArrow.java
@@ -0,0 +1,225 @@
+package apoc.export.arrow;
+
+import apoc.Extended;
+import apoc.Pools;
+import apoc.export.util.BatchTransaction;
+import apoc.export.util.ProgressReporter;
+import apoc.result.ProgressInfo;
+import apoc.util.FileUtils;
+import apoc.util.Util;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.BitVector;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowFileReader;
+import org.apache.arrow.vector.ipc.ArrowReader;
+import org.apache.arrow.vector.ipc.ArrowStreamReader;
+import org.neo4j.graphdb.Entity;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.security.URLAccessChecker;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import static apoc.util.ExtendedUtil.toValidValue;
+
+@Extended
+public class ImportArrow {
+
+    // TODO: field similar to the one placed in ArrowUtils (placed in core)
+    //  when the Arrow procedures will be placed in extended remove these lines 
+    //  and replace FIELD_ID with FIELD_ID.getName(), FIELD_LABELS with FIELD_LABELS.getName(), etc..
+    public static String FIELD_ID = "<id>";
+    public static String FIELD_LABELS = "labels";
+    public static String FIELD_SOURCE_ID = "<source.id>";
+    public static String FIELD_TARGET_ID = "<target.id>";
+    public static String FIELD_TYPE = "<type>";
+    // -- end ArrowUtils fields
+    
+    @Context
+    public Pools pools;
+
+    @Context
+    public GraphDatabaseService db;
+    
+    @Context
+    public URLAccessChecker urlAccessChecker;
+    
+    
+    @Procedure(name = "apoc.import.arrow", mode = Mode.WRITE)
+    @Description("Imports arrow from the provided arrow file or byte array")
+    public Stream<ProgressInfo> importFile(@Name("input") Object input, @Name(value = "config", defaultValue = "{}") Map<String, Object> config) throws Exception {
+
+        ProgressInfo result =
+                Util.inThread(pools, () -> {
+                    String file = null;
+                    String sourceInfo = "binary";
+                    if (input instanceof String) {
+                        file =  (String) input;
+                        sourceInfo = "file";
+                    }
+
+                    final ArrowConfig conf = new ArrowConfig(config);
+
+                    final Map<Long, Long> idMapping = new HashMap<>();
+                    
+                    AtomicInteger counter = new AtomicInteger();
+                    try (ArrowReader reader = getReader(input);
+                         VectorSchemaRoot schemaRoot = reader.getVectorSchemaRoot()) {
+
+                        final ProgressReporter reporter = new ProgressReporter(null, null, new ProgressInfo(file, sourceInfo, "arrow"));
+                        BatchTransaction btx = new BatchTransaction(db, conf.getBatchSize(), reporter);
+                        try {
+                            while (hasElements(counter, reader, schemaRoot)) {
+
+                                final Map<String, Object> row = schemaRoot.getFieldVectors()
+                                        .stream()
+                                        .collect(
+                                                HashMap::new,
+                                                (map, fieldVector) -> {
+                                                    Object read = read(fieldVector, counter.get(), conf);
+                                                    if (read == null) {
+                                                        return;
+                                                    }
+                                                    map.put(fieldVector.getName(), read);
+                                                },
+                                                HashMap::putAll);
+
+                                String relType = (String) row.remove(FIELD_TYPE);
+                                if (relType == null) {
+                                    // is node
+                                    String[] stringLabels = (String[]) row.remove(FIELD_LABELS);
+                                    Label[] labels = Optional.ofNullable(stringLabels)
+                                            .map(l -> Arrays.stream(l).map(Label::label).toArray(Label[]::new))
+                                            .orElse(new Label[]{});
+                                    final Node node = btx.getTransaction().createNode(labels);
+
+                                    long id = (long) row.remove(FIELD_ID);
+                                    idMapping.put(id, node.getId());
+
+                                    addProps(row, node);
+                                    reporter.update(1, 0, row.size());
+                                } else {
+                                    // is relationship
+                                    long sourceId = (long) row.remove(FIELD_SOURCE_ID);
+                                    Long idSource = idMapping.get(sourceId);
+                                    final Node source = btx.getTransaction().getNodeById(idSource);
+
+                                    long targetId = (long) row.remove(FIELD_TARGET_ID);
+                                    Long idTarget = idMapping.get(targetId);
+                                    final Node target = btx.getTransaction().getNodeById(idTarget);
+
+                                    final Relationship rel = source.createRelationshipTo(target, RelationshipType.withName(relType));
+                                    addProps(row, rel);
+                                    reporter.update(0, 1, row.size());
+                                }
+                                
+                                counter.incrementAndGet();
+                                btx.increment();
+                            }
+                            
+                            btx.doCommit();
+                        } catch (RuntimeException e) {
+                            btx.rollback();
+                            throw e;
+                        } finally {
+                            btx.close();
+                        }
+
+                        return reporter.getTotal();
+                    }
+                });
+
+        return Stream.of(result);
+    }
+
+    
+    private ArrowReader getReader(Object input) throws IOException {
+        RootAllocator allocator = new RootAllocator();
+        if (input instanceof String) {
+            final SeekableByteChannel channel = FileUtils.inputStreamFor(input, null, null, null, urlAccessChecker)
+                    .asChannel();
+            return new ArrowFileReader(channel, allocator);
+        }
+        ByteArrayInputStream inputStream = new ByteArrayInputStream((byte[]) input);
+        return new ArrowStreamReader(inputStream, allocator);
+    }
+
+
+    private static boolean hasElements(AtomicInteger counter, ArrowReader reader, VectorSchemaRoot schemaRoot) throws IOException {
+        if (counter.get() >= schemaRoot.getRowCount()) {
+            if (reader.loadNextBatch()) {
+                counter.set(0);
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    private static Object read(FieldVector fieldVector, int index, ArrowConfig conf) {
+        
+        if (fieldVector.isNull(index)) {
+            return null;
+        } else if (fieldVector instanceof BitVector) {
+            BitVector fe = (BitVector) fieldVector;
+            return fe.get(index) == 1;
+        } else {
+            Object object = fieldVector.getObject(index);
+            if (object instanceof Collection coll && coll.isEmpty()) {
+                return null;
+            }
+            return toValidValue(object, fieldVector.getName(), conf.getMapping());
+        }
+    }
+
+    private void addProps(Map<String, Object> row, Entity rel) {
+        row.forEach(rel::setProperty);
+    }
+
+    /**
+     * Analog to ArrowConfig present in APOC Core.
+     * TODO Merge these 2 classes when arrow procedure will be moved to APOC Extended
+     */
+    public static class ArrowConfig {
+
+        private final int batchSize;
+        private final Map<String, Object> mapping;
+
+        public ArrowConfig(Map<String, Object> config) {
+            if (config == null) {
+                config = Collections.emptyMap();
+            }
+            this.mapping = (Map<String, Object>) config.getOrDefault("mapping", Map.of());
+            this.batchSize = Util.toInteger(config.getOrDefault("batchSize", 2000));
+        }
+
+        public int getBatchSize() {
+            return batchSize;
+        }
+
+        public Map<String, Object> getMapping() {
+            return mapping;
+        }
+    }
+
+}
--- extended/src/main/java/apoc/export/parquet/ParquetReadUtil.java
+++ extended/src/main/java/apoc/export/parquet/ParquetReadUtil.java
@@ -1,166 +1,25 @@
 package apoc.export.parquet;
 
 import apoc.util.CompressionAlgo;
+import apoc.util.ExtendedUtil;
 import apoc.util.FileUtils;
-import apoc.util.JsonUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.parquet.io.DelegatingSeekableInputStream;
 import org.apache.parquet.io.InputFile;
 import org.apache.parquet.io.SeekableInputStream;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.neo4j.graphdb.security.URLAccessChecker;
-import org.neo4j.values.storable.DateTimeValue;
-import org.neo4j.values.storable.DateValue;
-import org.neo4j.values.storable.DurationValue;
-import org.neo4j.values.storable.LocalDateTimeValue;
-import org.neo4j.values.storable.LocalTimeValue;
-import org.neo4j.values.storable.PointValue;
-import org.neo4j.values.storable.TimeValue;
-import org.neo4j.values.storable.Values;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 
 public class ParquetReadUtil {
 
     public static Object toValidValue(Object object, String field, ParquetConfig config) {
-        Object fieldName = config.getMapping().get(field);
-        if (object != null && fieldName != null) {
-            return convertValue(object.toString(), fieldName.toString());
-        }
-
-        if (object instanceof Collection) {
-            // if there isn't a mapping config, we convert the list to a String[]
-            List<Object> collect = ((Collection<?>) object).stream()
-                    .map(i -> toValidValue(i, field, config))
-                    .collect(Collectors.toList());
-            try {
-                return collect.toArray(new String[0]);
-            } catch (ArrayStoreException e) {
-                return collect.toArray(new Object[0]);
-            }
-        }
-        if (object instanceof Map) {
-            return ((Map<String, Object>) object).entrySet().stream()
-                    .collect(Collectors.toMap(Map.Entry::getKey, e -> toValidValue(e.getValue(), field, config)));
-        }
-        try {
-            // we test if is a valid Neo4j type
-            Values.of(object);
-            return object;
-        } catch (Exception e) {
-            // otherwise we try to coerce it
-            return object.toString();
-        }
-    }
-
-    /**
-     * In case of complex type non-readable from Parquet, i.e. Duration, Point, List of Neo4j Types...
-     * we can use the `mapping: {keyToConvert: valueTypeName}` config to convert them.
-     * For example `mapping: {myPropertyKey: "DateArray"}`
-     */
-    private static Object convertValue(String value, String typeName) {
-        switch (typeName) {
-            case "Point":
-                return PointValue.parse(value);
-            case "LocalDateTime":
-                return LocalDateTimeValue.parse(value).asObjectCopy();
-            case "LocalTime":
-                return LocalTimeValue.parse(value).asObjectCopy();
-            case "DateTime":
-                return DateTimeValue.parse(value, () -> ZoneId.of("Z")).asObjectCopy();
-            case "Time":
-                return TimeValue.parse(value, () -> ZoneId.of("Z")).asObjectCopy();
-            case "Date":
-                return DateValue.parse(value).asObjectCopy();
-            case "Duration":
-                return DurationValue.parse(value);
-            case "Char":
-                return value.charAt(0);
-            case "Byte":
-                return value.getBytes();
-            case "Double":
-                return Double.parseDouble(value);
-            case "Float":
-                return Float.parseFloat(value);
-            case "Short":
-                return Short.parseShort(value);
-            case "Int":
-                return Integer.parseInt(value);
-            case "Long":
-                return Long.parseLong(value);
-            case "Node", "Relationship":
-                return JsonUtil.parse(value, null, Map.class);
-            case "NO_VALUE":
-                return null;
-            default:
-                // If ends with "Array", for example StringArray
-                if (typeName.endsWith("Array")) {
-                    value = StringUtils.removeStart(value, "[");
-                    value = StringUtils.removeEnd(value, "]");
-                    String array = typeName.replace("Array", "");
-
-                    final Object[] prototype = getPrototypeFor(array);
-                    return Arrays.stream(value.split(","))
-                            .map(item -> convertValue(StringUtils.trim(item), array))
-                            .toList()
-                            .toArray(prototype);
-                }
-                return value;
-        }
-    }
-
-    // similar to CsvPropertyConverter
-    static Object[] getPrototypeFor(String type) {
-        switch (type) {
-            case "Long":
-                return new Long[]{};
-            case "Integer":
-                return new Integer[]{};
-            case "Double":
-                return new Double[]{};
-            case "Float":
-                return new Float[]{};
-            case "Boolean":
-                return new Boolean[]{};
-            case "Byte":
-                return new Byte[]{};
-            case "Short":
-                return new Short[]{};
-            case "Char":
-                return new Character[]{};
-            case "String":
-                return new String[]{};
-            case "DateTime":
-                return new ZonedDateTime[]{};
-            case "LocalTime":
-                return new LocalTime[]{};
-            case "LocalDateTime":
-                return new LocalDateTime[]{};
-            case "Point":
-                return new PointValue[]{};
-            case "Time":
-                return new OffsetTime[]{};
-            case "Date":
-                return new LocalDate[]{};
-            case "Duration":
-                return new DurationValue[]{};
-            default:
-                throw new IllegalStateException("Type " + type + " not supported.");
-        }
+        Map<String, Object> mapping = config.getMapping();
+        return ExtendedUtil.toValidValue(object, field, mapping);
     }
 
     public static java.util.concurrent.TimeUnit toTimeUnitJava(LogicalTypeAnnotation.TimeUnit unit) {
