4f6ff8c9853af0adb1e42a300ca7c0b3f5d07dd9
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-02-26 10:32:08 +0100

    Fixes #2136: Support for additional HTTP headers in apoc.es procedures (#3953)

---
+++ extended/src/main/java/apoc/es/ElasticSearchConfig.java
@@ -0,0 +1,32 @@
+package apoc.es;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ElasticSearchConfig {
+    public static final String HEADERS_KEY = "headers";
+
+    private final Map<String, Object> headers;
+
+    public ElasticSearchConfig(Map<String, Object> config) {
+        this(config, null);
+    }
+    
+    public ElasticSearchConfig(Map<String, Object> config, String httpMethod) {
+        if (config == null) {
+            config = Collections.emptyMap();
+        }
+        
+        Map<String, Object> headerConf = (Map<String, Object>) config.getOrDefault(HEADERS_KEY, new HashMap<>());
+        headerConf.putIfAbsent("content-type", "application/json");
+        if (httpMethod != null) {
+            headerConf.putIfAbsent("method", httpMethod);
+        }
+        this.headers = headerConf;
+    }
+
+    public Map<String, Object> getHeaders() {
+        return headers;
+    }
+}
