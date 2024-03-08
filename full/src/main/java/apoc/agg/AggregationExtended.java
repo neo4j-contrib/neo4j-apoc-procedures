f50a8bd4269dfa695f6853a7916c269453ed9820
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-03-08 14:27:29 +0100

    [NOID] Fixes #2390: apoc.agg.position(agg-value, element) -> integer (#3955) (#3988)
    * [NOID] Fixes #2390: apoc.agg.position(agg-value, element) -> integer (#3955)
    
    * Fixes #2390: apoc.agg.position(agg-value, element) -> integer
    
    * Removed unused imports
    
    * updated extended.txt
    
    * [NOID] fix compile error

---
+++ full/src/main/java/apoc/agg/AggregationExtended.java
@@ -0,0 +1,69 @@
+package apoc.agg;
+
+import apoc.Extended;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.helpers.collection.Iterables;
+import org.neo4j.internal.helpers.collection.Iterators;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.UserAggregationFunction;
+import org.neo4j.procedure.UserAggregationResult;
+import org.neo4j.procedure.UserAggregationUpdate;
+
+@Extended
+public class AggregationExtended {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Context
+    public Transaction tx;
+
+    @UserAggregationFunction("apoc.agg.row")
+    @Description("apoc.agg.row(element, predicate) - Returns index of the `element` that match the given `predicate`")
+    public RowFunction row() {
+        BiPredicate<Object, Object> curr = (current, value) -> db.executeTransactionally(
+                "RETURN " + value,
+                Map.of("curr", current),
+                result -> Iterators.singleOrNull(result.<Boolean>columnAs(Iterables.single(result.columns()))));
+        return new RowFunction(curr);
+    }
+
+    @UserAggregationFunction("apoc.agg.position")
+    @Description("apoc.agg.position(element, value) - Returns index of the `element` that match the given `value`")
+    public RowFunction position() {
+        return new RowFunction(Object::equals);
+    }
+
+    public static class RowFunction {
+        private boolean found;
+        private final BiPredicate<Object, Object> biPredicate;
+        private long index = -1L;
+
+        public RowFunction(BiPredicate<Object, Object> biPredicate) {
+            this.biPredicate = biPredicate;
+        }
+
+        @UserAggregationUpdate
+        public void update(@Name("value") Object value, @Name("element") Object element) {
+            if (!found) {
+                try {
+                    found = this.biPredicate.test(value, element);
+                } catch (Exception e) {
+                    throw new RuntimeException(
+                            "The predicate query has thrown the following exception: \n" + e.getMessage());
+                }
+                index++;
+            }
+        }
+
+        @UserAggregationResult
+        public Object result() {
+            return index;
+        }
+    }
+}
