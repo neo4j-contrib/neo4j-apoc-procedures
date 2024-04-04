b91a711d51d968acfaada54fa4d6cdb27132fce2
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-04-04 10:30:28 +0200

    [NOID] Fixes #3526: Add a simple apoc.util.hashCode function that turns a scalar into a long (#4011) (#4031)
    * Fixes #3526: Add a simple apoc.util.hashCode function that turns a scalar into a long
    
    * Update extended.txt

---
+++ full/src/test/java/apoc/util/UtilsExtendedTest.java
@@ -0,0 +1,29 @@
+package apoc.util;
+
+import static apoc.util.TestUtil.testCall;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.neo4j.test.rule.DbmsRule;
+import org.neo4j.test.rule.ImpermanentDbmsRule;
+
+public class UtilsExtendedTest {
+
+    @ClassRule
+    public static DbmsRule db = new ImpermanentDbmsRule();
+
+    @BeforeClass
+    public static void setUp() {
+        TestUtil.registerProcedure(db, UtilsExtended.class);
+    }
+
+    @Test
+    public void testMultipleCharsetsCompressionWithDifferentResults() {
+        testCall(
+                db,
+                "RETURN apoc.util.hashCode(rand()) AS hashCode",
+                r -> assertTrue(r.get("hashCode") instanceof Long));
+    }
+}
