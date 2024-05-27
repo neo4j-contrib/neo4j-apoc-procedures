7bb63f58e31bf7ae3df859511deb699c82999ae8
Gem Lamont <106068376+gem-neo4j@users.noreply.github.com>
2024-05-27 08:38:59 +0200

    [WVuGDROz] Allow cypher.export.schema return using a stream (#4072)

--- core/src/main/java/apoc/export/cypher/ExportCypher.java
+++ core/src/main/java/apoc/export/cypher/ExportCypher.java
@@ -148,9 +148,6 @@ public class ExportCypher {
             throws IOException {
         if (Util.isNullOrEmpty(fileName)) fileName = null;
         String source = String.format("database: nodes(%d), rels(%d)", Util.nodeCount(tx), Util.relCount(tx));
-        // streamStatements
-        config.put("stream", false);
-        config.put("streamStatements", false);
         return exportCypher(fileName, source, new DatabaseSubGraph(tx), new ExportConfig(config), true);
     }
 
@@ -202,7 +199,7 @@ public class ExportCypher {
             ExportFileManager cypherFileManager) {
         MultiStatementCypherSubGraphExporter exporter = new MultiStatementCypherSubGraphExporter(graph, c, db);
 
-        if (onlySchema) exporter.exportOnlySchema(cypherFileManager, c);
+        if (onlySchema) exporter.exportOnlySchema(cypherFileManager, reporter, c);
         else exporter.export(c, reporter, cypherFileManager);
     }
 
