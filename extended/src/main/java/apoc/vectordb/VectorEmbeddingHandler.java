303cb7049fb1106260437c995a8e20c6af6ae84b
vga91 <giuseppe.villani@larus-ba.it>
2024-05-28 11:34:05 +0200

    changes review

--- extended/src/main/java/apoc/vectordb/VectorEmbeddingHandler.java
+++ extended/src/main/java/apoc/vectordb/VectorEmbeddingHandler.java
@@ -1,23 +1,11 @@
 package apoc.vectordb;
 
 import apoc.ml.RestAPIConfig;
-import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.neo4j.internal.kernel.api.procs.ProcedureCallContext;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import static apoc.ml.RestAPIConfig.BODY_KEY;
-import static apoc.ml.RestAPIConfig.ENDPOINT_KEY;
-import static apoc.ml.RestAPIConfig.HEADERS_KEY;
-import static apoc.ml.RestAPIConfig.JSON_PATH_KEY;
-import static apoc.ml.RestAPIConfig.METHOD_KEY;
-import static apoc.util.MapUtil.map;
 import static apoc.vectordb.VectorEmbeddingConfig.*;
 
 public interface VectorEmbeddingHandler {
