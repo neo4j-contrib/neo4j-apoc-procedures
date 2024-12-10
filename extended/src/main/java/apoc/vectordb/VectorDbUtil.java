55e2ced10383c67de3f328aee630331158786d4b
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-12-10 23:52:26 +0100

    Fixes #4232: The apoc.vectordb.configure(WEAVIATE', ..) procedure should append /v1 to url (#4248) (#4293)

--- extended/src/main/java/apoc/vectordb/VectorDbUtil.java
+++ extended/src/main/java/apoc/vectordb/VectorDbUtil.java
@@ -130,4 +130,18 @@ public class VectorDbUtil {
         config.put(METHOD_KEY, null);
         config.put(BODY_KEY, null);
     }
+
+    /**
+     * If the vectorDb is WEAVIATE and endpoint doesn't end with `/vN`, where N is a number,
+     * then add `/v1` to the endpoint
+     */
+    public static String appendVersionUrlIfNeeded(VectorDbHandler.Type type, String host) {
+        if (VectorDbHandler.Type.WEAVIATE == type) {
+            String regex = ".*(/v\\d+)$";
+            if (!host.matches(regex)) {
+                host = host + "/v1";
+            }
+        }
+        return host;
+    }
 }
