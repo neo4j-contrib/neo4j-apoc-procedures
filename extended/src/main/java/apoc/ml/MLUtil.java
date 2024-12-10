0f43ec93a710a2be8b074c368360faa548fca0cc
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-12-10 23:51:53 +0100

    Fixes #4156: Improves handling of empty or blank input for openai procedures (#4228) (#4296)
    * Fixes #4156: Improves handling of empty or blank input for openai procedures
    
    * fix tests
    
    * changed boolean conditions

--- extended/src/main/java/apoc/ml/MLUtil.java
+++ extended/src/main/java/apoc/ml/MLUtil.java
@@ -1,7 +1,7 @@
 package apoc.ml;
 
 public class MLUtil {
-    public static final String ERROR_NULL_INPUT = "The input provided is null. Please specify a valid input";
+    public static final String ERROR_NULL_INPUT = "Null, blank or empty input provided. Please specify a valid input";
 
     public static final String ENDPOINT_CONF_KEY = "endpoint";
     public static final String API_VERSION_CONF_KEY = "apiVersion";
--- extended/src/test/java/apoc/ml/MLTestUtil.java
+++ extended/src/test/java/apoc/ml/MLTestUtil.java
@@ -1,25 +1,15 @@
 package apoc.ml;
 
+import apoc.util.ExtendedTestUtil;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 import java.util.Map;
 
-import static apoc.ml.MLUtil.ERROR_NULL_INPUT;
-import static apoc.util.TestUtil.testCall;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static apoc.ml.MLUtil.*;
 
 public class MLTestUtil {
+    
     public static void assertNullInputFails(GraphDatabaseService db, String query, Map<String, Object> params) {
-        try {
-            testCall(db, query, params,
-                    (row) -> fail("Should fail due to null input")
-            );
-        } catch (RuntimeException e) {
-            String message = e.getMessage();
-            assertTrue("Current error message is: " + message, 
-                    message.contains(ERROR_NULL_INPUT)
-            );
-        }
+        ExtendedTestUtil.assertFails(db, query, params, ERROR_NULL_INPUT);
     }
 }
