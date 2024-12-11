ffa55e4baa5712025a1a35dba240856ef0f8eb6a
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-12-11 17:11:42 +0100

    [NOID] Fixes #4153: Handling OpenAI 429's gracefully (#4284)
    * Fixes #4153: Handling OpenAI 429's gracefully
    
    * cleanup
    
    * fix tests

---
+++ full/src/main/java/apoc/util/ExtendedUtil.java
@@ -0,0 +1,58 @@
+package apoc.util;
+
+import java.time.Duration;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ExtendedUtil {
+    public static <T> T withBackOffRetries(
+            Supplier<T> func,
+            boolean retry,
+            int backoffRetry,
+            boolean exponential,
+            Consumer<Exception> exceptionHandler) {
+        T result;
+        backoffRetry = backoffRetry < 1 ? 5 : backoffRetry;
+        int countDown = backoffRetry;
+        exceptionHandler = Objects.requireNonNullElse(exceptionHandler, exe -> {});
+        while (true) {
+            try {
+                result = func.get();
+                break;
+            } catch (Exception e) {
+                if (!retry || countDown < 1) throw e;
+                exceptionHandler.accept(e);
+                countDown--;
+                long delay = getDelay(backoffRetry, countDown, exponential);
+                backoffSleep(delay);
+            }
+        }
+        return result;
+    }
+
+    private static void backoffSleep(long millis) {
+        sleep(millis, "Operation interrupted during backoff");
+    }
+
+    public static void sleep(long millis, String interruptedMessage) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(interruptedMessage, ie);
+        }
+    }
+
+    private static long getDelay(int backoffRetry, int countDown, boolean exponential) {
+        int backOffTime = backoffRetry - countDown;
+        long sleepMultiplier = exponential
+                ? (long) Math.pow(2, backOffTime)
+                : // Exponential retry progression
+                backOffTime; // Linear retry progression
+        return Math.min(
+                Duration.ofSeconds(1).multipliedBy(sleepMultiplier).toMillis(),
+                Duration.ofSeconds(30).toMillis() // Max 30s
+                );
+    }
+}
