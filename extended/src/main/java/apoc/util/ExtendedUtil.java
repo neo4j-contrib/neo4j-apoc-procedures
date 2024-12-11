421f0bd79b57314f6e5f3d5ef185edc504bcea0f
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2024-12-11 17:11:42 +0100

    Fixes #4153: Handling OpenAI 429's gracefully (#4284)
    * Fixes #4153: Handling OpenAI 429's gracefully
    
    * cleanup
    
    * fix tests

--- extended/src/main/java/apoc/util/ExtendedUtil.java
+++ extended/src/main/java/apoc/util/ExtendedUtil.java
@@ -30,14 +30,9 @@ import java.time.OffsetTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.TemporalAccessor;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
@@ -353,5 +348,59 @@ public class ExtendedUtil
         }
         return floats;
     }
-            
+
+    public static <T> T withBackOffRetries(
+            Supplier<T> func,
+            boolean retry,
+            int backoffRetry,
+            boolean exponential,
+            Consumer<Exception> exceptionHandler
+    ) {
+        T result;
+        backoffRetry = backoffRetry < 1 
+                ? 5
+                : backoffRetry;
+        int countDown = backoffRetry;
+        exceptionHandler = Objects.requireNonNullElse(exceptionHandler, exe -> {});
+        while (true) {
+            try {
+                result = func.get();
+                break;
+            } catch (Exception e) {
+                if(!retry || countDown < 1) throw e;
+                exceptionHandler.accept(e);
+                countDown--;
+                long delay = getDelay(backoffRetry, countDown, exponential);
+                backoffSleep(delay);
+            }
+        }
+        return result;
+    }
+
+    private static void backoffSleep(long millis){
+        sleep(millis, "Operation interrupted during backoff");
+    }
+
+    public static void sleep(long millis, String interruptedMessage) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(interruptedMessage, ie);
+        }
+    }
+
+    private static long getDelay(int backoffRetry, int countDown, boolean exponential) {
+        int backOffTime = backoffRetry - countDown;
+        long sleepMultiplier = exponential ?
+                (long) Math.pow(2, backOffTime) : // Exponential retry progression
+                backOffTime; // Linear retry progression
+        return Math.min(
+                Duration.ofSeconds(1)
+                        .multipliedBy(sleepMultiplier)
+                        .toMillis(),
+                Duration.ofSeconds(30).toMillis() // Max 30s
+        );
+    }
+
 }
