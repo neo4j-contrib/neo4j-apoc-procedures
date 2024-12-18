6f9baa0e31ded2eb1ffee0763c7909a4a05acd7b
vga91 <giuseppe.villani@larus-ba.it>
2024-12-18 09:25:01 +0100

    [NOID] test license

--- full/src/main/java/apoc/agg/MultiStats.java
+++ full/src/main/java/apoc/agg/MultiStats.java
@@ -14,6 +14,7 @@ import org.neo4j.procedure.UserAggregationUpdate;
 
 @Extended
 public class MultiStats {
+    // test
 
     @UserAggregationFunction("apoc.agg.multiStats")
     @Description("Return a multi-dimensional aggregation")
