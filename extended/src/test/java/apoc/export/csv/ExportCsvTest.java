f57270c4b3f5401f189046a851205245f0cb72e5
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2025-05-12 17:51:20 +0200

    Fix invalid Groq models (#4425) (#4429)

--- extended/src/test/java/apoc/export/csv/ExportCsvTest.java
+++ extended/src/test/java/apoc/export/csv/ExportCsvTest.java
@@ -10,10 +10,7 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.dbms.api.DatabaseManagementService;
@@ -36,6 +33,7 @@ import static org.junit.Assert.assertEquals;
  * @author mh
  * @since 22.05.16
  */
+@Ignore("It fails due to `java.lang.NoClassDefFoundError: org/eclipse/jetty/servlet`")
 public class ExportCsvTest {
     private static final String EXPECTED = String.format("\"_id\",\"_labels\",\"age\",\"city\",\"kids\",\"male\",\"name\",\"street\",\"_start\",\"_end\",\"_type\"%n" +
             "\"0\",\":User:User1\",\"42\",\"\",\"[\"\"a\"\",\"\"b\"\",\"\"c\"\"]\",\"true\",\"foo\",\"\",,,%n" +
