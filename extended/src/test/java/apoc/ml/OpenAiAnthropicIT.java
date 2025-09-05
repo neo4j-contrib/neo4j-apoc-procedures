291408ddbb8f8a476c59f373a52172f77e4f3215
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2025-09-05 16:00:43 +0200

    Fix OpenAI with Anthropic default model (#4423) (#4476)
    * WIP - fix anthropic default model
    
    * Solve flaky systemDbGraphProcedureShouldObfuscateSensitiveData test (#4415)
    
    * Solve flaky systemDbGraphProcedureShouldObfuscateSensitiveData test
    
    * changed certificate command to inside the container
    
    * improved tests
    
    * added comment

--- extended/src/test/java/apoc/ml/OpenAiAnthropicIT.java
+++ extended/src/test/java/apoc/ml/OpenAiAnthropicIT.java
@@ -1,20 +1,21 @@
 package apoc.ml;
 
 import apoc.util.TestUtil;
+import apoc.util.Util;
 import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static apoc.ml.MLUtil.ANTHROPIC_VERSION;
 import static apoc.ml.MLUtil.MAX_TOKENS;
@@ -29,12 +30,29 @@ import static apoc.util.TestUtil.testCall;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OpenAIAnthropicIT {
+@RunWith(Parameterized.class)
+public class OpenAiAnthropicIT {
 
-    private String anthropicApiKey;
-    
+    protected String anthropicApiKey;
+
     @Rule
     public DbmsRule db = new ImpermanentDbmsRule();
+
+    @Parameterized.Parameters(name = "chatModel: {0}, completionModel: {1}")
+    public static Collection<String[]> data() {
+        return Arrays.asList(new String[][] {
+                // tests with model evaluated
+                {"claude-3-5-sonnet-20240620", "claude-2"},
+                // tests with default model
+                {null, null}
+        });
+    }
+
+    @Parameterized.Parameter(0)
+    public String chatModel;
+
+    @Parameterized.Parameter(1)
+    public String completionModel;
 
     @Before
     public void setUp() throws Exception {
@@ -43,28 +61,15 @@ public class OpenAIAnthropicIT {
         TestUtil.registerProcedure(db, OpenAI.class);
     }
 
-    @Test
-    public void completionWithAnthropic() {
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name()
-        );
-        testCall(db, COMPLETION_QUERY_EXTENDED_PROMPT,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
-                (row) -> {
-                    var result = (Map<String,Object>) row.get("value");
-                    var completion = (String) result.get("completion");
-                    assertTrue(completion.toLowerCase().contains("blue"),
-                            "Actual generatedText is " + completion);
-                });
-    }
 
     @Test
     public void chatWithAnthropic() {
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name()
+        Map<String, Object> conf = Util.map(
+                API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, chatModel
         );
         testCall(db, CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
                     var contentList = (List<Map<String, Object>>) result.get("content");
@@ -82,32 +87,33 @@ public class OpenAIAnthropicIT {
         String base64Image = Base64.getEncoder().encodeToString(fileContent);
 
         List<Map<String, ?>> contentBody = List.of(
-                Map.of(
+                Util.map(
                         "type", "image",
-                        "source", Map.of(
+                        "source", Util.map(
                                 "type", "base64",
                                 "media_type", "image/jpeg",
                                 "data", base64Image
                         )
                 ),
-                Map.of(
+                Util.map(
                         "type", "text",
                         "text", "What is in the above image?"
                 )
         );
 
-        List<Map<String, Object>> messages = List.of(Map.of(
+        List<Map<String, Object>> messages = List.of(Util.map(
                 "role", "user",
                 "content", contentBody
         ));
 
         String query = "CALL apoc.ml.openai.chat($messages, $apiKey, $conf)";
 
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name()
+        Map<String, Object> conf = Util.map(
+                API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, chatModel
         );
         testCall(db, query,
-                Map.of( "messages", messages,"conf", conf, "apiKey", anthropicApiKey),
+                Util.map( "messages", messages,"conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
                     var contentList = (List<Map<String, Object>>) result.get("content");
@@ -118,32 +124,45 @@ public class OpenAIAnthropicIT {
     }
 
     @Test
-    public void completionWithAnthropicNonDefaultModel() {
-        String modelId = "claude-3-haiku-20240307";
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name(),
-                MODEL_CONF_KEY, modelId
+    public void completionWithAnthropic() {
+        Map<String, Object> conf = Util.map(
+                API_TYPE_CONF_KEY, ANTHROPIC.name()
         );
-        testCall(db, CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+        testCall(db, COMPLETION_QUERY_EXTENDED_PROMPT,
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
-                    var contentList = (List<Map<String, Object>>) result.get("content");
-                    Map<String, Object> content = contentList.get(0);
-                    String generatedText = (String) content.get("text");
-                    assertTrue(generatedText.toLowerCase().contains("earth"),
-                            "Actual generatedText is " + generatedText);
+                    var completion = (String) result.get("completion");
+                    assertTrue(completion.toLowerCase().contains("blue"),
+                            "Actual generatedText is " + completion);
                 });
     }
 
     @Test
-    public void completionWithAnthropicNonDefaultMaxTokens() {
-        Map<String, Object> conf = Map.of(
+    public void completionWithAnthropicUnknownModel() {
+        String modelId = "unknown";
+        Map<String, Object> conf = Util.map(
                 API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, modelId
+        );
+
+        assertFails(
+                db,
+                CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
+                "Caused by: java.io.FileNotFoundException: https://api.anthropic.com/v1/messages"
+        );
+    }
+
+    @Test
+    public void completionWithAnthropicNonDefaultMaxTokens() {
+        Map<String, Object> conf = Util.map(
+                API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, completionModel,
                 MAX_TOKENS_TO_SAMPLE, 1
         );
         testCall(db, COMPLETION_QUERY_EXTENDED_PROMPT,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
                     var completion = (String) result.get("completion");
@@ -152,31 +171,17 @@ public class OpenAIAnthropicIT {
                 });
     }
 
-    @Test
-    public void completionWithAnthropicUnkwnownModel() {
-        String modelId = "unknown";
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name(),
-                MODEL_CONF_KEY, modelId
-        );
-
-        assertFails(
-                db,
-                CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
-                "Caused by: java.io.FileNotFoundException: https://api.anthropic.com/v1/messages"
-        );
-    }
 
     @Test
     public void completionWithAnthropicSmallTokenSize() {
-        Map<String, Object> conf = Map.of(
+        Map<String, Object> conf = Util.map(
                 API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, completionModel,
                 MAX_TOKENS, 1
         );
 
         testCall(db, CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
                     var contentList = (List<Map<String, Object>>) result.get("content");
@@ -189,12 +194,13 @@ public class OpenAIAnthropicIT {
 
     @Test
     public void completionWithAnthropicCustomVersion() {
-        Map<String, Object> conf = Map.of(
+        Map<String, Object> conf = Util.map(
                 API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, completionModel,
                 ANTHROPIC_VERSION, "2023-06-01"
         );
         testCall(db, CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 (row) -> {
                     var result = (Map<String,Object>) row.get("value");
                     var contentList = (List<Map<String, Object>>) result.get("content");
@@ -206,31 +212,17 @@ public class OpenAIAnthropicIT {
     }
 
     @Test
-    public void completionWithAnthropicWrongVersion() {
-        Map<String, Object> conf = Map.of(
-                API_TYPE_CONF_KEY, ANTHROPIC.name(),
-                ANTHROPIC_VERSION, "ajeje"
-        );
-
-        assertFails(
-                db,
-                COMPLETION_QUERY_EXTENDED_PROMPT,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
-                "Server returned HTTP response code: 400 for URL"
-        );
-    }
-
-    @Test
     public void chatWithAnthropicWrongVersion() {
-        Map<String, Object> conf = Map.of(
+        Map<String, Object> conf = Util.map(
                 API_TYPE_CONF_KEY, ANTHROPIC.name(),
+                MODEL_CONF_KEY, chatModel,
                 ANTHROPIC_VERSION, "ajeje"
         );
 
         assertFails(
                 db,
                 CHAT_COMPLETION_QUERY_WITHOUT_SYSTEM,
-                Map.of("conf", conf, "apiKey", anthropicApiKey),
+                Util.map("conf", conf, "apiKey", anthropicApiKey),
                 "Server returned HTTP response code: 400 for URL"
         );
     }
