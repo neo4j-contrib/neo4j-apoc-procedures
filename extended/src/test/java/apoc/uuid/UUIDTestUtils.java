95a7e1e8419f8ddeffa3bb38cf9462f0e3b68750
vga91 <giuseppe.villani@larus-ba.it>
2026-03-09 12:09:01 +0100

    Fix cannot find symbol matchesRegex compile error

--- extended/src/test/java/apoc/uuid/UUIDTestUtils.java
+++ extended/src/test/java/apoc/uuid/UUIDTestUtils.java
@@ -1,6 +1,5 @@
 package apoc.uuid;
 
-import org.hamcrest.Matchers;
 import org.junit.rules.TemporaryFolder;
 import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -17,8 +16,8 @@ import static apoc.uuid.UUIDTest.UUID_TEST_REGEXP;
 import static apoc.uuid.UuidConfig.*;
 import static apoc.uuid.UuidConfig.ADD_TO_SET_LABELS_KEY;
 import static apoc.uuid.UuidHandler.APOC_UUID_REFRESH;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class UUIDTestUtils {
     public static DatabaseManagementService startDbWithUuidApocConfigs(TemporaryFolder storeDir) throws IOException {
@@ -43,7 +42,9 @@ public class UUIDTestUtils {
                 }, TIMEOUT);
     }
 
-    public static void assertIsUUID(Object uuid) {
-        assertThat((String) uuid, Matchers.matchesRegex(UUID_TEST_REGEXP));
+    public static void assertIsUUID(Object value) {
+        String stringValue = (String) value;
+        assertTrue("The value is not UUID, but is: " + stringValue, 
+                stringValue.matches(UUID_TEST_REGEXP));
     }
 }
