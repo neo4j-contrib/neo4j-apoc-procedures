2e823db328d301e1c87a5cf93579736e8de76bfa
vga91 <giuseppe.villani@larus-ba.it>
2026-03-09 12:10:55 +0100

    solve cannot find symbol getEndpointConfiguration() compile error

--- extended/src/test/java/apoc/util/s3/S3Util.java
+++ extended/src/test/java/apoc/util/s3/S3Util.java
@@ -1,12 +1,18 @@
 package apoc.util.s3;
 
+import java.net.URI;
+
 public class S3Util {
     public static String removeRegionFromUrl(S3Container s3Container, String url) {
-        return url.replace(s3Container.getEndpointConfiguration().getSigningRegion() + ".", "");
+        URI endpoint = s3Container.getEndpoint();
+        System.out.println("endpoint = " + endpoint);
+        System.out.println("endpoint.getPath() = " + endpoint.getPath());
+        return url.replace(endpoint + ".", "");
     }
 
     public static String putToS3AndGetUrl(S3Container s3Container, String filename) {
         String url = s3Container.putFile(filename);
+        System.out.println("url = " + url);
         return removeRegionFromUrl(s3Container, url);
     }
 }
