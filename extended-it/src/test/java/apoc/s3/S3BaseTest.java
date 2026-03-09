5aaba637a6df09c839cf8d290f68fd0ad0634fc5
vga91 <giuseppe.villani@larus-ba.it>
2026-03-09 15:13:11 +0100

    Fix sdk.services.s3.model.InvalidRequestException error

--- extended-it/src/test/java/apoc/s3/S3BaseTest.java
+++ extended-it/src/test/java/apoc/s3/S3BaseTest.java
@@ -11,6 +11,9 @@ public abstract class S3BaseTest {
     public static void baseBeforeClass() {
         s3Container = new S3Container();
 
+        // In test environment we skip the MD5 validation that can cause issues
+        System.setProperty("com.amazonaws.services.s3.disableGetObjectMD5Validation", "true");
+
         // Used by S3ParamsExtractor to prepend http:// to endpoints for local testing
         System.setProperty("com.amazonaws.sdk.disableCertChecking", "true");
     }
